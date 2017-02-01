45719818bcf6596ca98daeede70382c954d32c62
Chris Whytock <cwhytock@gmail.com>
2017-02-01 13:04:20 -0800

    Initial framework for the UWP Xaml renderer (#72)
    * Initial framework for the UWP Xaml renderer
    
    This change adds the basic framework of the UWP Xaml renderer component.
    Ignore the actual methods defined in the idl, they exist to enable
    hooking up and calling from a future test application.
    
    * Remove accidentally committed file
    
    AdaptiveCards.XamlCardRenderer.h is generated from the idl, so the
    header itself shouldn't be checked in.
    
    * Update component version to NTDDI_WIN10_RS1
    
    Update component version to NTDDI_WIN10_RS1

---
+++ UWP/Renderer/lib/XamlCardRendererComponent.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include "XamlCardRendererComponent.h"
+
+#include <windows.foundation.collections.h>
+
+using namespace AdaptiveCards::XamlCardRenderer;
+using namespace Microsoft::WRL;
+using namespace Microsoft::WRL::Wrappers;
+using namespace ABI::Windows::Foundation::Collections;
+using namespace ABI::Windows::UI::Xaml;
+using namespace ABI::Windows::UI::Xaml::Controls;
+
+XamlCardRenderer::XamlCardRenderer()
+{
+}
+
+HRESULT XamlCardRenderer::RuntimeClassInitialize()
+{
+    return S_OK;
+}
+
+HRESULT XamlCardRenderer::RenderCardAsXaml(IUIElement** root)
+{
+    *root = nullptr;
+
+    ComPtr<IInspectable> inspectableGrid;
+    ComPtr<IGrid> grid;
+    ComPtr<IPanel> gridAsPanel;
+    ComPtr<IVector<UIElement*>> gridChildren;
+    RETURN_IF_FAILED(RoActivateInstance(HStringReference(RuntimeClass_Windows_UI_Xaml_Controls_Grid).Get(), inspectableGrid.ReleaseAndGetAddressOf()));
+    RETURN_IF_FAILED(inspectableGrid.As(&grid));
+    RETURN_IF_FAILED(inspectableGrid.As(&gridAsPanel));
+    RETURN_IF_FAILED(gridAsPanel->get_Children(gridChildren.GetAddressOf()));
+
+    return grid->QueryInterface(root);
+}
+
+HRESULT XamlCardRenderer::RenderCardAsImage()
+{
+    return S_OK;
+}
+
---
+++ UWP/Renderer/lib/XamlCardRendererComponent.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "AdaptiveCards.XamlCardRenderer.h"
+
+namespace AdaptiveCards { namespace XamlCardRenderer
+{
+    // This class is effectively a singleton, and stays around between subsequent renders.
+    class XamlCardRenderer :
+        public Microsoft::WRL::RuntimeClass<
+            Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::RuntimeClassType::WinRtClassicComMix>,
+            Microsoft::WRL::FtmBase,
+            ABI::AdaptiveCards::XamlCardRenderer::IXamlCardRenderer>
+    {
+        InspectableClass(L"AdaptiveCards.XamlCardRenderer", BaseTrust)
+
+    public:
+        XamlCardRenderer();
+        HRESULT RuntimeClassInitialize();
+
+        IFACEMETHODIMP RenderCardAsXaml(ABI::Windows::UI::Xaml::IUIElement** root);
+        IFACEMETHODIMP RenderCardAsImage();
+
+    };
+
+    ActivatableClass(XamlCardRenderer);
+}}
