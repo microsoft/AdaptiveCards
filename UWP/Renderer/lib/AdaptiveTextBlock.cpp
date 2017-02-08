a6cfeea8cec4a623f45840d4b989e9ba1b9835e7
Ricardo Arenas <riarenas@microsoft.com>
2017-02-08 13:28:50 -0800

    fix merge errors

--- UWP/Renderer/lib/AdaptiveTextBlock.cpp
+++ UWP/Renderer/lib/AdaptiveTextBlock.cpp
@@ -14,7 +14,7 @@ namespace AdaptiveCards {
 namespace XamlCardRenderer
 {
 
-AdaptiveTextBlock::AdaptiveTextBlock : m_TextBlock(std::make_unique<TextBlock>()) {}
+AdaptiveTextBlock::AdaptiveTextBlock() : m_TextBlock(std::make_unique<TextBlock>()) {}
 
 _Use_decl_annotations_
 HRESULT AdaptiveTextBlock::get_Text(HSTRING* Text)
@@ -32,28 +32,28 @@ HRESULT AdaptiveTextBlock::put_Text(HSTRING Text)
 }
 
 _Use_decl_annotations_
-HRESULT AdaptiveTextBlock::get_TextSize(ObjectModel::TextSize* TextSize)
+HRESULT AdaptiveTextBlock::get_TextSize(ABI::AdaptiveCards::XamlCardRenderer::TextSize* TextSize)
 {
-    *TextSize = static_cast<ObjectModel::TextSize>(m_TextBlock->GetTextSize());
+    *TextSize = static_cast<ABI::AdaptiveCards::XamlCardRenderer::TextSize>(m_TextBlock->GetTextSize());
     return S_OK;
 }
 
 _Use_decl_annotations_
-HRESULT AdaptiveTextBlock::put_TextSize(ObjectModel::TextSize TextSize)
+HRESULT AdaptiveTextBlock::put_TextSize(ABI::AdaptiveCards::XamlCardRenderer::TextSize TextSize)
 {
     m_TextBlock->SetTextSize(static_cast<AdaptiveCards::TextSize>(TextSize));
     return S_OK;
 }
 
 _Use_decl_annotations_
-HRESULT AdaptiveTextBlock::get_TextWeight(ObjectModel::TextWeight* TextWeight)
+HRESULT AdaptiveTextBlock::get_TextWeight(ABI::AdaptiveCards::XamlCardRenderer::TextWeight* TextWeight)
 {
-    *TextWeight = static_cast<ObjectModel::TextWeight>(m_TextBlock->GetTextWeight());
+    *TextWeight = static_cast<ABI::AdaptiveCards::XamlCardRenderer::TextWeight>(m_TextBlock->GetTextWeight());
     return S_OK;
 }
 
 _Use_decl_annotations_
-HRESULT AdaptiveTextBlock::put_TextWeight(ObjectModel::TextWeight TextWeight)
+HRESULT AdaptiveTextBlock::put_TextWeight(ABI::AdaptiveCards::XamlCardRenderer::TextWeight TextWeight)
 {
     m_TextBlock->SetTextWeight(static_cast<AdaptiveCards::TextWeight>(TextWeight));
     return S_OK;
