13eff208780326c81c02396613e54fed0d11d63f
Matt Hidinger <matt.hidinger@gmail.com>
2017-03-27 11:43:43 -0700

    proj cleanup

--- source/dotnet/AdaptiveCards.Renderers.WPF/Utilities.cs
+++ source/dotnet/AdaptiveCards.Renderers.WPF/Utilities.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using Adaptive;
 
 namespace Adaptive
 {
