ecd627adaa31604ba3bea1bafdc1ca78f295eddd
Tom Laird-McConnell <tomlm@microsoft.com>
2017-04-15 15:50:39 -0700

    fix scripts

--- source/dotnet/EnableDelaySign.cmd
+++ source/dotnet/EnableDelaySign.cmd
@@ -1,4 +1,4 @@
-@rep -find:"//[assembly: AssemblyKeyFileAttribute(\@\"\\\\cp1pd2cdscvlt01\\Public\\StrongNamePublicKeys\\35MSSharedLib1024.snk\")]" -replace:"[assembly: AssemblyKeyFileAttribute(\@\"\\\\cp1pd2cdscvlt01\\Public\\StrongNamePublicKeys\\35MSSharedLib1024.snk\")]" -r AssemblyInfo.cs
+@rep -find:"//[assembly: AssemblyKeyFileAttribute" -replace:"[assembly: AssemblyKeyFileAttribute" -r AssemblyInfo.cs
 
 @rep -find:"//[assembly: AssemblyDelaySignAttribute(true)]" -replace:"[assembly: AssemblyDelaySignAttribute(true)]" -r AssemblyInfo.cs
 
