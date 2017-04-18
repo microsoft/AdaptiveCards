c16341ae07c51d571668d350e15ed5cb1e0bd584
Tom Laird-McConnell <tomlm@microsoft.com>
2017-04-17 22:43:42 -0700

    remove delaysign

--- source/dotnet/Library/AdaptiveCards/Properties/AssemblyInfo.cs
+++ source/dotnet/Library/AdaptiveCards/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Reflection;
 [assembly: CLSCompliant(true)]
 
 // Uncomment to delay-sign
-[assembly: AssemblyKeyFileAttribute(@"..\..\35MSSharedLib1024.snk")]
-[assembly: AssemblyDelaySignAttribute(true)]
+//[assembly: AssemblyKeyFileAttribute(@"..\..\35MSSharedLib1024.snk")]
+//[assembly: AssemblyDelaySignAttribute(true)]
 
