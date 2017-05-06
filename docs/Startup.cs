2905271be749e8bf75d304673fce5e5f809c9f27
Tom Laird-McConnell <tomlm@microsoft.com>
2017-05-05 17:22:32 -0700

    fix .netcore for static files

--- docs/Startup.cs
+++ docs/Startup.cs
@@ -34,6 +34,8 @@ namespace Docs
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
