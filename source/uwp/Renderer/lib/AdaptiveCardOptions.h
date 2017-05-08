37cdff1eff660ab037a959324274be4b41a0485f
Ricardo Arenas <riarenas@microsoft.com>
2017-05-08 11:23:26 -0700

    Add styling for AdaptiveCard (#295)
    Update HostOptions

--- source/uwp/Renderer/lib/AdaptiveCardOptions.h
+++ source/uwp/Renderer/lib/AdaptiveCardOptions.h
@@ -20,20 +20,8 @@ namespace AdaptiveCards { namespace XamlCardRenderer
         IFACEMETHODIMP get_Padding(_Out_ ABI::AdaptiveCards::XamlCardRenderer::IAdaptiveBoundaryOptions** value);
         IFACEMETHODIMP put_Padding(_In_ ABI::AdaptiveCards::XamlCardRenderer::IAdaptiveBoundaryOptions* value);
 
-        IFACEMETHODIMP get_MaxActions(_Out_ UINT32 *value);
-        IFACEMETHODIMP put_MaxActions(_In_ UINT32 value);
-
-        IFACEMETHODIMP get_ActionAlignment(_Out_ ABI::AdaptiveCards::XamlCardRenderer::HAlignment* value);
-        IFACEMETHODIMP put_ActionAlignment(_In_ ABI::AdaptiveCards::XamlCardRenderer::HAlignment value);
-
         IFACEMETHODIMP get_ActionsOrientation(_Out_ ABI::AdaptiveCards::XamlCardRenderer::ActionsOrientation* value);
         IFACEMETHODIMP put_ActionsOrientation(_In_ ABI::AdaptiveCards::XamlCardRenderer::ActionsOrientation value);
-
-        IFACEMETHODIMP get_BorderColor(_Out_ ABI::Windows::UI::Color* value);
-        IFACEMETHODIMP put_BorderColor(_In_ ABI::Windows::UI::Color value);
-
-        IFACEMETHODIMP get_TextColor(_Out_ ABI::Windows::UI::Color* value);
-        IFACEMETHODIMP put_TextColor(_In_ ABI::Windows::UI::Color value);
 
         IFACEMETHODIMP get_BackgroundColor(_Out_ ABI::Windows::UI::Color* value);
         IFACEMETHODIMP put_BackgroundColor(_In_ ABI::Windows::UI::Color value);
