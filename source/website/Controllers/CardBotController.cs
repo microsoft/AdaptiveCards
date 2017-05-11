109958b0dde934f88c1350d38a290345db02fef4
Tom Laird-McConnell <tomlm@microsoft.com>
2017-05-11 16:05:04 -0700

    wubble with javascript

--- source/website/Controllers/CardBotController.cs
+++ source/website/Controllers/CardBotController.cs
@@ -1,18 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System.Net.Http;
-using Microsoft.Bot.Connector;
-using System.Threading;
-using System.IO;
-using System.Text;
-using Microsoft.AspNetCore.Hosting;
-using Newtonsoft.Json;
 using AdaptiveCards;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Docs.Controllers
 {
@@ -50,15 +49,65 @@ namespace Docs.Controllers
                         break;
                 }
             }
-            return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            return new HttpResponseMessage(HttpStatusCode.OK);
         }
 
-        private async Task<HttpResponseMessage> SendReply(IMessageActivity message, string text, Attachment[] attachments = null)
+        [HttpGet]
+        public async Task<ContentResult> Get(string cardName)
+        {
+            cardName = cardName.Replace("_", " ");
+            var path = Path.Combine(_env.WebRootPath, $@"samples\cards\{cardName}.json");
+            if (System.IO.File.Exists(path))
+            {
+                string json = System.IO.File.ReadAllText(path);
+                if (json != null)
+                {
+                    AdaptiveCard card = JsonConvert.DeserializeObject<AdaptiveCard>(json);
+
+                    return new ContentResult()
+                    {
+                        Content = $@"
+<html lang='en'>
+<head>
+    <meta charset = 'utf-8' />
+    <title>Adaptive Cards</title>
+
+    <link rel='stylesheet' href='/content/fonts.css'>
+	<link rel='stylesheet' href='/content/shared.css'>
+	<link rel='stylesheet' href='/content/card.css'>
+	<link rel='stylesheet' href='/explorer/explorer.css'>
+    <script src='/node_modules/jquery/dist/jquery.min.js'></script>
+    <script src='/node_modules/adaptive-cards/dist/adaptive-cards.js'></script> 
+	<script src='/explorer/explorer.js'></script>
+<style>
+body {{
+    margin:0px;
+}}
+.card {{
+    margin:0px;
+    width:100%;
+}}                            
+</style>
+
+</head>
+<body>
+<div id='kikCard' class='card' ></div>
+<script>renderCard('kikCard', {json});</script>
+</body>
+</html>",
+                        ContentType = "text/html"
+                    };
+                }
+            }
+            return null;
+        }
+
+        private async Task<HttpResponseMessage> SendReply(IMessageActivity message, string text, Attachment[] attachments = null, object channelData = null)
         {
             var reply = ((Activity)message).CreateReply(text);
             reply.Attachments = attachments;
+            reply.ChannelData = channelData;
 
-            //MicrosoftAppCredentials.TrustServiceUrl(message.ServiceUrl);
             var connectorAPI = new ConnectorClient(new Uri(message.ServiceUrl), new MicrosoftAppCredentials(AppId, AppPassword));
             await connectorAPI.Conversations.ReplyToActivityAsync(reply);
             return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
@@ -68,26 +117,46 @@ namespace Docs.Controllers
         {
             if (message.Text.Contains("cards"))
                 return await ListCards(message);
+            if (message.Text.Contains("help"))
+                return await SendHelp(message);
 
-            return await SendCard((Activity)message, message.Text.Replace(" ", ""));
+            return await SendCard((Activity)message, message.Text.ToLower());
         }
 
+        private async Task<HttpResponseMessage> SendHelp(IMessageActivity message)
+        {
+            return await SendReply(message, "* type **cards** to get a list of cards\n* type ***cardname* ** to display a given card by name");
+        }
 
         private async Task<HttpResponseMessage> SendCard(Activity activity, string cardName)
         {
             var path = Path.Combine(_env.WebRootPath, $@"samples\cards\{cardName}.json");
             if (System.IO.File.Exists(path))
             {
-                string json = System.IO.File.ReadAllText(path);
-                if (json != null)
-                {
-                    AdaptiveCard card = JsonConvert.DeserializeObject<AdaptiveCard>(json);
-                    var attachments = new Attachment[] { new Attachment(AdaptiveCard.ContentType, content: card) };
-                    return await SendReply(activity, String.Empty, attachments);
-                }
+                var incomingChannelData = JsonConvert.DeserializeObject<KikMessages>((string)activity.ChannelData);
+                var json = $@"
+ {{
+    'messages': [
+        {{
+            'type': 'widget',
+            'chatId': '{incomingChannelData.messages[0].chatId}',
+            'to': '{incomingChannelData.messages[0].from}',
+            'url': 'http://adaptivecards-staging.azurewebsites.net/api/CardBot?cardName={cardName.Replace(" ","_")}',
+            'size': {{
+                'width': 220,
+                'height': 220
+            }},
+            'static': false,
+        }}
+    ]
+}}
+";
+                dynamic channelData = JsonConvert.DeserializeObject(json);
+
+                return await SendReply(activity, String.Empty, attachments: null, channelData: channelData);
             }
-            return await SendReply(activity, $"Could not find a card for {activity.Text}");
-            return await SendReply(activity, "* type **cards** to get a list of cards\n* type **card *cardname* ** to display a given card by name");
+            await SendReply(activity, $"I could not find a card for {activity.Text}");
+            return await SendHelp(activity);
         }
 
 
@@ -105,4 +174,67 @@ namespace Docs.Controllers
             return await SendReply(message, sb.ToString());
         }
     }
+
+    public class KikMessage
+    {
+        // Conversation bot is involved in.  Must be sent back in all responses
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string chatId { get; set; }
+
+        // Id for this particular message
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string id { get; set; }
+
+        // "text", "link", "picture", "video", "start-chatting", "scan-data", "sticker", "is-typing", "delivery-receipt", "read-receipt"
+        public string type { get; set; }
+
+        // Kik username of message sender
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string from { get; set; }
+
+        // List of particpants in conversation
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public List<string> participants { get; set; }
+
+        // Text of the message when message is of type 'text'
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string body { get; set; }
+
+        // Data coming from a scan code
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string data { get; set; }
+
+        // Epoch in milliseconds when message was send
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public long? timestamp { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public bool? readReceiptRequested { get; set; }
+
+        // Url to an image when message is of type 'picture'
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string picUrl { get; set; }
+
+        // The URL of the video or GIF you wish to send.
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string videoUrl { get; set; }
+
+        // Url to a sticker image when message is of type 'sticker'
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string stickerUrl { get; set; }
+
+        // Link Url
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string url { get; set; }
+
+        // Not sent in JSON but as part of request header
+        public string to { get; set; }
+    }
+
+    public class KikMessages
+    {
+        public List<KikMessage> messages { get; set; }
+
+    }
+
 }
