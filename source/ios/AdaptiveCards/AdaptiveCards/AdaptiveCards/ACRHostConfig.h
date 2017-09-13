50601d2c15b7eb5d7845eb5cf77b208ca2652981
Joseph Woo <Joseph.Woo@microsoft.com>
2017-09-12 19:23:00 -0700

    host config added

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRHostConfig.h
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRHostConfig.h
@@ -1,30 +1,10 @@
 //
-//  ADCIOSAdaptiveHostConfig.h
-//  ADCIOSFramework
+//  ACRHostConfig.h
+//  ACRHostConfig
 //
 //  Copyright © 2017 Microsoft. All rights reserved.
 //
-
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-#import "TextBlock.h"
-#import "Image.h"
-#import "HostConfig.h"
-
-using namespace AdaptiveCards;
-
 @interface ACRHostConfig:NSObject
-
-- (CGSize) getImageSize:(std::shared_ptr<Image> const &) img;
-
-- (UIColor* ) getTextBlockColor:(std::shared_ptr<AdaptiveCards::TextBlock> const &) txtBlock;
-
-- (int) getTextBlockTextSize:(std::shared_ptr<TextBlock> const &)txtBlock;
-
-- (NSTextAlignment) getTextBlockAlignment:(std::shared_ptr<TextBlock> const &) txtBlock;
-
-- (NSNumber* ) getTextBlockTextWeight:(std::shared_ptr<TextBlock> const &) txtBlock;
-
-@property std::shared_ptr<HostConfig> m_hostConfig;
-
+- (instancetype)init;
+- (instancetype)initWithJson:(NSString *)payload;
 @end    
