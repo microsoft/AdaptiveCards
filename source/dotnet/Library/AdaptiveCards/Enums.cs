8601a5a866c1b63f69ea00c198fd941583293482
Eric Dahlvang <v-erdahl@microsoft.com>
2017-09-20 18:36:54 -0700

    Rename Column size to width. Also, use TextSize.Default in XamlTextBlock and Html RenderContext.

--- source/dotnet/Library/AdaptiveCards/Enums.cs
+++ source/dotnet/Library/AdaptiveCards/Enums.cs
@@ -30,6 +30,23 @@ namespace AdaptiveCards
     /// <summary>
     ///     Controls the horizontal size (width) of Column.
     /// </summary>
+    public class ColumnWidth
+    {
+        /// <summary>
+        ///     The width of the Column is optimally chosen depending on the space available in the element's container
+        /// </summary>
+        public const string Auto = "Auto";
+
+        /// <summary>
+        ///     The width of the Column adjusts to match that of its container
+        /// </summary>
+        public const string Stretch = "Stretch";
+    }
+
+    /// <summary>
+    ///     Controls the horizontal size (width) of Column.
+    /// </summary>
+    [Obsolete("ColumnSize has been deprecated.  Use ColumnWidth", false)]
     public class ColumnSize
     {
         /// <summary>
