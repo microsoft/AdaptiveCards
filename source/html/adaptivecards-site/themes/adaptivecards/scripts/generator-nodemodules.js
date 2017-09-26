2d37fdad30ec07000810d196083330831c0a17a3
Matt Hidinger <matt.hidinger@gmail.com>
2017-09-26 09:31:53 -0700

    cleanup

--- source/html/adaptivecards-site/themes/adaptivecards/scripts/generator-nodemodules.js
+++ source/html/adaptivecards-site/themes/adaptivecards/scripts/generator-nodemodules.js
@@ -1,22 +1,41 @@
 "use strict";
 
-// Copy scripts from node_modules into the public dir
+// Copy various adaptive card assets from elsehwere in the repo into the output folder
 
 var fs = require("hexo-fs");
 var path = require("path");
+var glob = require("glob");
 
-var modules = [
-    // TODO get from renderer
-    //"node_modules/microsoft-adaptivecards/dist/adaptive-cards.js"
+var assets = [
+    {
+        path: "../../../samples/v1.0/**/*.json",
+        dest: function (p) { return "payloads/" + path.basename(p); }
+    },
+    {
+        path: "../../../schemas/adaptive-card.json",
+        dest: function (p) { return "schemas/adaptive-card.json"; }
+    },
+    {
+        path: "../renderer/dist/adaptive-cards.json",
+        dest: function (p) { return "js/adaptive-cards.json"; }
+    },
+    {
+        path: "../visualizer/built/visualizer.js",
+        dest: function (p) { return "js/visualizer.js"; }
+    }
 ];
 
 hexo.extend.generator.register("generator-nodemodules", function (locals) {
-    return modules.map(function (file) {
-        return {
-            path: "js/" + path.basename(file),
-            data: function () {
-                return fs.createReadStream(file)
+
+    return assets.forEach(function (asset) {
+        glob.sync(asset.path, { nocase: false }).map(function (p) {
+
+            return {
+                path: asset.dest(p),
+                data: function () {
+                    return fs.createReadStream(p)
+                }
             }
-        }
+        });
     });
 });
--- source/html/adaptivecards-site/themes/adaptivecards/scripts/generator-visualizer.js
+++ source/html/adaptivecards-site/themes/adaptivecards/scripts/generator-visualizer.js
@@ -1,27 +1,27 @@
 "use strict";
 
-// Copy scripts from node_modules into the public dir
-
 var fs = require("hexo-fs");
 var path = require("path");
 
-
-var modules = [
-    // TODO get from renderer
-    //"node_modules/microsoft-adaptivecards/dist/adaptive-cards.js"
-];
-
-var visualizerPath = "C:/Dev/Projects/AdaptiveCards/source/html/visualizer/";
-
 hexo.extend.generator.register("generator-visualizer", function (locals) {
 
-    return {
-        path: "visualizer/index.html",
-        layout: "visualizer",
-        data: {
-            
+    return [
+        {
+            path: "visualizer/index.html",
+            layout: "visualizer",
+            data: {}
+        },
+        {
+            path: "js/visualizer.js",
+            data: function () {
+                var file = fs.createWriteStream("../visualizer/built/visualizer.js")
+                if (!file) {
+                    throw new Error("MISSING Visualizer script. Did you build it?");
+                }
+                return file;
+            }
         }
-    }
+    ];
 
 });
 
