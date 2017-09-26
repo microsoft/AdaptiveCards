021358b81ce9e754e40d90f8ef2ea1645f56b79d
Matt Hidinger <matt.hidinger@gmail.com>
2017-09-25 22:48:31 -0700

    getting closer

--- source/html/adaptivecards-site/themes/adaptivecards/scripts/propertiesDetails.js
+++ source/html/adaptivecards-site/themes/adaptivecards/scripts/propertiesDetails.js
@@ -5,33 +5,36 @@ var marked = require("marked");
 var fs = require("fs");
 
 hexo.extend.helper.register('propertiesDetails', function (properties) {
-  var html = '';
+  var html = '<h2 class="w3-container">Properties</h2>';
 
   for (var name in properties) {
     if (properties.hasOwnProperty(name)) {
       var property = properties[name];
 
       // Get the markdown for the property and turn it into HTML
-      var md = markedschema.generateMarkdown.createPropertyDetails(property, 3);
-      html += marked(md, { headerPrefix: "dedupe-header" });
+      html += '<div class="w3-container">';
+    
+      html += marked(markedschema.generateMarkdown.createPropertyDetails(property, 3), { headerPrefix: "dedupe-header" });
+      html += '</div>'
 
 
       // Load card example file into a div, using JS to render it later
       property.examples.forEach(function (example, i) {
         if (i === 0) {
-          html += '<h3>Example</h3>';
+          html += '<h3 class="w3-container">Example</h3>';
         }
 
         var exampleContent = fs.readFileSync(example, "utf8");
 
         html += '<div class="w3-cell-row">';
 
-        html += '<div class="w3-container w3-cell w3-card-2 w3-light-grey w3-mobile">';
-        html += "<pre><code>" + exampleContent + "</code></pre>";
+        html += '<div class="w3-container w3-cell w3-mobile code-snippet">';
+        html += "<pre><code class='json'>" + exampleContent + "</code></pre>";
         html += "</div>";
 
-        html += '<div class="w3-container w3-cell w3-card-2 w3-blue w3-mobile" style="min-width: 250px; max-width: 250px">';
+        html += '<div class="w3-container w3-cell w3-mobile" style="width: 300px">';
         html += '<div class="adaptivecard">' + exampleContent + '</div>';
+        html += '<a class="w3-btn w3-blue" target="_blank" href="/visualizer?card=/explorer/cards/TextBlock.Color.json">Try it Yourself »</a>';
         html += '</div>';
 
         html += '</div>'; // w3-cell-row
--- source/html/adaptivecards-site/themes/adaptivecards/source/js/script.js
+++ source/html/adaptivecards-site/themes/adaptivecards/source/js/script.js
@@ -21,132 +21,129 @@
 
 
   var hostConfig = {
-    adaptiveCard: {
-      allowCustomStyle: false
-    },
     spacing: {
-        small: 3,
-        default: 8,
-        medium: 20,
-        large: 30,
-        extraLarge: 40,
-        padding: 10
+      small: 3,
+      default: 8,
+      medium: 20,
+      large: 30,
+      extraLarge: 40,
+      padding: 10
     },
     separator: {
-        lineThickness: 1,
-        lineColor: "#EEEEEE"
+      lineThickness: 1,
+      lineColor: "#EEEEEE"
     },
     supportsInteractivity: true,
     fontFamily: "Segoe UI",
     fontSizes: {
-        small: 12,
-        default: 14,
-        medium: 17,
-        large: 21,
-        extraLarge: 26
+      small: 12,
+      default: 14,
+      medium: 17,
+      large: 21,
+      extraLarge: 26
     },
     fontWeights: {
-        lighter: 200,
-        default: 400,
-        bolder: 600
+      lighter: 200,
+      default: 400,
+      bolder: 600
     },
     containerStyles: {
-        default: {
-            backgroundColor: "#00000000",
-            fontColors: {
-                default: {
-                    normal: "#333333",
-                    subtle: "#EE333333"
-                },
-                accent: {
-                    normal: "#2E89FC",
-                    subtle: "#882E89FC"
-                },
-                attention: {
-                    normal: "#FFD800",
-                    subtle: "#DDFFD800"
-                },
-                good: {
-                    normal: "#00FF00",
-                    subtle: "#DD00FF00"
-                },
-                warning: {
-                    normal: "#FF0000",
-                    subtle: "#DDFF0000"
-                }
-            }
-        },
-        emphasis: {
-            backgroundColor: "08000000",
-            fontColors: {
-                default: {
-                    normal: "#333333",
-                    subtle: "#EE333333"
-                },
-                accent: {
-                    normal: "#2E89FC",
-                    subtle: "#882E89FC"
-                },
-                attention: {
-                    normal: "#FFD800",
-                    subtle: "#DDFFD800"
-                },
-                good: {
-                    normal: "#00FF00",
-                    subtle: "#DD00FF00"
-                },
-                warning: {
-                    normal: "#FF0000",
-                    subtle: "#DDFF0000"
-                }
-            }
+      default: {
+        backgroundColor: "#00000000",
+        fontColors: {
+          default: {
+            normal: "#333333",
+            subtle: "#EE333333"
+          },
+          accent: {
+            normal: "#2E89FC",
+            subtle: "#882E89FC"
+          },
+          attention: {
+            normal: "#FFD800",
+            subtle: "#DDFFD800"
+          },
+          good: {
+            normal: "#00FF00",
+            subtle: "#DD00FF00"
+          },
+          warning: {
+            normal: "#FF0000",
+            subtle: "#DDFF0000"
+          }
         }
+      },
+      emphasis: {
+        backgroundColor: "08000000",
+        fontColors: {
+          default: {
+            normal: "#333333",
+            subtle: "#EE333333"
+          },
+          accent: {
+            normal: "#2E89FC",
+            subtle: "#882E89FC"
+          },
+          attention: {
+            normal: "#FFD800",
+            subtle: "#DDFFD800"
+          },
+          good: {
+            normal: "#00FF00",
+            subtle: "#DD00FF00"
+          },
+          warning: {
+            normal: "#FF0000",
+            subtle: "#DDFF0000"
+          }
+        }
+      }
     },
     imageSizes: {
-        small: 40,
-        medium: 80,
-        large: 160
+      small: 40,
+      medium: 80,
+      large: 160
     },
     actions: {
-        maxActions: 3,
-        spacing: "default",
-        buttonSpacing: 20,
-        showCard: {
-            actionMode: "inline",
-            inlineTopMargin: 16
-        },
-        actionsOrientation: "horizontal",
-        actionAlignment: "left"
+      maxActions: 3,
+      spacing: 2,
+      buttonSpacing: 10,
+      showCard: {
+        actionMode: "inline",
+        inlineTopMargin: 16
+      },
+      actionsOrientation: 0,
+      actionAlignment: 0
     },
     adaptiveCard: {
-        allowCustomStyle: false
+      allowCustomStyle: false
     },
     image: {
-        size: "medium",
+      size: "medium",
     },
     imageSet: {
-        imageSize: "medium",
-        maxImageHeight: 100
+      imageSize: "medium",
+      maxImageHeight: 100
     },
     factSet: {
-        title: {
-            color: "default",
-            size: "default",
-            isSubtle: false,
-            weight: "bolder",
-            wrap: true,
-            maxWidth: 150
-        },
-        value: {
-            color: "default",
-            size: "default",
-            isSubtle: false,
-            weight: "default",
-            wrap: true
-        },
-        spacing: 10
+      title: {
+        color: "default",
+        size: "default",
+        isSubtle: false,
+        weight: "bolder",
+        wrap: true,
+        maxWidth: 150
+      },
+      value: {
+        color: "default",
+        size: "default",
+        isSubtle: false,
+        weight: "default",
+        wrap: true
+      },
+      spacing: 10
     }
-};
+  };
 
   $('.adaptivecard').each(function (item) {
 
@@ -156,14 +153,14 @@
     adaptiveCard.parse(json);
     var renderedCard = adaptiveCard.render();
 
-    $(this).replaceWith(renderedCard).parent().show();
+    $(this).text('').append(renderedCard).show();
 
   });
 
   $('.ac-properties table').addClass("w3-table w3-bordered w3-responsive")
 
-  $(document).ready(function() {
-    $('pre code').each(function(i, block) {
+  $(document).ready(function () {
+    $('pre code').each(function (i, block) {
       hljs.highlightBlock(block);
     });
   });
