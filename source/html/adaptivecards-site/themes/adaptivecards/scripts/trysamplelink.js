0fa227307651cbea1853d5e6b8dbf14b7ed6d0d2
Matt Hidinger <matt.hidinger@gmail.com>
2017-09-26 11:11:29 -0700

    finished!

--- source/html/adaptivecards-site/themes/adaptivecards/scripts/trysamplelink.js
+++ source/html/adaptivecards-site/themes/adaptivecards/scripts/trysamplelink.js
@@ -3,6 +3,5 @@
 var path = require("path");
 
 hexo.extend.helper.register('trysamplelink', function (samplePath) {
-  // TODO: figure this out
-  //return '<a class="w3-btn w3-blue" target="_blank" href="/visualizer/index.html?card=/payloads/' + path.basename(samplePath); + '">Try it Yourself »</a>';
+  return '<a class="w3-btn w3-blue" target="_blank" href="/visualizer/index.html?card=/payloads/' + path.basename(samplePath) + '">Try it Yourself »</a>';
 });
