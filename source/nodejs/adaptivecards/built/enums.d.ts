f5670be715da99e9a3ff2a06f45794a6c39a7958
Matt Hidinger <matt.hidinger@gmail.com>
2017-09-28 16:07:48 -0700

    update npm package name and version (#716)
    * update npm package name and version
    
    * npm package updates
    
    * rename source

---
+++ source/nodejs/adaptivecards/built/enums.d.ts
@@ -0,0 +1,92 @@
+export declare enum Size {
+    Auto = 0,
+    Stretch = 1,
+    Small = 2,
+    Medium = 3,
+    Large = 4,
+}
+export declare enum TextSize {
+    Small = 0,
+    Default = 1,
+    Medium = 2,
+    Large = 3,
+    ExtraLarge = 4,
+}
+export declare enum Spacing {
+    None = 0,
+    Small = 1,
+    Default = 2,
+    Medium = 3,
+    Large = 4,
+    ExtraLarge = 5,
+    Padding = 6,
+}
+export declare enum Padding {
+    None = 0,
+    Default = 1,
+}
+export declare enum TextWeight {
+    Lighter = 0,
+    Default = 1,
+    Bolder = 2,
+}
+export declare enum TextColor {
+    Default = 0,
+    Accent = 1,
+    Good = 2,
+    Warning = 3,
+    Attention = 4,
+}
+export declare enum HorizontalAlignment {
+    Left = 0,
+    Center = 1,
+    Right = 2,
+}
+export declare enum VerticalAlignment {
+    Top = 0,
+    Center = 1,
+    Bottom = 2,
+}
+export declare enum ActionAlignment {
+    Left = 0,
+    Center = 1,
+    Right = 2,
+    Stretch = 3,
+}
+export declare enum ImageStyle {
+    Default = 0,
+    Person = 1,
+}
+export declare enum ShowCardActionMode {
+    Inline = 0,
+    Popup = 1,
+}
+export declare enum Orientation {
+    Horizontal = 0,
+    Vertical = 1,
+}
+export declare enum BackgroundImageMode {
+    Stretch = 0,
+    RepeatHorizontally = 1,
+    RepeatVertically = 2,
+    Repeat = 3,
+}
+export declare enum ContainerStyle {
+    Default = 0,
+    Emphasis = 1,
+}
+export declare enum ValidationError {
+    Hint = 0,
+    ActionTypeNotAllowed = 1,
+    CollectionCantBeEmpty = 2,
+    Deprecated = 3,
+    ElementTypeNotAllowed = 4,
+    InteractivityNotAllowed = 5,
+    InvalidPropertyValue = 6,
+    MissingCardType = 7,
+    PropertyCantBeNull = 8,
+    TooManyActions = 9,
+    UnknownActionType = 10,
+    UnknownElementType = 11,
+    UnsupportedCardVersion = 12,
+}
