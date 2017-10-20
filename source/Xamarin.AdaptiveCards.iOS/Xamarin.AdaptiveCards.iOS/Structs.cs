9f895874a35f4943a326297a94571b017e4f01d8
Joseph Woo <Joseph.Woo@microsoft.com>
2017-10-20 16:09:46 -0700

    xamarin ios update

---
+++ source/Xamarin.AdaptiveCards.iOS/Xamarin.AdaptiveCards.iOS/Structs.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+namespace Xamarin.AdaptiveCards.iOS
+{
+}
---
+++ source/Xamarin.AdaptiveCards.iOS/Xamarin.AdaptiveCards.iOS/libAdaptiveCards.linkwith.cs
@@ -0,0 +1,10 @@
+// WARNING: This feature is deprecated. Use the "Native References" folder instead.
+// Right-click on the "Native References" folder, select "Add Native Reference",
+// and then select the static library or framework that you'd like to bind.
+//
+// Once you've added your static library or framework, right-click the library or
+// framework and select "Properties" to change the LinkWith values.
+
+using ObjCRuntime;
+
+[assembly: LinkWith ("libAdaptiveCards.a", SmartLink = true, ForceLoad = true)]
