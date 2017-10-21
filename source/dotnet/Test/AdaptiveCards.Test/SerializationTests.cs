08ad8cbb8c46997da4397f06a8cc30906e7b3237
Matt Hidinger <matt.hidinger@gmail.com>
2017-10-21 14:31:53 -0700

    Fix serialization for container style (#806)
    * Fix serialization for container style
    
    * add assemblyinfo back
    
    * fix tests

--- source/dotnet/Test/AdaptiveCards.Test/SerializationTests.cs
+++ source/dotnet/Test/AdaptiveCards.Test/SerializationTests.cs
@@ -47,5 +47,30 @@ namespace AdaptiveCards.Test
             Assert.AreEqual(expected, card.ToJson());
             
         }
+
+        [TestMethod]
+        public void TestSerializingTextBlock()
+        {
+            var card = new AdaptiveCard()
+            {
+                Body =
+                {
+                    new AdaptiveTextBlock()
+                    {
+                        Text = "Hello world"
+                    }
+                }
+            };
+
+            string json = card.ToJson();
+
+            // Re-parse the card
+            card = AdaptiveCard.FromJson(json).Card;
+
+            // Ensure there's a text element
+            Assert.AreEqual(1, card.Body.Count);
+            Assert.IsInstanceOfType(card.Body[0], typeof(AdaptiveTextBlock));
+            Assert.AreEqual("Hello world", (card.Body[0] as AdaptiveTextBlock).Text);
+        }
     }
 }
