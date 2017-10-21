055d8e87d5c9e6219a3b926e8c0ee1d15fd9a571
Matt Hidinger <matt.hidinger@gmail.com>
2017-10-21 14:28:02 -0700

    add assemblyinfo back

---
+++ source/dotnet/Library/AdaptiveCards.Rendering.Wpf/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Reflection;
+
+#if DELAY_SIGN
+[assembly: AssemblyKeyFileAttribute(@"\\cp1pd2cdscvlt01\Public\StrongNamePublicKeys\35MSSharedLib1024.snk")]
+[assembly: AssemblyDelaySignAttribute(true)]
+#endif
