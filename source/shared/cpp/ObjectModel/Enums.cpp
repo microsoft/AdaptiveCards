9b15b7175c28a7c9cd0256ea6d0fcf1d1d049970
Alberto Medina Gutierrez <almedina@microsoft.com>
2018-02-05 14:04:58 -0800

    Removes Region from enums

--- source/shared/cpp/ObjectModel/Enums.cpp
+++ source/shared/cpp/ObjectModel/Enums.cpp
@@ -89,7 +89,6 @@ void InitializeAdaptiveCardSchemaKeyEnumMappings()
         { AdaptiveCardSchemaKey::NumberInput, "numberInput" },
         { AdaptiveCardSchemaKey::Padding, "padding" },
         { AdaptiveCardSchemaKey::Placeholder, "placeholder" },
-        // { AdaptiveCardSchemaKey::Region, "region" },
         { AdaptiveCardSchemaKey::Right, "right" },
         { AdaptiveCardSchemaKey::SelectAction, "selectAction" },
         { AdaptiveCardSchemaKey::Separator, "separator" },
