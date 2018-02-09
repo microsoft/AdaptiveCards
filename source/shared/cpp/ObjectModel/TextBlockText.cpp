095d2669345a730834878383189cc991512ae028
Alberto Medina Gutierrez <almedina@microsoft.com>
2018-02-09 14:16:03 -0800

    Removes duplicate TextSection class

--- source/shared/cpp/ObjectModel/TextBlockText.cpp
+++ source/shared/cpp/ObjectModel/TextBlockText.cpp
@@ -19,113 +19,6 @@
 
 using namespace AdaptiveCards;
 
-TextSection::TextSection() : m_text(""), m_format(TextSectionFormat::RegularString)
-{
-}
-
-TextSection::TextSection(std::string text, TextSectionFormat format) : m_text(text), m_format(format)
-{
-}
-
-std::string TextSection::GetText() const
-{
-    return m_text;
-}
-
-std::string TextSection::GetOriginalText() const
-{
-    return m_originalText;
-}
-
-TextSectionFormat TextSection::GetFormat() const
-{
-    return m_format;
-}
-
-int TextSection::GetDay() const
-{
-    int day{};
-    if (m_format == TextSectionFormat::DateShort || 
-        m_format == TextSectionFormat::DateLong || 
-        m_format == TextSectionFormat::DateCompact)
-    {
-        day = std::stoi(Split(m_text, '/').at(1));
-    }
-    return day;
-}
-
-int TextSection::GetMonth() const
-{
-    int minute{};
-    if (m_format == TextSectionFormat::DateShort ||
-        m_format == TextSectionFormat::DateLong ||
-        m_format == TextSectionFormat::DateCompact)
-    {
-        minute = std::stoi(Split(m_text, '/').at(0));
-    }
-    return minute;
-}
-
-int TextSection::GetYear() const
-{
-    int year{};
-    if (m_format == TextSectionFormat::DateShort ||
-        m_format == TextSectionFormat::DateLong ||
-        m_format == TextSectionFormat::DateCompact)
-    {
-        year = std::stoi(Split(m_text, '/').at(2));
-    }
-    return year;
-}
-
-int TextSection::GetSecond() const
-{
-    int second{};
-    if (m_format == TextSectionFormat::Time)
-    {
-        second = std::stoi(Split(m_text, ':').at(2));
-    }
-    return second;
-}
-
-int TextSection::GetMinute() const
-{
-    int minute{};
-    if (m_format == TextSectionFormat::Time)
-    {
-        minute = std::stoi(Split(m_text, ':').at(1));
-    }
-    return minute;
-}
-
-int TextSection::GetHour() const
-{
-    int hour{};
-    if (m_format == TextSectionFormat::Time)
-    {
-        hour = std::stoi(Split(m_text, ':').at(0));
-    }
-    return hour;
-}
-
-std::vector<std::string> TextSection::Split(const std::string& in, char delimiter) const
-{
-    std::vector<std::string> tokens;
-    size_t startIndex{};
-    size_t endIndex = in.find(delimiter, startIndex);
-
-    while (std::string::npos != endIndex)
-    {
-        std::string token = in.substr(startIndex, endIndex - startIndex);
-        tokens.emplace_back(token);
-        startIndex = endIndex + 1;
-        endIndex = in.find(delimiter, startIndex);
-    }
-
-    tokens.emplace_back(in.substr(startIndex));
-    return tokens;
-}
-
 TextBlockText::TextBlockText()
 {
 }
@@ -143,6 +36,11 @@ std::vector<TextSection> TextBlockText::GetString() const
 void TextBlockText::AddTextSection(std::string text, TextSectionFormat format)
 {
     m_fullString.emplace_back(TextSection(text, format));
+}
+
+void TextBlockText::AddTextSection(std::string text, std::string originalText, TextSectionFormat format)
+{
+    m_fullString.emplace_back(TextSection(text, originalText, format));
 }
 
 std::string TextBlockText::Concatenate()
@@ -307,15 +205,15 @@ void TextBlockText::ParseDateTime(std::string in)
                     {
                         // SHORT Style
                         case 'S':
-                            AddTextSection(plainDate, TextSectionFormat::DateShort);
+                            AddTextSection(plainDate, WstringToString(matches[0].str()), TextSectionFormat::DateShort);
                             break;
                         // LONG Style
                         case 'L':
-                            AddTextSection(plainDate, TextSectionFormat::DateLong);
+                            AddTextSection(plainDate, WstringToString(matches[0].str()), TextSectionFormat::DateLong);
                             break;
                         // COMPACT or DEFAULT Style
                         case 'C': default:
-                            AddTextSection(plainDate, TextSectionFormat::DateCompact);
+                            AddTextSection(plainDate, WstringToString(matches[0].str()), TextSectionFormat::DateCompact);
                             break;
                     }
                 }
@@ -323,7 +221,8 @@ void TextBlockText::ParseDateTime(std::string in)
                 {
                     AddTextSection(std::to_string(result.tm_hour) + ":" +
                         std::to_string(result.tm_min) + ":" +
-                        std::to_string(result.tm_sec),
+                        std::to_string(result.tm_sec), 
+                        WstringToString(matches[0].str()),
                         TextSectionFormat::Time);
                 }
             }
@@ -362,9 +261,9 @@ std::wstring TextBlockText::StringToWstring(const std::string& in) const
     std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> utfConverter;
     return utfConverter.from_bytes(in);
 #else
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> utfConverter;
+    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> utfConverter;
     return utfConverter.from_bytes(in);
-#endif
+#endif // _WIN32
 }
 
 std::string TextBlockText::WstringToString(const std::wstring& input) const
@@ -383,5 +282,5 @@ std::string TextBlockText::WstringToString(const std::wstring& input) const
 #else
     std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> utfConverter;
     return utfConverter.to_bytes(ToU16String(input));
-#endif
+#endif // _WIN32
 }
--- source/shared/cpp/ObjectModel/TextBlockText.h
+++ source/shared/cpp/ObjectModel/TextBlockText.h
@@ -1,42 +1,10 @@
 #pragma once
 #include <string>
 #include <vector>
+#include "TextSection.h"
 
 namespace AdaptiveCards
-{
-    enum class TextSectionFormat : uint16_t
-    {
-        RegularString = 0,
-        Time,
-        DateCompact,
-        DateShort,
-        DateLong
-    };
-
-    class TextSection
-    {
-    public:
-        TextSection(); 
-        TextSection(std::string text, TextSectionFormat format);
-
-        std::string GetText() const;
-        std::string GetOriginalText() const;
-        TextSectionFormat GetFormat() const;
-        int GetDay() const;
-        int GetMonth() const;
-        int GetYear() const;
-        int GetSecond() const;
-        int GetMinute() const;
-        int GetHour() const;
-
-    private:
-        std::vector<std::string> Split(const std::string& in, char delimiter) const;
-
-        std::string m_text;
-        std::string m_originalText;
-        TextSectionFormat m_format;
-    };
-    
+{  
     class TextBlockText
     {
     public:
@@ -44,6 +12,7 @@ namespace AdaptiveCards
         TextBlockText(std::string in);
         std::vector<TextSection> GetString() const;
         void AddTextSection(std::string text, TextSectionFormat format);
+        void AddTextSection(std::string text, std::string originalText, TextSectionFormat format);
         std::string Concatenate();
 
     private:
