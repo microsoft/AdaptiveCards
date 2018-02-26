bd24d44861ff7ecf6bf70035e78184ebbf28faa8
Alberto Medina Gutierrez <almedina@microsoft.com>
2018-02-26 11:22:59 -0800

    Fix objectmodel_wrap after squash

--- source/android/adaptivecards/src/main/java/com/microsoft/adaptivecards/objectmodel/AdaptiveCardObjectModelJNI.java
+++ source/android/adaptivecards/src/main/java/com/microsoft/adaptivecards/objectmodel/AdaptiveCardObjectModelJNI.java
@@ -316,6 +316,8 @@ public class AdaptiveCardObjectModelJNI {
   public final static native int ChoiceSetInput_GetChoiceSetStyle(long jarg1, ChoiceSetInput jarg1_);
   public final static native void ChoiceSetInput_SetChoiceSetStyle(long jarg1, ChoiceSetInput jarg1_, int jarg2);
   public final static native long ChoiceSetInput_GetChoices__SWIG_0(long jarg1, ChoiceSetInput jarg1_);
+  public final static native String ChoiceSetInput_GetValue(long jarg1, ChoiceSetInput jarg1_);
+  public final static native void ChoiceSetInput_SetValue(long jarg1, ChoiceSetInput jarg1_, String jarg2);
   public final static native long ChoiceSetInput_dynamic_cast(long jarg1, BaseCardElement jarg1_);
   public final static native void delete_ChoiceSetInput(long jarg1);
   public final static native long ChoiceSetInputParser_Deserialize(long jarg1, ChoiceSetInputParser jarg1_, long jarg2, ElementParserRegistration jarg2_, long jarg3, ActionParserRegistration jarg3_, long jarg4);
