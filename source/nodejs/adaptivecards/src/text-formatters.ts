da9fc1f5808bd54f97c4091ef6c427335a9b2599
David Claux <dclaux@users.noreply.github.com>
2018-02-27 12:01:09 -0800

    [TypeScript] Add lang support

--- source/nodejs/adaptivecards/src/text-formatters.ts
+++ source/nodejs/adaptivecards/src/text-formatters.ts
@@ -1,18 +1,18 @@
 abstract class AbstractTextFormatter {
     private _regularExpression: RegExp;
 
-    protected abstract internalFormat(matches: RegExpExecArray): string;
+    protected abstract internalFormat(lang: string, matches: RegExpExecArray): string;
 
     constructor(regularExpression: RegExp) {
         this._regularExpression = regularExpression;
     }
 
-    format(input: string): string {
+    format(lang: string, input: string): string {
         var matches;
         var result = input;
 
         while ((matches = this._regularExpression.exec(input)) != null) {
-            result = result.replace(matches[0], this.internalFormat(matches));
+            result = result.replace(matches[0], this.internalFormat(lang, matches));
         };
 
         return result;
@@ -20,12 +20,12 @@ abstract class AbstractTextFormatter {
 }
 
 class DateFormatter extends AbstractTextFormatter {
-    protected internalFormat(matches: RegExpExecArray): string {
+    protected internalFormat(lang: string, matches: RegExpExecArray): string {
         var date = new Date(Date.parse(matches[1]));
         var format = matches[2] != undefined ? matches[2].toLowerCase() : "compact";
 
         if (format != "compact") {
-            return date.toLocaleDateString(undefined, { day: "numeric", weekday: format, month: format, year: "numeric" });
+            return date.toLocaleDateString(lang, { day: "numeric", weekday: format, month: format, year: "numeric" });
         }
         else {
             return date.toLocaleDateString();
@@ -34,14 +34,14 @@ class DateFormatter extends AbstractTextFormatter {
 }
 
 class TimeFormatter extends AbstractTextFormatter {
-    protected internalFormat(matches: RegExpExecArray): string {
+    protected internalFormat(lang: string, matches: RegExpExecArray): string {
         var date = new Date(Date.parse(matches[1]));
 
-        return date.toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit' });
+        return date.toLocaleTimeString(lang, { hour: 'numeric', minute: '2-digit' });
     }
 }
 
-export function formatText(text: string): string {
+export function formatText(lang: string, text: string): string {
     const formatters: Array<AbstractTextFormatter> = [
         new DateFormatter(/\{{2}DATE\((\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:Z|(?:(?:-|\+)\d{2}:\d{2})))(?:, ?(COMPACT|LONG|SHORT))?\)\}{2}/g),
         new TimeFormatter(/\{{2}TIME\((\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:Z|(?:(?:-|\+)\d{2}:\d{2})))\)\}{2}/g)
@@ -50,7 +50,7 @@ export function formatText(text: string): string {
     var result = text;
 
     for (var i = 0; i < formatters.length; i++) {
-        result = formatters[i].format(result);
+        result = formatters[i].format(lang, result);
     }
 
     return result;
