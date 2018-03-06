2aa07a46da10e043e903c6f9ae0dfddbc3d3138a
Joseph Woo <Joseph.Woo@microsoft.com>
2018-03-06 12:18:35 -0800

    ios changes

---
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRParseWarning.h
@@ -0,0 +1,28 @@
+//
+//  ACRParseWarning
+//  ACRParseWarning.h
+//
+//  Copyright © 2018 Microsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, ACRParseWarningStatusCode)
+{
+    ACRUnknownElementType = 0,
+    ACRUnknownPropertyOnElement,
+    ACRUnknownEnumValue,
+    ACRNoRendererForType,
+    ACRInteractivityNotSupported,
+    ACRMaxActionsExceeded,
+    ACRAssetLoadFailed,
+    ACRUnsupportedSchemaVersion,
+};
+
+@interface ACRParseWarning:NSObject
+
+@property (readonly)ACRParseWarningStatusCode statusCode;
+@property (readonly)NSString *reason; 
+
+@end
+
