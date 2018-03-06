74901c445d1ee960f7dd1d5ade1359ac2fe3c4f9
Matt Hidinger <matt.hidinger@gmail.com>
2018-03-06 13:23:25 -0800

    fix resize

--- source/nodejs/adaptivecards-visualizer/src/monaco-loader.js
+++ source/nodejs/adaptivecards-visualizer/src/monaco-loader.js
@@ -30,6 +30,12 @@ function loadMonacoEditor(jsonSchema, callback) {
             }
         );
 
+        window.addEventListener('resize', function () {
+            monacoEditor.layout();
+        });
+
+        //monacoEditor.layout();
+
         callback();
     });
 }
