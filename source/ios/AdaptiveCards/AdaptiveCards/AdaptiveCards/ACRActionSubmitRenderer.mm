8895e564d9ad237ef415e82a4eb3987bcfcb578b
Alberto Medina Gutierrez <almedina@microsoft.com>
2018-04-03 16:54:28 -0700

    Add fix to previous commit

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRActionSubmitRenderer.mm
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRActionSubmitRenderer.mm
@@ -31,9 +31,9 @@
     std::shared_ptr<BaseActionElement> elem = [acoElem element];
     std::shared_ptr<SubmitAction> action = std::dynamic_pointer_cast<SubmitAction>(elem);
 
-    NSString *title = [NSString stringWithCString:action->GetTitle().c_str()
-                                        encoding:NSUTF8StringEncoding];
-    UIButton *button = [UIButton acr_renderButton:view title:title andHostConfig:config];
+    NSString *title = [NSString stringWithCString:action->GetTitle().c_str() encoding:NSUTF8StringEncoding];
+    NSString *iconUrl = [NSString stringWithCString:action->GetIconUrl().c_str() encoding:NSUTF8StringEncoding];
+    UIButton *button = [UIButton acr_renderButton:view title:title iconUrl:iconUrl andHostConfig:config];
 
     ACRAggregateTarget *target = [[ACRAggregateTarget alloc] initWithActionElement:acoElem rootView:(ACRView*)view];
 
