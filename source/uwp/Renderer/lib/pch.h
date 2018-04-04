9a63ccd0c968236794d5feb69f2771907b8fa4e6
Esteban Chavez <eschavez@microsoft.com>
2018-04-03 17:35:54 -0700

    Fixing macro

--- source/uwp/Renderer/lib/pch.h
+++ source/uwp/Renderer/lib/pch.h
@@ -8,7 +8,7 @@
 #else
 #define AdaptiveRuntime(cls) InspectableClass(RuntimeClass_AdaptiveCards_Rendering_Uwp_##cls, BaseTrust)
 #define AdaptiveRuntimeStatic(cls) InspectableClassStatic(RuntimeClass_AdaptiveCards_Rendering_Uwp_##cls, BaseTrust)
-#define AdaptiveRuntimeStringClass(cls) InspectableClass(L"AdaptiveCards.Rendering.Uwp.##cls", BaseTrust)
+#define AdaptiveRuntimeStringClass(cls) InspectableClass(L"AdaptiveCards.Rendering.Uwp." L#cls, BaseTrust)
 #define AdaptiveNamespaceStart namespace AdaptiveCards { namespace Rendering { namespace Uwp { 
 #define AdaptiveNamespaceRef AdaptiveCards::Rendering::Uwp
 #define AdaptiveNamespaceEnd }}}
