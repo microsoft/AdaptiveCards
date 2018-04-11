acb266fbff93c9e154dc13dd1b0d5f3c6db3cdc8
Joseph Woo <Joseph.Woo@microsoft.com>
2018-04-10 18:32:31 -0700

    Vertical alignment added

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRUILabel.mm
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRUILabel.mm
@@ -9,4 +9,17 @@
 
 @implementation ACRUILabel
 
+-(void)drawTextInRect:(CGRect)rect
+{
+    if (_isFactSetLabel) {
+        CGSize labelStringSize = [self.text boundingRectWithSize:CGSizeMake(self.frame.size.width, CGFLOAT_MAX)
+                                                         options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
+                                                      attributes:@{NSFontAttributeName:self.font}
+                                                         context:nil].size;
+        [super drawTextInRect:CGRectMake(0, 0, ceilf(self.frame.size.width),ceilf(labelStringSize.height))];
+    } else {
+        [super drawTextInRect:rect];
+    }
+}
+
 @end
