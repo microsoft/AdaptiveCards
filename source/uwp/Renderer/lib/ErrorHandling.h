67a41634079db53ed06a16b2529a0e21c9cb2ef5
Gilles Khouzam <gillesk@microsoft.com>
2018-04-20 17:31:28 -0700

    Prevent RETURN_IF_FAILED from executing the same statement twice on failure.

--- source/uwp/Renderer/lib/ErrorHandling.h
+++ source/uwp/Renderer/lib/ErrorHandling.h
@@ -2,6 +2,6 @@
 
 #include <sal.h>
 
-#define RETURN_IF_FAILED(hr)        do { if (FAILED(hr)) { return hr;}} while (0, 0)
+#define RETURN_IF_FAILED(statement) do { HRESULT hr = statement; if (FAILED(hr)) { return hr;}} while (0, 0)
 #define THROW_IF_FAILED(hr)         do { if (FAILED(hr)) { throw new std::exception();}} while (0, 0)
 #define CATCH_RETURN catch (...) {return E_FAIL;}
