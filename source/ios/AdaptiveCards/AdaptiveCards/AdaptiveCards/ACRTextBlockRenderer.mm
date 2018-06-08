7973ef6c1f76e87d93e984f85a59f61a687f8eb3
Joseph Woo <Joseph.Woo@microsoft.com>
2018-06-08 16:56:48 -0700

    Only render text on iOS if markdown is present
    * optimized use of html rendering

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRTextBlockRenderer.mm
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRTextBlockRenderer.mm
@@ -50,10 +50,20 @@
         NSDictionary* data = textMap[key];
         NSData *htmlData = data[@"html"];
         NSDictionary *options = data[@"options"];
+        NSDictionary *descriptor = data[@"descriptor"];
+        NSString *text = data[@"nonhtml"];
+
         // Initializing NSMutableAttributedString for HTML rendering is very slow
-        content = [[NSMutableAttributedString alloc] initWithData:htmlData options:options documentAttributes:nil error:nil];
-        // Drop newline char
-        [content deleteCharactersInRange:NSMakeRange([content length] -1, 1)];
+        if(htmlData) {
+            content = [[NSMutableAttributedString alloc] initWithData:htmlData options:options documentAttributes:nil error:nil];
+            // Drop newline char
+            [content deleteCharactersInRange:NSMakeRange([content length] -1, 1)];
+        } else {
+            // if html rendering is skipped, remove p tags from both ends (<p>, </p>)
+            content = [[NSMutableAttributedString alloc] initWithString:text attributes:descriptor];
+            [content deleteCharactersInRange:NSMakeRange(0, 3)];
+            [content deleteCharactersInRange:NSMakeRange([content length] -4, 4)];
+        }
 
         // Set paragraph style such as line break mode and alignment
         NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
