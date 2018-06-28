0b11ba8e41cac268705078e87104d0e0a68c6427
almedina-ms <35784165+almedina-ms@users.noreply.github.com>
2018-06-28 15:05:20 -0700

    [Android] Refocus when a show card changes visibility (#1654)
    * Refocus on show card
    
    * Set focus for cards instead of input fields
    
    * Fix activity retrieval
    
    * Fix formatting and null check

--- source/android/adaptivecards/src/main/java/io/adaptivecards/renderer/action/ActionElementRenderer.java
+++ source/android/adaptivecards/src/main/java/io/adaptivecards/renderer/action/ActionElementRenderer.java
@@ -1,12 +1,15 @@
 package io.adaptivecards.renderer.action;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.graphics.Bitmap;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.v4.app.FragmentManager;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -108,9 +111,32 @@ public class ActionElementRenderer implements IBaseActionElementRenderer
             m_hiddenCardsLayout = hiddenCardsLayout;
         }
 
+        private Activity getActivity(Context context)
+        {
+            while (context instanceof ContextWrapper)
+            {
+                if (context instanceof Activity)
+                {
+                    return (Activity)context;
+                }
+                context = ((ContextWrapper)context).getBaseContext();
+            }
+            return null;
+        }
+
         @Override
         public void onClick(View v)
         {
+            Activity hostingActivity = getActivity(v.getContext());
+            if(hostingActivity != null)
+            {
+                View currentFocusedView = hostingActivity.getCurrentFocus();
+                if (currentFocusedView != null)
+                {
+                    currentFocusedView.clearFocus();
+                }
+            }
+
             v.setPressed(m_invisibleCard.getVisibility() != View.VISIBLE);
             for(int i = 0; i < m_hiddenCardsLayout.getChildCount(); ++i)
             {
@@ -130,6 +156,7 @@ public class ActionElementRenderer implements IBaseActionElementRenderer
             if (m_invisibleCard.getVisibility() == View.VISIBLE)
             {
                 mainCardView.setPadding(padding, padding, padding, 0);
+                m_invisibleCard.requestFocus();
             }
             else
             {
