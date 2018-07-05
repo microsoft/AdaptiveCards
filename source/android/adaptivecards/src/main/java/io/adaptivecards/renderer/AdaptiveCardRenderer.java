efcd0f52efd76929518b136f0ccf766bc91dea93
Alberto Medina Gutierrez <almedina@microsoft.com>
2018-07-05 10:29:03 -0700

    Revert "[Android] Refocus when a show card changes visibility (#1654)"
    This reverts commit 0b11ba8e41cac268705078e87104d0e0a68c6427.

--- source/android/adaptivecards/src/main/java/io/adaptivecards/renderer/AdaptiveCardRenderer.java
+++ source/android/adaptivecards/src/main/java/io/adaptivecards/renderer/AdaptiveCardRenderer.java
@@ -116,8 +116,6 @@ public class AdaptiveCardRenderer
         LinearLayout rootLayout = new LinearLayout(context);
         rootLayout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         rootLayout.setOrientation(LinearLayout.VERTICAL);
-        rootLayout.setFocusable(true);
-        rootLayout.setFocusableInTouchMode(true);
 
         LinearLayout layout = new LinearLayout(context);
         layout.setTag(adaptiveCard);
