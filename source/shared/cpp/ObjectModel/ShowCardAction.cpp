1a5a7764b93bc9ae88124307da99654cb234bcf5
Ignacio Rodriguez <inz@fastmicroservices.com>
2018-07-13 09:30:37 +0700

    Revert "removed unused" since ES.84
    This reverts commit c403517575dd7c1b26e26ff4b8ff4af695988497.

--- source/shared/cpp/ObjectModel/ShowCardAction.cpp
+++ source/shared/cpp/ObjectModel/ShowCardAction.cpp
@@ -51,7 +51,7 @@ std::shared_ptr<BaseActionElement> ShowCardActionParser::Deserialize(
     auto parseResult = AdaptiveCard::Deserialize(json.get(propertyName, Json::Value()), std::numeric_limits<double>::max(), elementParserRegistration, actionParserRegistration);
 
     auto showCardWarnings = parseResult->GetWarnings();
-    warnings.insert(warnings.end(), showCardWarnings.begin(), showCardWarnings.end());
+    auto warningsEnd = warnings.insert(warnings.end(), showCardWarnings.begin(), showCardWarnings.end());
 
     showCardAction->SetCard(parseResult->GetAdaptiveCard());
 
@@ -76,5 +76,6 @@ void ShowCardAction::GetResourceInformation(std::vector<RemoteResourceInformatio
 {
     auto card = GetCard();
     auto showCardResources = card->GetResourceInformation();
-    resourceInfo.insert(resourceInfo.end(), showCardResources.begin(), showCardResources.end());
+    auto resourceInfoEnd = resourceInfo.insert(resourceInfo.end(), showCardResources.begin(), showCardResources.end());
+    return;
 }
