8abbd72dddf850a4ae86675dceeebbb229e43421
Paul Campbell (CMD) <paulcam@microsoft.com>
2018-07-24 17:06:03 -0700

    Implement factset

--- source/console/ConsoleApplication/ConsoleRenderer/ConsoleRender.cpp
+++ source/console/ConsoleApplication/ConsoleRenderer/ConsoleRender.cpp
@@ -5,6 +5,7 @@
 #include "Container.h"
 #include "Image.h"
 #include "ColumnSet.h"
+#include "FactSet.h"
 
 using namespace AdaptiveSharedNamespace;
 using namespace std::string_literals;
@@ -12,6 +13,10 @@ using namespace std::string_literals;
 size_t RenderElements(std::vector<std::shared_ptr<BaseCardElement>> elements, size_t columnWidth, size_t x, size_t y, std::vector<std::string>& consoleString);
 size_t RenderColumnSet(ColumnSet &set, size_t columnWidth, size_t x, size_t y, std::vector<std::string> &consoleString);
 size_t RenderColumn(Column &set, size_t columnWidth, size_t x, size_t y, std::vector<std::string> &consoleString);
+size_t RenderFactSet(FactSet &set, size_t FactWidth, size_t x, size_t y, std::vector<std::string> &consoleString);
+size_t RenderFact(Fact &set, size_t FactWidth, size_t x, size_t y, std::vector<std::string> &consoleString);
+
+#define ESC "\x1b"
 
 std::vector<std::string> ConvertToVector(const std::string &consoleString, size_t width)
 {
@@ -50,10 +55,10 @@ void EnsureRows(std::vector<std::string> &toEnsure, size_t cRows)
 	}
 }
 
-void WriteTextAtX(std::string &dest, std::string &src, size_t x)
+void WriteTextAtX(std::string &dest, std::string &src, size_t srcWidth, size_t x)
 {
 	// dest.resize(????
-	while (dest.size() < (x + src.size()))
+	while (dest.size() < (x + srcWidth))
 	{
 		dest.push_back(' ');
 	}
@@ -61,12 +66,67 @@ void WriteTextAtX(std::string &dest, std::string &src, size_t x)
 	dest.replace(x, src.size(), src);
 }
 
-void CompositeVector(std::vector<std::string> &vectorDest, std::vector<std::string> &vectorIn, size_t x, size_t y)
+void CompositeVector(std::vector<std::string> &vectorDest, std::vector<std::string> &vectorIn, size_t vectorInWidth, size_t x, size_t y)
 {
 	EnsureRows(vectorDest, y + vectorIn.size());
 	for (size_t currRow = y; currRow < (vectorIn.size() + y); currRow++)
 	{
-		WriteTextAtX(vectorDest[currRow], vectorIn[currRow - y], x);
+		WriteTextAtX(vectorDest[currRow], vectorIn[currRow - y], vectorInWidth, x);
+	}
+}
+
+#define BOLD_TEXT "\x1b[1m"
+void ApplyTextProperties(bool bold, ForegroundColor color, std::vector<std::string> &output)
+{
+	for (auto &row : output)
+	{
+		bool haveProperties = false;
+		if (bold)
+		{
+			row.insert(0, BOLD_TEXT);
+			haveProperties = true;
+		}
+		
+		std::string sgrCode;
+		switch (color)
+		{
+		case ForegroundColor::Accent:
+		{
+			sgrCode = "34";
+			break;
+		}
+		case ForegroundColor::Good:
+		{
+			sgrCode = "32";
+			break;
+		}
+		case ForegroundColor::Warning:
+		{
+			sgrCode = "33";
+			break;
+		}
+		case ForegroundColor::Attention:
+		{
+			sgrCode = "31";
+			break;
+		}
+		case ForegroundColor::Light:
+		{
+			sgrCode = "37";
+			break;
+		}
+		}
+
+		if (!sgrCode.empty())
+		{ 
+			std::string sgrSequence = ESC "[" + sgrCode + "m";
+			row.insert(0, sgrSequence);
+		}
+
+		if (haveProperties)
+		{
+			row.append(ESC "[0m");
+		}
 	}
 }
 
@@ -80,9 +140,11 @@ size_t RenderElement(std::shared_ptr<BaseCardElement> element,  size_t columnWid
 		break;
 	case CardElementType::TextBlock:
 	{
-		auto textBlock = ConvertToVector(std::dynamic_pointer_cast<TextBlock>(element)->GetText(), columnWidth);
-		CompositeVector(consoleString, textBlock, x, y);
-		rowsRendered = textBlock.size();
+		auto textBlock = std::dynamic_pointer_cast<TextBlock>(element);
+		auto textOut = ConvertToVector(textBlock->GetText(), columnWidth);
+		//ApplyTextProperties(textBlock->GetTextWeight() == TextWeight::Bolder, textBlock->GetTextColor(), textOut);
+		CompositeVector(consoleString, textOut, columnWidth, x, y);
+		rowsRendered = textOut.size();
 		break;
 	}
 	case CardElementType::Image:
@@ -91,7 +153,7 @@ size_t RenderElement(std::shared_ptr<BaseCardElement> element,  size_t columnWid
 		std::string imageText = imageElem->GetAltText();
 		imageText.append(" <" + imageElem->GetUrl() + ">");
 		auto textBlock = ConvertToVector(imageText, columnWidth);
-		CompositeVector(consoleString, textBlock, x, y);
+		CompositeVector(consoleString, textBlock, columnWidth, x, y);
 		rowsRendered = textBlock.size();
 		break;
 	}
@@ -101,12 +163,24 @@ size_t RenderElement(std::shared_ptr<BaseCardElement> element,  size_t columnWid
 		break;
 	}
 	case CardElementType::ColumnSet:
+	{
 		rowsRendered = RenderColumnSet(*std::dynamic_pointer_cast<ColumnSet>(element), columnWidth, x, y, consoleString);
 		break;
+	}
+	case CardElementType::Fact:
+	{
+		rowsRendered = RenderFact(*std::dynamic_pointer_cast<Fact>(element), columnWidth, x, y, consoleString);
+		break;
+	}
+	case CardElementType::FactSet:
+	{
+		rowsRendered = RenderFactSet(*std::dynamic_pointer_cast<FactSet>(element), columnWidth, x, y, consoleString);
+		break;
+	}
 	default:
 	{
 		auto unimplText = ConvertToVector("Unimplemented: " + AdaptiveSharedNamespace::CardElementTypeToString(element->GetElementType()), columnWidth);
-		CompositeVector(consoleString, unimplText, x, y);
+		CompositeVector(consoleString, unimplText, columnWidth, x, y);
 		rowsRendered = unimplText.size();
 		break;
 	}
@@ -135,8 +209,29 @@ size_t RenderColumn(Column &column, size_t columnWidth, size_t x, size_t y, std:
 	}
 
 	// composite column into place
-	CompositeVector(consoleString, columnText, x, y);
+	CompositeVector(consoleString, columnText, columnWidth, x, y);
 	return columnY;
+}
+
+size_t RenderFact(Fact &set, size_t columnWidth, size_t x, size_t y, std::vector<std::string> &consoleString)
+{
+	const size_t itemWidth = (columnWidth / 2) - 1;
+	auto titleBlock = ConvertToVector(set.GetTitle(), itemWidth);
+	auto valueBlock = ConvertToVector(set.GetValue(), itemWidth);
+
+	CompositeVector(consoleString, titleBlock, itemWidth, x, y);
+	CompositeVector(consoleString, valueBlock, itemWidth, x + itemWidth + 1, y);
+	return std::max(titleBlock.size(), valueBlock.size());
+}
+
+size_t RenderFactSet(FactSet &set, size_t columnWidth, size_t x, size_t y, std::vector<std::string> &consoleString)
+{
+	size_t cRows = 0;
+	for (auto fact : set.GetFacts())
+	{
+		cRows += RenderFact(*fact, columnWidth, x, y+cRows, consoleString);
+	}
+	return cRows;
 }
 
 size_t RenderColumnSet(ColumnSet &set, size_t columnWidth, size_t x, size_t y, std::vector<std::string> &consoleString)
@@ -153,7 +248,6 @@ size_t RenderColumnSet(ColumnSet &set, size_t columnWidth, size_t x, size_t y, s
 	return maxY;
 }
 
-#define ESC "\x1b"
 void VTSaveCursor()
 {
 	printf(ESC "7");
