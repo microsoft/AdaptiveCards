fd6eb53645b9f1cc252153b3887cdbf321de10cb
Joseph Woo <joswo@microsoft.com>
2018-07-25 18:23:54 -0700

    added AVFoundation

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRMediaTarget.h
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRMediaTarget.h
@@ -6,7 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
 #import "ACRView.h"
 #import "ACOMediaEvent.h"
 #import "ACRLongPressGestureRecognizerEventHandler.h"
