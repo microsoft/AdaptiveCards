5c2886e9b966892ea7785d606606f95fffd90cef
Tuan Anh Pham <tuanh118@gmail.com>
2018-08-01 13:30:48 -0700

    allowed double for pixel width and added a check for positive width value

--- source/dotnet/Library/AdaptiveCards.Rendering.Wpf/AdaptiveColumnSetRenderer.cs
+++ source/dotnet/Library/AdaptiveCards.Rendering.Wpf/AdaptiveColumnSetRenderer.cs
@@ -53,10 +53,10 @@ namespace AdaptiveCards.Rendering.Wpf
                     uiColumnSet.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
                 else
                 {
-                    if (double.TryParse(width, out double val))
+                    if (double.TryParse(width, out double val) && val >= 0)
                         // Weighted proportion (number only)
                         uiColumnSet.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(val, GridUnitType.Star) });
-                    else if (width.EndsWith("px") && int.TryParse(width.Substring(0, width.Length-2), out int pxVal))
+                    else if (width.EndsWith("px") && double.TryParse(width.Substring(0, width.Length-2), out double pxVal) && pxVal >= 0)
                         // Exact pixel (number followed by "px")
                         uiColumnSet.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(pxVal, GridUnitType.Pixel) });
                     else
