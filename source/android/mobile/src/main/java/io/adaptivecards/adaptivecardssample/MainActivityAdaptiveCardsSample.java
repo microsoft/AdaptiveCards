ce48bfc1edc5708e874834247bca88ba2e8cfa86
Alberto Medina Gutierrez <almedina@microsoft.com>
2018-08-06 16:35:27 -0700

    Moves IOnlineMediaLoader example outside of package

--- source/android/mobile/src/main/java/io/adaptivecards/adaptivecardssample/MainActivityAdaptiveCardsSample.java
+++ source/android/mobile/src/main/java/io/adaptivecards/adaptivecardssample/MainActivityAdaptiveCardsSample.java
@@ -1,5 +1,7 @@
 package io.adaptivecards.adaptivecardssample;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentActivity;
 import android.content.Context;
@@ -8,6 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -18,7 +21,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-import io.adaptivecards.renderer.OnlineMediaLoader;
+import io.adaptivecards.renderer.IMediaDownloadListener;
+import io.adaptivecards.renderer.IOnlineMediaLoader;
 import io.adaptivecards.renderer.RenderedAdaptiveCard;
 import io.adaptivecards.renderer.BaseCardElementRenderer;
 import io.adaptivecards.renderer.actionhandler.ICardActionHandler;
@@ -30,18 +34,137 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.annotation.TargetApi;
+import android.media.MediaDataSource;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivityAdaptiveCardsSample extends FragmentActivity
     implements ICardActionHandler
 {
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public class OnlineMediaLoader extends IOnlineMediaLoader
+    {
+
+        Runnable m_downloadMediaRunnable = new Runnable() {
+            @Override
+            public void run() {
+                try
+                {
+                    URL url = new URL("https://adaptivecardsblob.blob.core.windows.net/assets/AdaptiveCardsOverviewVideo.mp4");
+                    InputStream inputStream = url.openStream();
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    int read = 0;
+                    while(read != -1)
+                    {
+                        read = inputStream.read();
+                        byteArrayOutputStream.write(read);
+                    }
+                    inputStream.close();
+
+                    byteArrayOutputStream.flush();
+                    m_mediaBuffer = byteArrayOutputStream.toByteArray();
+
+                    byteArrayOutputStream.close();
+
+                    m_downloadListener.onMediaDownloaded();
+                }
+                catch (MalformedURLException urlException)
+                {
+                    urlException.printStackTrace();
+                    Log.d("Error on video download", urlException.toString());
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                    Log.d("Error on video download", e.toString());
+                }
+                finally
+                {
+                    m_isDownloading = false;
+                }
+            }
+        };
+
+        public OnlineMediaLoader()
+        {
+            m_isDownloading = false;
+        }
+
+        public void loadMedia(IMediaDownloadListener mediaDownloadListener)
+        {
+            if(m_isDownloading)
+            {
+                return;
+            }
+
+            m_downloadListener = mediaDownloadListener;
+            Thread mediaDownloadThread = new Thread(m_downloadMediaRunnable);
+            mediaDownloadThread.start();
+            m_isDownloading = true;
+        }
+
+        @Override
+        public int readAt(long position, byte[] buffer, int offset, int size) throws IOException
+        {
+            synchronized (m_mediaBuffer)
+            {
+                int length = m_mediaBuffer.length;
+                if(position >= length)
+                {
+                    return -1;
+                }
+
+                if(position + size > length)
+                {
+                    size -= (position + size) - length;
+                    // size = length - position;
+                }
+                System.arraycopy(m_mediaBuffer, (int)position, buffer, offset, size);
+                return size;
+            }
+        }
+
+        @Override
+        public long getSize() throws IOException
+        {
+            synchronized (m_mediaBuffer)
+            {
+                return m_mediaBuffer.length;
+            }
+        }
+
+        @Override
+        public void close() throws IOException
+        {
+        }
+
+        private volatile byte[] m_mediaBuffer;
+        private volatile boolean m_isDownloading;
+
+        private volatile IMediaDownloadListener m_downloadListener;
+    }
 
     // Used to load the 'adaptivecards-native-lib' library on application startup.
     static {
