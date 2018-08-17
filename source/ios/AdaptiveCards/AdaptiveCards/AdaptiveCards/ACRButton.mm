a93d8289e088fd02309d53bd442ea3bc2f586c5b
Joseph Woo <joswo@microsoft.com>
2018-08-17 10:51:46 -0700

    add iconsize suppoert

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRButton.mm
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRButton.mm
@@ -16,18 +16,26 @@
 
 + (void)setImageView:(UIImage*)image inButton:(UIButton*)button withConfig:(ACOHostConfig *)config
 {
-    // Format the image so it fits in the button and is placed where it must be placed
-    CGSize contentSize = [button.titleLabel intrinsicContentSize];
-    float imageHeight = contentSize.height;
+    float imageHeight = 0.0f;
     IconPlacement iconPlacement = [config getHostConfig]->actions.iconPlacement;
+    CGSize contentSize = [button.titleLabel intrinsicContentSize];
+
+    if(iconPlacement == AdaptiveCards::IconPlacement::AboveTitle && config.allActionsHaveIcons) {
+        //apply explicit image size when the below condition is met
+        imageHeight = [config getHostConfig]->actions.iconSize;
+        // TODO: if height is zero, create warning
+    } else {
+        // Format the image so it fits in the button and is placed where it must be placed
+        imageHeight = contentSize.height;
+    }
 
     CGFloat widthToHeightRatio = 0.0f;
-    if(image){
-        if(image.size.height > 0) {
-            widthToHeightRatio = image.size.width / image.size.height;
-        }
+    if(image && image.size.height > 0) {
+        widthToHeightRatio = image.size.width / image.size.height;
     }
+
     CGSize imageSize = CGSizeMake(imageHeight * widthToHeightRatio, imageHeight);
+
     ACRUIImageView *imageView = [[ACRUIImageView alloc] initWithFrame:CGRectMake(0, 0, imageSize.width, imageSize.height)];
     imageView.translatesAutoresizingMaskIntoConstraints = NO;
     imageView.image = image;
