c20753ce24b46801a0a5cb9c6252061f186ea081
Carlos Zamora <cazamor@microsoft.com>
2018-08-23 14:26:49 -0700

    finished Fact tests

--- source/shared/cpp/AdaptiveCardsSharedModel/AdaptiveCardsSharedModelUnitTest/FactUnitTest.cpp
+++ source/shared/cpp/AdaptiveCardsSharedModel/AdaptiveCardsSharedModelUnitTest/FactUnitTest.cpp
@@ -1,32 +1,46 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "Fact.h"
-#include <Windows.h>
-#include <StrSafe.h>
+#include "ActionParserRegistration.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace AdaptiveCards;
-using namespace std;
 
-namespace AdaptiveCardsSharedModelUnitTest {
-	TEST_CLASS(FactTest) {
-	public:
-		TEST_METHOD(DefineFromEmptyConstructor) {
-			Fact emptyFact;
-			Assert::IsTrue(emptyFact.GetTitle().empty());
+namespace AdaptiveCardsSharedModelUnitTest 
+{
+    TEST_CLASS(FactTest) 
+    {
+    public:
+        TEST_METHOD(DefineFromEmptyConstructor) 
+        {
+            Fact emptyFact;
+            Assert::IsTrue(emptyFact.GetTitle().empty());
 
-			//Define & test title
-			string testTitle = "1 Example Title!";
-			emptyFact.SetTitle(testTitle);
-			Assert::AreEqual(testTitle, emptyFact.GetTitle());
+            // Define & test title
+            std::string testTitle = "1 Example Title!";
+            emptyFact.SetTitle(testTitle);
+            Assert::AreEqual(testTitle, emptyFact.GetTitle());
 
-			//Define & test value
-			string testValue = "1 Example Value!";
-			emptyFact.SetValue(testValue);
-			Assert::AreEqual(testValue, emptyFact.GetValue());
-		}
+            // Define & test value
+            std::string testValue = "1 Example Value!";
+            emptyFact.SetValue(testValue);
+            Assert::AreEqual(testValue, emptyFact.GetValue());
 
-		TEST_METHOD() {	
-		}
-	};
+            std::string json_data = emptyFact.Serialize();
+            Assert::IsTrue(json_data == "{\"title\":\"1 Example Title!\",\"value\":\"1 Example Value!\"}\n");
+
+            std::shared_ptr<ElementParserRegistration> elementParserRegistration;
+            elementParserRegistration.reset(new ElementParserRegistration());
+
+            std::shared_ptr<ActionParserRegistration> actionParserRegistration;
+            actionParserRegistration.reset(new ActionParserRegistration());
+
+            std::vector<std::shared_ptr<AdaptiveCardParseWarning>> warnings;
+
+            auto parsedFact = Fact::DeserializeFromString(elementParserRegistration, actionParserRegistration, warnings, json_data);
+            
+            Assert::AreEqual(emptyFact.GetTitle(), parsedFact->GetTitle());
+            Assert::AreEqual(emptyFact.GetValue(), parsedFact->GetValue());
+        }
+    };
 }
