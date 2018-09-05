90a2e5281e8b814caca174361b90d8ce98262140
Alberto Medina Gutierrez <almedina@microsoft.com>
2018-09-05 15:03:00 -0700

    Fix size converter

--- source/dotnet/Library/AdaptiveCards/StringSizeWithUnitConverter.cs
+++ source/dotnet/Library/AdaptiveCards/StringSizeWithUnitConverter.cs
@@ -11,16 +11,16 @@ namespace AdaptiveCards
 
         readonly JsonSerializer defaultSerializer = new JsonSerializer();
 
-        private readonly bool isExplicitSize;
+        private readonly bool isHeight;
 
         public StringSizeWithUnitConverter()
         {
-            isExplicitSize = true;
+            isHeight = true;
         }
 
-        public StringSizeWithUnitConverter(bool isExplicit)
+        public StringSizeWithUnitConverter(bool height)
         {
-            isExplicitSize = isExplicit;
+            isHeight = height;
         }
 
         public override bool CanConvert(Type objectType)
@@ -32,12 +32,17 @@ namespace AdaptiveCards
         // Checks if the size string was never intended to be explicit size
         private bool isPixelHeight(String size)
         {
-            foreach(AdaptiveHeight height in (AdaptiveHeight[])Enum.GetValues(typeof(AdaptiveHeight)))
-            {
-                if(String.Compare(size, height.ToString(), true) == 0)
-                    return false;
-            }
-            return true;
+            return !((String.Compare(size, AdaptiveHeightType.Auto.ToString(), true) == 0) 
+                || (String.Compare(size, AdaptiveHeightType.Stretch.ToString(), true) == 0));
+        }
+
+        private object GenerateReturnValue(uint px)
+        {
+            if (!isHeight)
+                return px;
+            if (px != 0)
+                return new AdaptiveHeight(px);
+            return null;
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
@@ -46,63 +51,76 @@ namespace AdaptiveCards
             {
                 var dimension = defaultSerializer.Deserialize(reader) as string;
 
-                if (isExplicitSize)
+                // Only the height can have auto or stretch sizes
+                if(isHeight)
                 {
                     // If the received string is not auto or stretch
-                    if (isPixelHeight(dimension))
+                    if (!isPixelHeight(dimension))
                     {
-                        if (dimension.Length < 3)
-                        {
-                            Warnings.Add(new AdaptiveWarning(-1,
-                                $"The Value \"{reader.Value}\" for field \"{reader.Path}\" was not specified as a proper dimension in the format (\\d+(.\\d+)?px), it will be ignored."));
-                            return 0U;
-                        }
-
-                        var unit = dimension.Substring(dimension.Length - 2);
-                        if (String.Compare(unit, "px", false) != 0)
-                        {
-                            Warnings.Add(new AdaptiveWarning(-1,
-                                $"The Value \"{unit}\" was not specified as a proper unit(px), it will be ignored."));
-                            return 0U;
-                        }
-
-                        if (double.TryParse(dimension.Substring(0, dimension.Length - 2), NumberStyles.AllowDecimalPoint, null, out double dimensionInPix))
-                        {
-                            // we need check this because AllowDecimalPoint flags allows TryParse to accept number in .\d+ format
-                            if (dimension[0] == '.')
-                            {
-                                Warnings.Add(new AdaptiveWarning(-1,
-                                    $"The Value \"{reader.Value}\" for field \"{reader.Path}\" was not specified as a proper dimension in the format (\\d+(.\\d+)?px), it will be ignored."));
-                            }
-                            return (uint)dimensionInPix;
-                        }
-                        else
-                        {
-                            Warnings.Add(new AdaptiveWarning(-1,
-                                $"The Value \"{reader.Value}\" for field \"{reader.Path}\" was not specified as a proper dimension in the format (\\d+(.\\d+)?px), it will be ignored."));
-                            return 0U;
-                        }
+                        var heightType = dimension.ToLower();
+                        if (String.Compare(dimension, AdaptiveHeightType.Stretch.ToString(), true) == 0)
+                            return new AdaptiveHeight(AdaptiveHeightType.Stretch);
+                        return new AdaptiveHeight(AdaptiveHeightType.Auto);
                     }
-                    return 0U;
+                }
+
+                if (dimension.Length < 3)
+                {
+                    Warnings.Add(new AdaptiveWarning(-1,
+                        $"The Value \"{reader.Value}\" for field \"{reader.Path}\" was not specified as a proper dimension in the format (\\d+(.\\d+)?px), it will be ignored."));
+                    return GenerateReturnValue(0U);
+                }
+
+                var unit = dimension.Substring(dimension.Length - 2);
+                if (String.Compare(unit, "px", false) != 0)
+                {
+                    Warnings.Add(new AdaptiveWarning(-1,
+                        $"The Value \"{unit}\" was not specified as a proper unit(px), it will be ignored."));
+                    return GenerateReturnValue(0U);
+                }
+
+                if (double.TryParse(dimension.Substring(0, dimension.Length - 2), NumberStyles.AllowDecimalPoint, null, out double dimensionInPix))
+                {
+                    // we need check this because AllowDecimalPoint flags allows TryParse to accept number in .\d+ format
+                    if (dimension[0] == '.')
+                    {
+                        Warnings.Add(new AdaptiveWarning(-1,
+                            $"The Value \"{reader.Value}\" for field \"{reader.Path}\" was not specified as a proper dimension in the format (\\d+(.\\d+)?px), it will be ignored."));
+                    }
+                    return GenerateReturnValue((uint)dimensionInPix);
                 }
                 else
                 {
-                    var heightType = dimension.ToLower();
-                    if (String.Compare(dimension, AdaptiveHeight.Stretch.ToString(), true) == 0)
-                        return AdaptiveHeight.Stretch;
-                    return AdaptiveHeight.Auto;
+                    Warnings.Add(new AdaptiveWarning(-1,
+                        $"The Value \"{reader.Value}\" for field \"{reader.Path}\" was not specified as a proper dimension in the format (\\d+(.\\d+)?px), it will be ignored."));
+                    return GenerateReturnValue(0U);
                 }
             }
 
             Warnings.Add(new AdaptiveWarning(-1, $"The Value \"{reader.Value}\" for field \"{reader.Path}\" of type \"{reader.TokenType}\" was not proper type, it will be ignored."));
-            return 0U;
+            return GenerateReturnValue(0U);
         }
 
         public override bool CanWrite { get { return true; } }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            writer.WriteValue(value.ToString() + "px");
+            if (!isHeight)
+            {
+                writer.WriteValue(value.ToString() + "px");
+            }
+            else
+            {
+                AdaptiveHeight adaptiveHeight = (AdaptiveHeight)value;
+                if (adaptiveHeight.IsPixel())
+                {
+                    writer.WriteValue(adaptiveHeight.Unit.ToString() + "px");
+                }
+                else if (isHeight)
+                {
+                    writer.WriteValue(adaptiveHeight.HeightType.ToString().ToLower());
+                }
+            }
         }
     }
 }
