f12644368e1304c80f3ab14b446c026782cdaeb8
nesalang <nesalang@gmail.com>
2018-09-06 10:36:33 -0700

    standardized versioning scheme

---
+++ ACAndroidSetVersion.ps1
@@ -0,0 +1,12 @@
+﻿#$version=Get-Content .\custom.props | Select-String -Pattern "NugetPackVersion"
+#$version.ToString().Split('>')[1].Split('<')[0]
+
+$adaptivecardsversion = $env:XES_PACKAGEVERSIONNUMBER
+foreach($pathComponent in $env:BUILD_SOURCEBRANCH.ToString().Split('\'))
+{
+    if($pathComponent -eq "release") {
+        $adaptivecardsversion = "($adaptivecardsversion + $env:BUILD_BUILDNUMBER)"
+    }
+}
+ 
+Write-Host "##vso[task.setvariable variable=AdaptiveCardsVersion]$adaptivecardsversion"
--- ACSetVersion.ps1
+++
@@ -1,4 +0,0 @@
-﻿$version=Get-Content .\custom.props | Select-String -Pattern "NugetPackVersion"
-$adaptivecardsversion=$version.ToString().Split('>')[1].Split('<')[0]
-$adaptivecardsversion= "$($adaptivecardsversion + '-' + $(Get-Date -Format FileDateTime))"
-Write-Host "##vso[task.setvariable variable=AdaptiveCardsVersion]$adaptivecardsversion"
