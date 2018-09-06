bf1e4f12f3812bca6e505d89451bd9b114f4cbfe
nesalang <nesalang@gmail.com>
2018-09-06 11:36:44 -0700

    updated script

--- ACAndroidSetVersion.ps1
+++ ACAndroidSetVersion.ps1
@@ -1,12 +1,12 @@
 ﻿#$version=Get-Content .\custom.props | Select-String -Pattern "NugetPackVersion"
 #$version.ToString().Split('>')[1].Split('<')[0]
 
-$adaptivecardsversion = $env:XES_PACKAGEVERSIONNUMBER
+$adaptivecardsversion = "($env:XES_PACKAGEVERSIONNUMBER-20$env:BUILD_BUILDNUMBER)"
 foreach($pathComponent in $env:BUILD_SOURCEBRANCH.ToString().Split('\'))
 {
     if($pathComponent -eq "release") {
-        $adaptivecardsversion = "($adaptivecardsversion + $env:BUILD_BUILDNUMBER)"
+        $adaptivecardsversion = $env:XES_PACKAGEVERSIONNUMBER
     }
 }
- 
+
 Write-Host "##vso[task.setvariable variable=AdaptiveCardsVersion]$adaptivecardsversion"
