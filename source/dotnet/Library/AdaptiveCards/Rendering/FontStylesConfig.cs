c14449a24fc437838b9396d177e02052b5ead09b
Carlos Zamora <cazamor@microsoft.com>
2018-09-28 14:43:08 -0700

    Added parse implementation of FontStyles in HostConfig and AdaptiveCard

---
+++ source/dotnet/Library/AdaptiveCards/Rendering/FontStylesConfig.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace AdaptiveCards.Rendering
+{
+    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
+    public class FontStylesConfig
+    {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public FontStyleConfig Default { get; set; } = new FontStyleConfig();
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public FontStyleConfig Display { get; set; } = new FontStyleConfig();
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public FontStyleConfig Monospace { get; set; } = new FontStyleConfig();
+    }
+}
