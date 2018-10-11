fb547e3f8172e9eeff724ace00a3e9e7af362913
nesalang <nesalang@gmail.com>
2018-10-10 17:28:44 -0700

    updated with missing header

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import json
 import base64
-import urllib
+import requests
 import os
 
 personalAccessToken = ':7rvtdw3ihxbfo262z7h24asnkr42znukpzzccy7fl2eoinfdqrdq';
@@ -14,19 +14,20 @@ headers['Authorization'] = b'Basic ' + base64.b64encode(bytes(personalAccessToke
 #url = 'https://artifacts.dev.azure.com/microsoft/_apis/drop/manifests/os' + buildId + 'api-version=2.0'
 url = 'https://artifacts.dev.azure.com/microsoft/_apis/drop/manifests/os%2f20181005.1%2f12228408?api-version=2.0'
 
-r = requests.get(url)
-urlToArtifacts = (r.json()[0]['blob']['url'])
-
-outputBuff = [];
-with open('AdaptiveCards.podspec', 'r') as f:
-    lines = f.readlines();
-    for line in lines:
-        if 'spec.source' in line:
-            outputBuff.append("  spec.source = { :http => %s\n :type => ''zip''}\n" % urlToArtifacts) 
-        elif ':type => ''zip ''' not in line:
-            outputBuff.append(line)
-
-with open('AdaptiveCards.podspec', 'w') as f:
-    f.writelines(outputBuff)
-
-print(urlToArtifacts)
+r = requests.get(url, headers=headers)
+print(r)
+#urlToArtifacts = (r.json()[0]['blob']['url'])
+#
+#outputBuff = [];
+#with open('AdaptiveCards.podspec', 'r') as f:
+#    lines = f.readlines();
+#    for line in lines:
+#        if 'spec.source' in line:
+#            outputBuff.append("  spec.source = { :http => %s\n :type => ''zip''}\n" % urlToArtifacts) 
+#        elif ':type => ''zip ''' not in line:
+#            outputBuff.append(line)
+#
+#with open('AdaptiveCards.podspec', 'w') as f:
+#    f.writelines(outputBuff)
+#
+#print(urlToArtifacts)
