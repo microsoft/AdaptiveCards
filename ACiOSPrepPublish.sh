cee12daae5628ff581950dab3feca2d00ef7a14b
nesalang <nesalang@gmail.com>
2018-10-11 15:24:15 -0700

    updated .netrc

--- ACiOSPrepPublish.sh
+++ ACiOSPrepPublish.sh
@@ -4,13 +4,13 @@ user='login joswo@microsoft.com'
 ACPAT='7rvtdw3ihxbfo262z7h24asnkr42znukpzzccy7fl2eoinfdqrdq'
 
 machineRepo='machine microsoft.visualstudio.com'
-userRepo='login joswo'
+userRepo='login joswo@microsoft.com'
 ACRepoPAT='lp4qhhgrruxy5k6sznwdmlb3sk3lyivodox5x4vntnje5vdtaca'
 
 echo -e "\n${machine}" >> ~/.netrc
 echo "  ${user}" >> ~/.netrc
 echo "  password ${ACPAT}" >> ~/.netrc
 
-echo "${machineRepo}" >> ~/.netrc
+echo -e "\n${machineRepo}" >> ~/.netrc
 echo "  ${userRepo}" >> ~/.netrc
 echo "  password ${ACRepoPAT}" >> ~/.netrc
