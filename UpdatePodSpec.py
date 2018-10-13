0e0985949cb791a2fabef14944b65a52b82780b5
nesalang <nesalang@gmail.com>
2018-10-12 19:17:31 -0700

    updated buildId

--- UpdatePodSpec.py
+++ UpdatePodSpec.py
@@ -28,7 +28,7 @@ buildNumber = os.environ['BUILD_BUILDNUMBER']
 acversion = acversion + '-' + buildNumber;
 
 buildId = '%2f' + buildNumber + '%2f' +  os.environ['BUILD_BUILDID'] + '%2f' + '5'
-
+print(buildId)
 url = 'https://artifacts.dev.azure.com/microsoft/_apis/drop/manifests/os' + buildId + 'api-version=2.0'
 
 urlToArtifacts = ''
