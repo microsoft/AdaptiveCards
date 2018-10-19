86babcc87ebfca3a7d5e8eb4ba8a9804cf89608a
Matt Hidinger <matt.hidinger@gmail.com>
2018-10-19 10:08:00 -0700

    not working

--- source/nodejs/OLD designer stuff/containers/default-container.ts
+++
@@ -1,8 +0,0 @@
-import { HostContainer } from "./host-container";
-
-export class DefaultContainer extends HostContainer {
-    public renderTo(hostElement: HTMLElement) {
-        hostElement.classList.add("card-frame");
-        hostElement.appendChild(this.cardHost);
-    }
-}
