f9585e276004b4ec8ca9ed1986307d543960a5a2
Alberto Medina Gutierrez <almedina@microsoft.com>
2018-10-29 09:43:01 -0700

    Add data URI support

--- source/android/adaptivecards/src/main/java/io/adaptivecards/renderer/GenericImageLoaderAsync.java
+++ source/android/adaptivecards/src/main/java/io/adaptivecards/renderer/GenericImageLoaderAsync.java
@@ -35,6 +35,22 @@ public abstract class GenericImageLoaderAsync extends AsyncTask<String, Void, Ht
     // Main function to try different ways to load an image
     HttpRequestResult<Bitmap> loadImage(String path, Context context)
     {
+        // The syntax of data URIs as in RFX 2397 is  data:[<media type>][;base64],<data>
+        path = path.trim();
+        if( path.startsWith("data:") )
+        {
+            try {
+                //
+                String base64Data = path.split(",")[1];
+                byte[] decodedString = Base64.decode(base64Data, Base64.DEFAULT);
+                Bitmap b = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                return new HttpRequestResult<>(b);
+            } catch (Exception e)
+            {
+                return new HttpRequestResult<>(e);
+            }
+        }
+
         try
         {
             // Let's try to see if we got the image in the card as a base64 encoded string
