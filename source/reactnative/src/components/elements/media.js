fb146db2e6f350b9d288c0029fd17ebeaa145807
Ibrahim Sulaiman <ibrahim.sulaiman@jet.com>
2018-12-20 17:31:10 +0530

    Handled the props and schema for the media in adaptive cards

--- source/reactnative/src/components/elements/media.js
+++ source/reactnative/src/components/elements/media.js
@@ -1,21 +1,67 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Image } from "react-native";
 import * as Constants from "../../utils/constants";
 import Video from "react-native-video";
+import ElementWrapper from './element-wrapper'
 
 export class Media extends React.Component {
+    constructor(props) {
+        super(props);
+        this.payload = props.json;
+        this.sources = this.getMediaSources(this.payload.sources)
+        this.state = {
+            currentSourceIndex: 0,
+            onLoad: false
+        }
+    }
+
+    getMediaSources = (sources) => {
+        sources.forEach(source => {
+            this.addUriAttribute(source)
+        })
+        return sources
+    }
+
+    addUriAttribute = (source) => {
+        if (source.url) {
+            source.uri = source.url
+        }
+        return source;
+    }
+
+    videoError = () => {
+        if (this.state.currentSourceIndex < (this.sources.length - 1)) {
+            this.setState({
+                currentSourceIndex: this.state.currentSourceIndex + 1
+            })
+        }
+    }
+
+    videoLoadSuccess = () => {
+        this.setState({
+            onLoad: true
+        })
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Video
-                    source={{
-                        uri:
-                            "https://adaptivecardsblob.blob.core.windows.net/assets/AdaptiveCardsOverviewVideo.mp4"
-                    }}
-                    controls={true}
-                    style={styles.nativeVideoControls}
-                />
-            </View>
+            <ElementWrapper json={this.payload}>
+                <View style={styles.container}>
+                    <TouchableOpacity style={styles.fullScreen}>
+                        {this.sources.length > 0 && <Video
+                            source={this.sources[this.state.currentSourceIndex]}
+                            fullscreen={true}
+                            controls={true}
+                            id={this.payload.id ? this.payload.id : "video"}
+                            paused={true}
+                            onError={this.videoError}
+                            onLoad={this.videoLoadSuccess}
+                            style={styles.nativeVideoControls}
+                        />}
+                    </TouchableOpacity>
+                    {(!this.state.onLoad && this.payload.poster) && <Image source={{ uri: this.payload.poster }} style={styles.nativeVideoControls}></Image>}
+                </View >
+            </ElementWrapper>
         );
     }
 }
@@ -26,6 +72,13 @@ const styles = StyleSheet.create({
         justifyContent: Constants.CenterString,
         alignItems: Constants.CenterString,
         backgroundColor: Constants.blackColor
+    },
+    fullScreen: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
     },
     nativeVideoControls: {
         height: 300,
