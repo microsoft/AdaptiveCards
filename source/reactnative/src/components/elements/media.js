cd8fd5106d9b232596bad0ed52f5fbfa970d84a2
SivaPrasad <sivaprasad.gottapu@letsmobility.com>
2018-12-27 12:01:29 +0530

    Added Media control support for Android.

--- source/reactnative/src/components/elements/media.js
+++ source/reactnative/src/components/elements/media.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { View, StyleSheet, TouchableOpacity, Image, Platform} from "react-native";
+import MediaControls, { PLAYER_STATES } from 'react-native-media-controls';
 import * as Constants from "../../utils/constants";
 import Video from "react-native-video";
 import ElementWrapper from './element-wrapper'
 
 export class Media extends React.Component {
+    videoPlayer;
     constructor(props) {
         super(props);
         this.payload = props.json;
         this.sources = this.getMediaSources(this.payload.sources)
         this.state = {
             currentSourceIndex: 0,
-            onLoad: false
+            onLoad: false,
+            currentTime: 0,
+            duration: 0,
+            isFullScreen: false,
+            isLoading: true,
+            paused: false,
+            playerState: PLAYER_STATES.PLAYING,
+
         }
     }
 
@@ -43,24 +52,84 @@ export class Media extends React.Component {
         })
     }
 
+    onSeek = seek => {
+        this.videoPlayer.seek(seek);
+    };
+
+    onPaused = playerState => {
+        this.setState({
+            paused: !this.state.paused,
+            playerState,
+        });
+    };
+
+    onReplay = () => {
+        this.setState({ playerState: PLAYER_STATES.PLAYING });
+        this.videoPlayer.seek(0);
+    };
+
+    onProgress = data => {
+        const { isLoading, playerState } = this.state;
+        // Video Player will continue progress even if the video already ended
+        if (!isLoading && playerState !== PLAYER_STATES.ENDED) {
+            this.setState({ currentTime: data.currentTime });
+        }
+    };
+
+    onLoad = data => this.setState({ duration: data.duration, isLoading: false });
+
+    onLoadStart = data => this.setState({ isLoading: true });
+
+    onEnd = () => this.setState({ playerState: PLAYER_STATES.ENDED });
+
+    exitFullScreen = () => { };
+
+    enterFullScreen = () => { };
+
+    onFullScreen = () => { };
+
+    onSeeking = currentTime => this.setState({ currentTime });
+
     render() {
         return (
             <ElementWrapper json={this.payload}>
                 <View style={styles.container}>
-                    <TouchableOpacity style={styles.fullScreen}>
-                        {this.sources.length > 0 && <Video
-                            source={this.sources[this.state.currentSourceIndex]}
+                    {/* <TouchableOpacity style={styles.fullScreen}> */}
+                         <Video
+                            source={this.sources[this.state.currentSourceIndex ]}
                             fullscreen={true}
                             controls={true}
                             id={this.payload.id ? this.payload.id : "video"}
-                            paused={true}
+                            // paused={true}
                             onError={this.videoError}
-                            onLoad={this.videoLoadSuccess}
+                            // onLoad={this.videoLoadSuccess}
+                            onEnd={this.onEnd}
+                            onLoad={this.onLoad}
+                            onLoadStart={this.onLoadStart}
+                            onProgress={this.onProgress}
+                            paused={this.state.paused}
+                            ref={videoPlayer => (this.videoPlayer = videoPlayer)}
                             style={styles.nativeVideoControls}
-                        />}
-                    </TouchableOpacity>
-                    {(!this.state.onLoad && this.payload.poster) && <Image source={{ uri: this.payload.poster }} style={styles.nativeVideoControls}></Image>}
-                </View >
+                        />
+                    {Platform.OS !== 'ios' && (
+                        <MediaControls
+                            duration={this.state.duration}
+                            isLoading={this.state.isLoading}
+                            mainColor="orange"
+                            onFullScreen={this.onFullScreen}
+                            onPaused={this.onPaused}
+                            onReplay={this.onReplay}
+                            onSeek={this.onSeek}
+                            onSeeking={this.onSeeking}
+                            isFullScreen ={true}
+                            playerState={this.state.playerState}
+                            progress={this.state.currentTime}
+                        />
+                    )}
+
+                    {/* </TouchableOpacity> */}
+                    {(this.state.isLoading && this.payload.poster) && <Image source={{ uri: this.payload.poster }} style={styles.nativeVideoControls}></Image>}
+                </View>
             </ElementWrapper>
         );
     }
