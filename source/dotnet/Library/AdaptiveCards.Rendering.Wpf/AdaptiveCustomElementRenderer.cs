37e5a7b601608ab4a4986d7126435b48b73a87f1
Andrew Leader <aleader@microsoft.com>
2019-01-15 14:47:53 -0800

    Remove unused code

--- source/dotnet/Library/AdaptiveCards.Rendering.Wpf/AdaptiveCustomElementRenderer.cs
+++ source/dotnet/Library/AdaptiveCards.Rendering.Wpf/AdaptiveCustomElementRenderer.cs
@@ -23,46 +23,6 @@ namespace AdaptiveCards.Rendering.Wpf
                 }
 
                 return context.Render(customElement.ResolvedElement);
-
-                if (context.ElementDefinitions != null && context.ElementDefinitions.TryGetValue(customElement.Type, out object defObj))
-                {
-                    string definition;
-                    if (defObj is string url)
-                    {
-                        HttpClient c = new HttpClient();
-                        var task = c.GetStringAsync(url);
-                        task.Wait();
-                        definition = task.Result;
-                    }
-                    else
-                    {
-                        definition = defObj.ToString();
-                    }
-
-                    // Super simple data binding
-                    foreach (var prop in customElement.AdditionalProperties)
-                    {
-                        definition = definition.Replace("{" + prop.Key + "}", prop.Value.ToString());
-                    }
-
-                    AdaptiveElementDefinition parsedDefinition = JsonConvert.DeserializeObject<AdaptiveElementDefinition>(definition, new JsonSerializerSettings
-                    {
-                        Converters = { new StrictIntConverter() }
-                    });
-
-                    if (parsedDefinition.Element != null)
-                    {
-                        return context.Render(parsedDefinition.Element);
-                    }
-                    else
-                    {
-                        context.Warnings.Add(new AdaptiveWarning(1, "Element definition was empty for " + customElement.Type));
-                        return null;
-                    }
-                }
-
-                context.Warnings.Add(new AdaptiveWarning(1, "No element definition found for " + customElement.Type));
-                return null;
             }
             catch (Exception ex)
             {
