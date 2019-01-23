937b7865bb920a651d78d49deed737c2803b3cc8
Andrew Bares <anbare@microsoft.com>
2019-01-22 18:38:14 -0800

    Fix null literals between strings

--- source/dotnet/Library/JsonTransformLanguage/JsonStringTransformer.cs
+++ source/dotnet/Library/JsonTransformLanguage/JsonStringTransformer.cs
@@ -67,6 +67,7 @@ namespace JsonTransformLanguage
                 }
                 else
                 {
+                    answer += remainingBindingExpression;
                     break;
                 }
 
