d1dfcb5956116f52c003d50d6e494119153b2ed0
David Claux <dclaux@users.noreply.github.com>
2019-02-26 14:20:45 -0800

    Manual merge from master

--- source/nodejs/adaptivecards-designer/src/miscellaneous.ts
+++ source/nodejs/adaptivecards-designer/src/miscellaneous.ts
@@ -64,7 +64,6 @@ export var defaultHostConfig = new Adaptive.HostConfig(
     {
         preExpandSingleShowCardAction: true,
         supportsInteractivity: true,
-        fontFamily: "Segoe UI",
         spacing: {
             small: 10,
             default: 20,
@@ -80,22 +79,42 @@ export var defaultHostConfig = new Adaptive.HostConfig(
         textAlign: {
             right: "right"
         },
-        fontSizes: {
-            small: 12,
-            default: 14,
-            medium: 17,
-            large: 21,
-            extraLarge: 26
-        },
-        fontWeights: {
-            lighter: 200,
-            default: 400,
-            bolder: 600
-        },
         imageSizes: {
             small: 40,
             medium: 80,
             large: 160
+        },
+        fontStyles: {
+            default: {
+                fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+                fontSizes: {
+                    small: 12,
+                    default: 14,
+                    medium: 17,
+                    large: 21,
+                    extraLarge: 26
+                },
+                fontWeights: {
+                    lighter: 200,
+                    default: 400,
+                    bolder: 600
+                }
+            },
+            monospace: {
+                fontFamily: "'Courier New', Courier, monospace",
+                fontSizes: {
+                    small: 12,
+                    default: 14,
+                    medium: 17,
+                    large: 21,
+                    extraLarge: 26
+                },
+                fontWeights: {
+                    lighter: 200,
+                    default: 400,
+                    bolder: 600
+                }
+            }
         },
         containerStyles: {
             default: {
@@ -129,6 +148,138 @@ export var defaultHostConfig = new Adaptive.HostConfig(
                     default: {
                         default: "#333333",
                         subtle: "#EE333333"
+                    },
+                    accent: {
+                        default: "#2E89FC",
+                        subtle: "#882E89FC"
+                    },
+                    attention: {
+                        default: "#cc3300",
+                        subtle: "#DDcc3300"
+                    },
+                    good: {
+                        default: "#54a254",
+                        subtle: "#DD54a254"
+                    },
+                    warning: {
+                        default: "#e69500",
+                        subtle: "#DDe69500"
+                    }
+                }
+            },
+            accent: {
+                backgroundColor: "#C7DEF9",
+                foregroundColors: {
+                    default: {
+                        default: "#333333",
+                        subtle: "#EE333333"
+                    },
+                    dark: {
+                        default: "#000000",
+                        subtle: "#66000000"
+                    },
+                    light: {
+                        default: "#FFFFFF",
+                        subtle: "#33000000"
+                    },
+                    accent: {
+                        default: "#2E89FC",
+                        subtle: "#882E89FC"
+                    },
+                    attention: {
+                        default: "#cc3300",
+                        subtle: "#DDcc3300"
+                    },
+                    good: {
+                        default: "#54a254",
+                        subtle: "#DD54a254"
+                    },
+                    warning: {
+                        default: "#e69500",
+                        subtle: "#DDe69500"
+                    }
+                }
+            },
+            good: {
+                backgroundColor: "#CCFFCC",
+                foregroundColors: {
+                    default: {
+                        default: "#333333",
+                        subtle: "#EE333333"
+                    },
+                    dark: {
+                        default: "#000000",
+                        subtle: "#66000000"
+                    },
+                    light: {
+                        default: "#FFFFFF",
+                        subtle: "#33000000"
+                    },
+                    accent: {
+                        default: "#2E89FC",
+                        subtle: "#882E89FC"
+                    },
+                    attention: {
+                        default: "#cc3300",
+                        subtle: "#DDcc3300"
+                    },
+                    good: {
+                        default: "#54a254",
+                        subtle: "#DD54a254"
+                    },
+                    warning: {
+                        default: "#e69500",
+                        subtle: "#DDe69500"
+                    }
+                }
+            },
+            attention: {
+                backgroundColor: "#FFC5B2",
+                foregroundColors: {
+                    default: {
+                        default: "#333333",
+                        subtle: "#EE333333"
+                    },
+                    dark: {
+                        default: "#000000",
+                        subtle: "#66000000"
+                    },
+                    light: {
+                        default: "#FFFFFF",
+                        subtle: "#33000000"
+                    },
+                    accent: {
+                        default: "#2E89FC",
+                        subtle: "#882E89FC"
+                    },
+                    attention: {
+                        default: "#cc3300",
+                        subtle: "#DDcc3300"
+                    },
+                    good: {
+                        default: "#54a254",
+                        subtle: "#DD54a254"
+                    },
+                    warning: {
+                        default: "#e69500",
+                        subtle: "#DDe69500"
+                    }
+                }
+            },
+            warning: {
+                backgroundColor: "#FFE2B2",
+                foregroundColors: {
+                    default: {
+                        default: "#333333",
+                        subtle: "#EE333333"
+                    },
+                    dark: {
+                        default: "#000000",
+                        subtle: "#66000000"
+                    },
+                    light: {
+                        default: "#FFFFFF",
+                        subtle: "#33000000"
                     },
                     accent: {
                         default: "#2E89FC",
