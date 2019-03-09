416a7c896644337dc52a6125b4359b87fc8ba385
joswo@microsoft.com <nesalang@gmail.com>
2019-03-08 16:56:44 -0800

    updates for ios

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACOBaseCardElement.h
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACOBaseCardElement.h
@@ -30,6 +30,7 @@ typedef NS_ENUM(NSInteger, ACRCardElementType) {
     ACRImageSet,
     ACRMedia,
     ACRNumberInput,
+    ACRRichTextBlock,
     ACRTextBlock,
     ACRTextInput,
     ACRTimeInput,
