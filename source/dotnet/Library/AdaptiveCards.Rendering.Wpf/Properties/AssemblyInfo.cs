45734f8b4ac5196f35de5d7215aa9ae9f259fc30
Paul Campbell (CMD) <paulcam@microsoft.com>
2019-03-29 14:44:39 -0700

    Update remaining snks

--- source/dotnet/Library/AdaptiveCards.Rendering.Wpf/Properties/AssemblyInfo.cs
+++ source/dotnet/Library/AdaptiveCards.Rendering.Wpf/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Windows;
 
 #if DELAY_SIGN
-[assembly: AssemblyKeyFileAttribute(@"\\cp1pd2cdscvlt01\Public\StrongNamePublicKeys\35MSSharedLib1024.snk")]
+[assembly: AssemblyKeyFileAttribute(@"\\cp1l4omt\StrongNamePublicKeys\35MSSharedLib1024.snk")]
 [assembly: AssemblyDelaySignAttribute(true)]
 #endif
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
