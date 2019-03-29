d73da387e6865313f2d5b4b680b37b8358d62445
joswo@microsoft.com <nesalang@gmail.com>
2019-03-29 10:34:12 -0700

    dev complete

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRActionSetRenderer.h
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRActionSetRenderer.h
@@ -6,8 +6,9 @@
 //
 
 #import "ACRBaseActionElementRenderer.h"
+#import "ACRIBaseCardElementRenderer.h"
 
-@interface ACRActionSetRenderer:NSObject<ACRIBaseActionSetRenderer>
+@interface ACRActionSetRenderer:NSObject<ACRIBaseActionSetRenderer, ACRIBaseCardElementRenderer>
 
 + (ACRActionSetRenderer *)getInstance;
 
