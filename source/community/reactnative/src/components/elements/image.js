f9cca36a6ac828a40107ab0fd807b50f8d3b2f85
balaji.rajendran <balaji.rajendran@imaginea.com>
2019-04-12 16:57:39 +0530

    Removed irrelevant validation calls.

--- source/community/reactnative/src/components/elements/image.js
+++ source/community/reactnative/src/components/elements/image.js
@@ -240,48 +240,26 @@ export class Img extends React.Component {
 
 	onPageLayoutHandler = (event) => {
 		const { width: layoutWidth } = event.nativeEvent.layout;
-		if (!Utils.isNullOrEmpty(this.url)) {
-			if (Utils.validateUrl(this.url)) {
-				//This function is implemented to determine the actual dimensions of the component.
-					Image.getSize(this.url, (width, height) => {
+		//This function is implemented to determine the actual dimensions of the component.
+		Image.getSize(this.url, (width, height) => {
 
-						/**
-						 * Calculating the width to height ratio based on layoutWidth and actual image width.
-						 */
-						const w2hratio = layoutWidth / width
-						/**
-							* The image-width and height are set in state to 
-							* re-render the element once we get the dimensions of the image.
-							*/
+			/**
+			 * Calculating the width to height ratio based on layoutWidth and actual image width.
+			 */
+			const w2hratio = layoutWidth / width
+			/**
+				* The image-width and height are set in state to 
+				* re-render the element once we get the dimensions of the image.
+				*/
 
-						/**
-						 * If the payload contains "fromImageset" i.e(if the image is rendered via ImageSet),
-						 * the height and width of the image is set to maxImageHeight for sizes "auto" and "stretch"
-						 */
+			/**
+			 * If the payload contains "fromImageset" i.e(if the image is rendered via ImageSet),
+			 * the height and width of the image is set to maxImageHeight for sizes "auto" and "stretch"
+			 */
 
-						if (this.payload.fromImageSet == true &&
-							(this.payload.size === Constants.Auto ||
-								this.payload.size === Constants.AlignStretch)) {
-							this.setState({
-								imageWidth: this.hostConfig.imageSet.maxImageHeight,
-								imageHeight: this.hostConfig.imageSet.maxImageHeight,
-							});
-							this.width = this.payload.width || this.hostConfig.imageSet.maxImageHeight;
-							this.height = this.payload.height || this.hostConfig.imageSet.maxImageHeight;
-						}
-						else {
-							this.setState({
-								imageWidth: layoutWidth,
-								imageHeight: w2hratio * height,
-							});
-							this.width = this.payload.width || layoutWidth;
-							this.height = this.payload.height || w2hratio * height;
-						}
-
-					}, (error) => {
-						console.log(`Couldn't get the image size: ${error.message}`);
-					});
-			} else {
+			if (this.payload.fromImageSet == true &&
+				(this.payload.size === Constants.Auto ||
+					this.payload.size === Constants.AlignStretch)) {
 				this.setState({
 					imageWidth: this.hostConfig.imageSet.maxImageHeight,
 					imageHeight: this.hostConfig.imageSet.maxImageHeight,
@@ -289,7 +267,18 @@ export class Img extends React.Component {
 				this.width = this.payload.width || this.hostConfig.imageSet.maxImageHeight;
 				this.height = this.payload.height || this.hostConfig.imageSet.maxImageHeight;
 			}
-		}
+			else {
+				this.setState({
+					imageWidth: layoutWidth,
+					imageHeight: w2hratio * height,
+				});
+				this.width = this.payload.width || layoutWidth;
+				this.height = this.payload.height || w2hratio * height;
+			}
+
+		}, (error) => {
+			console.log(`Couldn't get the image size: ${error.message}`);
+		});
 	}
 
 	render() {
