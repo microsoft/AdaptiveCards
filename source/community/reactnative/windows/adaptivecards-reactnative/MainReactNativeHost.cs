2809708dcc7251b502be84965f757be0dfd12636
balaji.rajendran <balaji.rajendran@imaginea.com>
2019-04-16 10:37:35 +0530

    Added windows project.

---
+++ source/community/reactnative/windows/adaptivecards-reactnative/MainReactNativeHost.cs
@@ -0,0 +1,29 @@
+﻿using ReactNative;
+using ReactNative.Modules.Core;
+using ReactNative.Shell;
+using System.Collections.Generic;
+
+namespace adaptivecards-reactnative
+{
+    class MainReactNativeHost : ReactNativeHost
+    {
+        public override string MainComponentName => "adaptivecards-reactnative";
+
+#if !BUNDLE || DEBUG
+        public override bool UseDeveloperSupport => true;
+#else
+        public override bool UseDeveloperSupport => false;
+#endif
+
+        protected override string JavaScriptMainModuleName => "index";
+
+#if BUNDLE
+        protected override string JavaScriptBundleFile => "ms-appx:///ReactAssets/index.windows.bundle";
+#endif
+
+        protected override List<IReactPackage> Packages => new List<IReactPackage>
+        {
+            new MainReactPackage(),
+        };
+    }
+}
