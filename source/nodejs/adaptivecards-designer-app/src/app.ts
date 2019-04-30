2803fc6fa6256c3a9b29f1225c2abc7d90b86069
Matt Hidinger <matt.hidinger@gmail.com>
2019-04-30 13:28:56 -0700

    basic app running and saving to blob storage

--- source/nodejs/adaptivecards-designer-app/src/app.ts
+++ source/nodejs/adaptivecards-designer-app/src/app.ts
@@ -3,80 +3,201 @@
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import * as markdownit from "markdown-it";
 import * as ACDesigner from "adaptivecards-designer";
+import * as Azure from "@azure/storage-blob";
+import { AZURE_STORAGE_ACCOUNT_NAME, AZURE_STORAGE_SAS } from "./secrets";
+import { generateUuid } from "ms-rest-js";
+import "adaptivecards-designer/dist/adaptivecards-designer.css";
 import "./app.css";
 
-// TODO: should this be necessary?
-import "adaptivecards-designer/lib/adaptivecards-designer.css";
-import "adaptivecards-designer/node_modules/adaptivecards-controls/lib/adaptivecards-controls.css";
+const card = `{
+    "type": "AdaptiveCard",
+    "body": [
+        {
+            "type": "TextBlock",
+            "size": "Large",
+            "text": "Please complete this referral request",
+            "wrap": true
+        },
+        {
+            "type": "TextBlock",
+            "color": "Accent",
+            "text": "Patient Name"
+        },
+        {
+            "type": "Input.Text",
+            "id": "PatientName",
+            "placeholder": "Placeholder text"
+        },
+        {
+            "type": "Input.Toggle",
+            "id": "HasInsurance",
+            "title": "Does this patient have insurance?",
+            "value": "false"
+        },
+        {
+            "type": "TextBlock",
+            "color": "Accent",
+            "text": "Referring To"
+        },
+        {
+            "type": "Input.Text",
+            "id": "DoctorName",
+            "placeholder": "Placeholder text"
+        },
+        {
+            "type": "TextBlock",
+            "color": "Accent",
+            "text": "Referral Need"
+        },
+        {
+            "type": "Input.Text",
+            "id": "ReferralNeed",
+            "placeholder": "Placeholder text"
+        },
+        {
+            "type": "TextBlock",
+            "color": "Accent",
+            "text": "Are you the referring physician?"
+        },
+        {
+            "type": "Input.ChoiceSet",
+            "id": "Rating",
+            "placeholder": "Placeholder text",
+            "choices": [
+                {
+                    "title": "Yes",
+                    "value": "yes"
+                },
+                {
+                    "title": "No",
+                    "value": "no"
+                }
+            ],
+            "style": "expanded"
+        },
+        {
+            "type": "TextBlock",
+            "color": "Accent",
+            "text": "Requested Date for Appointment"
+        },
+        {
+            "type": "Input.Date",
+            "id": "RequestedDate"
+        },
+        {
+            "type": "TextBlock",
+            "color": "Accent",
+            "text": "Any notes we should add for the doctor?"
+        },
+        {
+            "type": "Input.Text",
+            "id": "Notes",
+            "placeholder": "Let us know!",
+            "isMultiline": true
+        }
+    ],
+    "actions": [
+        {
+            "type": "Action.Submit",
+            "id": "Submit",
+            "title": "Submit Referral"
+        }
+    ],
+    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+    "version": "1.0"
+}`;
 
-window.onload = function() {
-    // Comment to disable preview features (data binding)
-    ACDesigner.GlobalSettings.previewFeaturesEnabled = true;
+const pipeline = Azure.StorageURL.newPipeline(new Azure.AnonymousCredential());
+const serviceURL = new Azure.ServiceURL(`https://${AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net?${AZURE_STORAGE_SAS}`, pipeline);
+let designer: ACDesigner.CardDesigner;
+
+
+
+function saveCard() {
+
+	// List containers
+
+	// Create a container
+	//   const containerName = `newcontainer${new Date().getTime()}`;
+	//   const containerURL = Azure.ContainerURL.fromServiceURL(serviceURL, containerName);
+	//   await containerURL.create(Azure.Aborter.None);
+
+	const containerURL = Azure.ContainerURL.fromServiceURL(serviceURL, "newcontainer1556579201786");
+
+	const blobURL = Azure.BlockBlobURL.fromContainerURL(containerURL, `${generateUuid()}.json`);
+
+	let card = designer.getCard();
+
+	var blob = new Blob([JSON.stringify(card)]);
+
+	Azure.uploadBrowserDataToBlockBlob(Azure.Aborter.timeout(3000), blob, blobURL, {blobHTTPHeaders: { blobContentType: "application/json"}})
+	alert("Saved!");
+
+}
+
+window.onload = async function () {
+
+	let marker;
+	do {
+		const listContainersResponse = await serviceURL.listContainersSegment(
+			Azure.Aborter.None,
+			marker
+		);
+
+		marker = listContainersResponse.nextMarker;
+		for (const container of listContainersResponse.containerItems) {
+			console.log(`Container: ${container.name}`);
+		}
+	} while (marker);
+
+	// Comment to disable preview features (data binding)
+	ACDesigner.GlobalSettings.previewFeaturesEnabled = false;
 
 	ACDesigner.CardDesigner.onProcessMarkdown = (text: string, result: { didProcess: boolean, outputHtml: string }) => {
 		result.outputHtml = new markdownit().render(text);
 		result.didProcess = true;
 	}
 
-	if (!ACDesigner.SettingsManager.isLocalStorageAvailable) {
-		console.log("Local storage is not available.");
-	}
-
 	let hostContainers: Array<ACDesigner.HostContainer> = [];
 	hostContainers.push(new ACDesigner.WebChatContainer("Bot Framework WebChat", "containers/webchat-container.css"));
-	hostContainers.push(new ACDesigner.CortanaContainer("Cortana Skills", "containers/cortana-container.css"));
-	hostContainers.push(new ACDesigner.OutlookContainer("Outlook Actionable Messages", "containers/outlook-container.css"));
-	hostContainers.push(new ACDesigner.TimelineContainer("Windows Timeline", "containers/timeline-container.css"));
-	hostContainers.push(new ACDesigner.DarkTeamsContainer("Microsoft Teams - Dark", "containers/teams-container-dark.css"));
-	hostContainers.push(new ACDesigner.LightTeamsContainer("Microsoft Teams - Light", "containers/teams-container-light.css"));
-	hostContainers.push(new ACDesigner.BotFrameworkContainer("Bot Framework Other Channels (Image render)", "containers/bf-image-container.css"));
-	hostContainers.push(new ACDesigner.ToastContainer("Windows Notifications (Preview)", "containers/toast-container.css"));
 
-    let designer = new ACDesigner.CardDesigner(hostContainers);
-    designer.sampleCatalogueUrl = window.location.origin + "/sample-catalogue.json";
-    designer.attachTo(document.getElementById("designerRootHost"));
-    
-    /* Uncomment to test a custom palette item example
-    let exampleSnippet = new ACDesigner.SnippetPaletteItem("Custom", "Example");
-    exampleSnippet.snippet = {
-        type: "ColumnSet",
-        columns: [
-            {
-                width: "auto",
-                items: [
-                    {
-                        type: "Image",
-                        size: "Small",
-                        style: "Person",
-                        url: "https://pbs.twimg.com/profile_images/3647943215/d7f12830b3c17a5a9e4afcc370e3a37e_400x400.jpeg"
-                    }
-                ]
-            },
-            {
-                width: "stretch",
-                items: [
-                    {
-                        type: "TextBlock",
-                        text: "John Doe",
-                        weight: "Bolder",
-                        wrap: true
-                    },
-                    {
-                        type: "TextBlock",
-                        spacing: "None",
-                        text: "Additional information",
-                        wrap: true
-                    }
-                ]
-            }
-        ]
-    };
+	designer = new ACDesigner.CardDesigner(hostContainers);
+	designer.sampleCatalogueUrl = window.location.origin + "/sample-catalogue.json";
+	designer.toolbar.getElementById(ACDesigner.CardDesigner.ToolbarCommands.HostAppPicker).isVisible = false;
+	//designer.toolbar.getElementById(ACDesigner.CardDesigner.ToolbarCommands.CopyJSON).isVisible = false;
 
-    designer.customPaletteItems = [ exampleSnippet ];
-    */
+	let saveButton = new ACDesigner.ToolbarButton(
+        "saveButton",
+        "Save",
+        null,
+        (sender) => { saveCard(); });
+    saveButton.separator = true;
+    designer.toolbar.insertElementAfter(saveButton, ACDesigner.CardDesigner.ToolbarCommands.HostAppPicker);
+ 
+
+
+	designer.attachTo(document.getElementById("designerRootHost"));
+
+	designer.jsonEditorToolbox.collapse();
+
+	designer.setCard(JSON.parse(card));
+
+
+	/* Uncomment to test a custom palette item example */
+	let exampleSnippet = new ACDesigner.SnippetPaletteItem("Data Fields", "Patient Referrer");
+	exampleSnippet.snippet = {
+		"type": "Input.Text"
+	};
+
+	designer.customPaletteItems = [exampleSnippet];
+
 
 	designer.monacoModuleLoaded(monaco);
 
+}
+
+function loadSampleData()
+{
 	let sampleData = {
 		firstName: "John",
 		lastName: "Doe",
@@ -101,7 +222,7 @@ window.onload = function() {
 			}
 		]
 	};
-	
-	designer.dataStructure = ACDesigner.FieldDefinition.create(sampleData);
-	designer.sampleData = sampleData;
+
+	// designer.dataStructure = ACDesigner.FieldDefinition.create(sampleData);
+	// designer.sampleData = sampleData;
 }
