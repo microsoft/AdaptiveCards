e2bcbd5373d1be3c8c33c451c4c70ebbc1361bb6
almedina-ms <35784165+almedina-ms@users.noreply.github.com>
2019-05-17 11:35:05 -0700

    [WPF] Fix lighter weight support (#2923)
    * Fix font being a bunch of fonts concatenated by strings
    
    * Fix default behaviour

--- source/dotnet/Library/AdaptiveCards.Rendering.Wpf/RendererUtil.cs
+++ source/dotnet/Library/AdaptiveCards.Rendering.Wpf/RendererUtil.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,33 @@ namespace AdaptiveCards.Rendering.Wpf
 {
     static class RendererUtil
     {
+        /// <summary>
+        /// Checks if any of the fonts to be used for the TexBlock is installed, and if so, returns it
+        /// </summary>
+        /// <param name="fontList">Font list to check if any exist</param>
+        /// <returns>First font in list that is installed, otherwise, default system font</returns>
+        public static string GetFontFamilyFromList(string fontList)
+        {
+            var installedFonts = new InstalledFontCollection();
+            string[] fontFamilies = fontList.Split(',');
+
+            for (int i = 0; i < fontFamilies.Length; ++i)
+            {
+                string fontFamily = fontFamilies[i].Trim('\'');
+
+                foreach (var installedFontFamily in installedFonts.Families)
+                {
+                    if (installedFontFamily.Name == fontFamily)
+                    {
+                        return fontFamily;
+                    }
+                }
+            }
+
+            // If no valid font was found in list, return the system default font
+            return SystemFonts.MessageFontFamily.ToString();
+        }
+
         public static void ApplyVerticalContentAlignment(FrameworkElement uiElement, AdaptiveCollectionElement element)
         {
             switch (element.VerticalContentAlignment)
