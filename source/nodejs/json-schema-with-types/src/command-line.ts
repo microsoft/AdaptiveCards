25a3e22d649ad071e83cbf64a012a2ec4cc6c7b2
Andrew Leader <aleader@microsoft.com>
2019-05-30 17:10:55 -0700

    New classType schema

--- source/nodejs/json-schema-with-types/src/command-line.ts
+++ source/nodejs/json-schema-with-types/src/command-line.ts
@@ -3,8 +3,16 @@ import * as tschema from "./json-schema-with-types";
 declare var process;
 
 var folderToTransform = process.argv[2];
-var primaryTypeName = process.argv[3];
+var primaryTypeName = process.argv[3].split(",");
+var defaultPrimaryType:string|null = null;
+if (process.argv.length > 4) {
+	defaultPrimaryType = process.argv[4];
+}
+var typePropertyName:string|null = null;
+if (process.argv.length > 5) {
+	typePropertyName = process.argv[5];
+}
 
-var transformed = tschema.transformFolder(folderToTransform, primaryTypeName);
+var transformed = tschema.transformFolder(folderToTransform, primaryTypeName, defaultPrimaryType, typePropertyName);
 
 console.log(JSON.stringify(transformed, null, 2));
