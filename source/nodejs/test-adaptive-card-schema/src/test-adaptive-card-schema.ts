baed307e9fc173666dc2088fb88cd83e4fba1ac3
Andrew Leader <aleader@microsoft.com>
2019-06-04 14:07:26 -0700

    All samples valid

--- source/nodejs/test-adaptive-card-schema/src/test-adaptive-card-schema.ts
+++ source/nodejs/test-adaptive-card-schema/src/test-adaptive-card-schema.ts
@@ -8,19 +8,51 @@ var ajv = new Ajv();
 ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
 var validate = ajv.compile(JSON.parse(schemaTxt));
 
+var shouldFail:string[] = [
+	"v1.2/Scenarios/SimpleFallback.json", // Uses custom types
+	"v1.2/Elements/ActionFallback.json", // Uses custom types
+	"v1.2/Elements/Action.Submit.Style.json", // Uses custom "other" style
+	"v1.2/Elements/Action.ShowCard.Style.json", // Uses custom "other" style
+	"v1.2/Elements/Action.OpenUrl.Style.json", // Uses custom "other" style
+	"Tests/Input.Text.InlineAction.ShowCard.json", // Purposefully incorrect for test purposes
+	"Tests/TypeIsRequired.json", // Purposefully omits type
+	"Tests/FallbackParsing.json", // Uses custom types
+	"Tests/EmptyFallbackCard.json", // Uses custom types
+	"Tests/DeepFallback.json", // Uses custom types
+	"Tests/CustomParsingTestUsingProgressBar.json", // Uses custom types
+	"Tests/AdditionalProperty.json", // Uses custom property
+	"Tests/AdaptiveCard.UnknownElements.json", // Uses custom type
+	"Tests/Action.CustomParsing.json", // Uses custom type
+];
+
 var total = 0;
 var passed = 0;
 
 allCards.forEach(path => {
+	var shouldBeValid = true;
+	shouldFail.forEach(skip => {
+		if (path.endsWith(skip)) {
+			shouldBeValid = false;
+		}
+	});
 	try {
 		total++;
 		var cardTxt = fs.readFileSync(path);
 		var isValid = validate(JSON.parse(cardTxt));
-		if (!isValid) {
-			console.warn(`${path}: INVALID`);
+		if (shouldBeValid) {
+			if (!isValid) {
+				console.warn(`${path}: INVALID`);
+			} else {
+				console.log(`${path}: Success`);
+				passed++;
+			}
 		} else {
-			console.log(`${path}: Success`);
-			passed++;
+			if (!isValid) {
+				console.log(`${path}: Success (correctly invalid)`);
+				passed++;
+			} else {
+				console.warn(`${path}: FAILED (should be invalid but was valid)`);
+			}
 		}
 	} catch (err) {
 		console.warn(`${path}: ${err}`);
