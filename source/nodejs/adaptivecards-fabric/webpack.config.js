0b588c42c68c7d4991fe93d67fb6d634d4e99a47
Matt Hidinger <matt.hidinger@gmail.com>
2019-06-28 12:13:55 -0700

    cleaning up fabric file names and package output structure

--- source/nodejs/adaptivecards-fabric/webpack.config.js
+++ source/nodejs/adaptivecards-fabric/webpack.config.js
@@ -10,11 +10,11 @@ module.exports = (env, argv) => {
 
     return {
         entry: {
-            'adaptivecards': './src/adaptivecards.ts'
+            'adaptivecards-fabric': './src/adaptivecards-fabric.ts'
         },
         output: {
             path: path.resolve(__dirname, "./dist"),
-            filename: "[name].js",
+			filename: devMode ? "[name].js" : "[name].min.js",
             libraryTarget: "umd",
             library: "@ms-conv-ux/adaptive-cards-extended"
         },
