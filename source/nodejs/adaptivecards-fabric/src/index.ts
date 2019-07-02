b3b7c0954eac5b4b59c719ee42c0c090ab51209f
Denis Fisenko <defisenk@microsoft.com>
2019-07-02 09:36:36 -0700

    updated exports

--- source/nodejs/adaptivecards-fabric/src/index.ts
+++ source/nodejs/adaptivecards-fabric/src/index.ts
@@ -1,4 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-export { useFabricComponents } from "./components/Fabric";
+export * from "./Components";
+export * from "./Actions";
+export * from "./Utils/shared";
