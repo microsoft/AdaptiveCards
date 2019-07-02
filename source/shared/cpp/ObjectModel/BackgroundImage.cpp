b503e246f7e82f20640ea7880ba13fbf858b8909
Alberto Medina Gutierrez <almedina@microsoft.com>
2019-07-02 10:08:25 -0700

    Fix formatting

--- source/shared/cpp/ObjectModel/BackgroundImage.cpp
+++ source/shared/cpp/ObjectModel/BackgroundImage.cpp
@@ -28,69 +28,69 @@ VerticalAlignment BackgroundImage::GetVerticalAlignment() const { return m_vAlig
 void BackgroundImage::SetVerticalAlignment(const VerticalAlignment& value) { m_vAlignment = value; }
 
 std::string BackgroundImage::Serialize() const
-    {
-        return ParseUtil::JsonToString(SerializeToJsonValue());
-    }
+{
+    return ParseUtil::JsonToString(SerializeToJsonValue());
+}
 
 Json::Value BackgroundImage::SerializeToJsonValue() const
+{
+    Json::Value root;
+
+    // if BackgroundImage has a url and the rest as default values
+    if (!m_url.empty() && m_fillMode == ImageFillMode::Cover && m_hAlignment == HorizontalAlignment::Left &&
+        m_vAlignment == VerticalAlignment::Top)
     {
-        Json::Value root;
-
-        // if BackgroundImage has a url and the rest as default values
-        if (!m_url.empty() && m_fillMode == ImageFillMode::Cover && m_hAlignment == HorizontalAlignment::Left &&
-            m_vAlignment == VerticalAlignment::Top)
-        {
-            root = m_url;
-        }
-
-        // standard serialization process
-        else
-        {
-            if (!m_url.empty())
-            {
-                root[AdaptiveCardSchemaKeyToString(AdaptiveCardSchemaKey::Url)] = m_url;
-            }
-
-            if (m_fillMode != ImageFillMode::Cover)
-            {
-                root[AdaptiveCardSchemaKeyToString(AdaptiveCardSchemaKey::FillMode)] =
-                    ImageFillModeToString(m_fillMode);
-            }
-
-            if (m_hAlignment != HorizontalAlignment::Left)
-            {
-                root[AdaptiveCardSchemaKeyToString(AdaptiveCardSchemaKey::HorizontalAlignment)] =
-                    HorizontalAlignmentToString(m_hAlignment);
-            }
-
-            if (m_vAlignment != VerticalAlignment::Top)
-            {
-                root[AdaptiveCardSchemaKeyToString(AdaptiveCardSchemaKey::VerticalAlignment)] =
-                    VerticalAlignmentToString(m_vAlignment);
-            }
-        }
-        return root;
+        root = m_url;
     }
+
+    // standard serialization process
+    else
+    {
+        if (!m_url.empty())
+        {
+            root[AdaptiveCardSchemaKeyToString(AdaptiveCardSchemaKey::Url)] = m_url;
+        }
+
+        if (m_fillMode != ImageFillMode::Cover)
+        {
+            root[AdaptiveCardSchemaKeyToString(AdaptiveCardSchemaKey::FillMode)] =
+                ImageFillModeToString(m_fillMode);
+        }
+
+        if (m_hAlignment != HorizontalAlignment::Left)
+        {
+            root[AdaptiveCardSchemaKeyToString(AdaptiveCardSchemaKey::HorizontalAlignment)] =
+                HorizontalAlignmentToString(m_hAlignment);
+        }
+
+        if (m_vAlignment != VerticalAlignment::Top)
+        {
+            root[AdaptiveCardSchemaKeyToString(AdaptiveCardSchemaKey::VerticalAlignment)] =
+                VerticalAlignmentToString(m_vAlignment);
+        }
+    }
+    return root;
+}
 
 std::shared_ptr<BackgroundImage> BackgroundImage::Deserialize(const Json::Value& json)
-    {
-        std::shared_ptr<BackgroundImage> image = std::make_shared<BackgroundImage>();
+{
+    std::shared_ptr<BackgroundImage> image = std::make_shared<BackgroundImage>();
 
-        image->SetUrl(ParseUtil::GetString(json, AdaptiveCardSchemaKey::Url, true));
+    image->SetUrl(ParseUtil::GetString(json, AdaptiveCardSchemaKey::Url, true));
 
-        image->SetFillMode(ParseUtil::GetEnumValue<ImageFillMode>(
-            json, AdaptiveCardSchemaKey::FillMode, ImageFillMode::Cover, ImageFillModeFromString));
+    image->SetFillMode(ParseUtil::GetEnumValue<ImageFillMode>(
+        json, AdaptiveCardSchemaKey::FillMode, ImageFillMode::Cover, ImageFillModeFromString));
 
-        image->SetHorizontalAlignment(ParseUtil::GetEnumValue<HorizontalAlignment>(
-            json, AdaptiveCardSchemaKey::HorizontalAlignment, HorizontalAlignment::Left, HorizontalAlignmentFromString));
+    image->SetHorizontalAlignment(ParseUtil::GetEnumValue<HorizontalAlignment>(
+        json, AdaptiveCardSchemaKey::HorizontalAlignment, HorizontalAlignment::Left, HorizontalAlignmentFromString));
 
-        image->SetVerticalAlignment(ParseUtil::GetEnumValue<VerticalAlignment>(
-            json, AdaptiveCardSchemaKey::VerticalAlignment, VerticalAlignment::Top, VerticalAlignmentFromString));
+    image->SetVerticalAlignment(ParseUtil::GetEnumValue<VerticalAlignment>(
+        json, AdaptiveCardSchemaKey::VerticalAlignment, VerticalAlignment::Top, VerticalAlignmentFromString));
 
-        return image;
-    }
+    return image;
+}
 
 std::shared_ptr<BackgroundImage> BackgroundImage::DeserializeFromString(const std::string& jsonString)
-    {
-        return BackgroundImage::Deserialize(ParseUtil::GetJsonValueFromString(jsonString));
-    }
+{
+    return BackgroundImage::Deserialize(ParseUtil::GetJsonValueFromString(jsonString));
+}
