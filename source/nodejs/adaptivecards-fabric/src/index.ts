f53fef59911b6657fcd6e69b8825c38c1fddc4c3
Matt Hidinger <matt.hidinger@gmail.com>
2019-07-02 16:19:01 -0700

    rename folders to be lower case

--- source/nodejs/adaptivecards-fabric/src/index.ts
+++ source/nodejs/adaptivecards-fabric/src/index.ts
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-export * from "./Components";
-export * from "./Actions";
-export * from "./Utils/shared";
+export * from "./components";
+export * from "./actions";
+export * from "./utils/shared";
