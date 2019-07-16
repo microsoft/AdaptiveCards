670fbfb49943c4be20863379c4bcc2af0ae92c41
Joseph Woo <Joseph.Woo@microsoft.com>
2019-07-16 10:31:18 -0700

    Fixes issue #3159 (#3195)
    * first commit
    
    * need to fix hidden constraints
    
    * fixed issue
    
    * CR comments update

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRColumnView.mm
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRColumnView.mm
@@ -18,10 +18,13 @@
 - (void)addArrangedSubview:(UIView *)view
 {
     // if auto, maintain content size whenever possible
-    if([self.columnWidth isEqualToString:@"auto"]){
-        [view setContentHuggingPriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisHorizontal];
-        [view setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisHorizontal];
+    if([self.columnWidth isEqualToString:@"auto"]){        
+        [view setContentHuggingPriority:ACRColumnWidthPriorityAuto forAxis:UILayoutConstraintAxisHorizontal];
+        [view setContentCompressionResistancePriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisHorizontal];
       // if columnWidth is set to stretch or number, allow column to fill the available space
+    } else if([self.columnWidth isEqualToString:@"stretch"]){ 
+        [view setContentHuggingPriority:ACRColumnWidthPriorityStretch forAxis:UILayoutConstraintAxisHorizontal];
+        [view setContentCompressionResistancePriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
     } else {
         [view setContentHuggingPriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
         [view setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisHorizontal];
