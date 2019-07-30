870a7c187cd39f361de7b5b206fb804b33068059
Matt Hidinger <matt.hidinger@gmail.com>
2019-07-29 18:26:48 -0700

    tweaking github author generator

--- source/nodejs/adaptivecards-site/themes/adaptivecards/scripts/filter-githubauthor.js
+++ source/nodejs/adaptivecards-site/themes/adaptivecards/scripts/filter-githubauthor.js
@@ -1,60 +1,44 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-var https = require('https');
-var yaml = require('yaml-front-matter');
+const yaml = require('yaml-front-matter');
+const request = require('request-promise');
 
 hexo.extend.filter.register('before_post_render', function (data) {
-	if(data.layout !== "post")
+
+	if (data.layout !== "post")
 		return;
 
+	const frontMatter = yaml.loadFront(data.raw);
+	const github_username = frontMatter.github_username;
+	data.image = frontMatter.featured_image;
+
+	if (github_username) {
+		return request({
+			uri: `https://api.github.com/users/${github_username}`,
+			headers: {
+				'User-Agent': 'Request-Promise'
+			},
+			json: true
+		}).then(function (githubData) {
+			data.author = {
+				github_username: github_username,
+				avatar: githubData.avatar_url,
+				name: githubData.name,
+				bio: githubData.bio,
+				webpage: githubData.html_url,
+				twitter: frontMatter.twitter
+			};
+		}).catch(function (err) {
+			console.log(`ERROR: Failed getting GitHub data on page ${data.source}. Message: ${err.message}`);
+		});
+	} else {
+		console.log(`WARN: No GitHub username in page: ${data.source}`);
 		data.author = {
-			github_username: "matthidinger",
-			avatar: "matthidinger",
-			name: "Matt Hidinger",
-			bio: "I like thingss",
-			webpage: "http://matthidinger.com",
-			twitter: "matthidinger"
+			github_username: "Microsoft/AdaptiveCards",
+			avatar: "https://adaptivecards.io/content/adaptive-cards-100.png",
+			name: "Adaptive Cards Team",
+			bio: "Break outside the box of templated cards",
+			webpage: "https://adaptivecards.io"
 		};
-		return;
-
-	return new Promise(function (resolve, reject) {
-		
-		const frontMatter = yaml.loadFront(data.raw);
-		const github_username = frontMatter.github_username;
-
-		if (github_username) {
-			https.get(`https://api.github.com/users/${github_username}`, {
-				headers: { "User-Agent": "Adaptive-Cards-Blog-Engine" }
-			}, (resp) => {
-				let response = '';
-
-				resp.on('data', (chunk) => {
-					response += chunk;
-				});
-
-				resp.on('end', () => {
-					var githubData = JSON.parse(response);
-					data.author = {
-						github_username: github_username,
-						avatar: githubData.avatar_url,
-						name: githubData.name,
-						bio: githubData.bio,
-						webpage: githubData.html_url,
-						twitter: frontMatter.twitter
-					};
-					data.image = frontMatter.featured_image;
-					resolve();
-				});
-
-			}).on("error", (err) => {
-				console.log(`ERROR: Failed getting GitHub data on page ${data.source}. Message: ${err.message}`);
-				reject();
-			});
-		}
-		else {
-			console.log(`WARN: No GitHub username in page: ${data.source}`);
-			resolve();
-		}
-	});
-
+	}
 });
