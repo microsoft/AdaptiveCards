fcd0439f0bfc7fb1c9f8baec172fcedfadc609f3
Matt Hidinger <matt.hidinger@gmail.com>
2019-07-29 17:08:40 -0700

    fix env var

--- source/nodejs/adaptivecards-site/themes/adaptivecards/scripts/filter-setupconfig.js
+++ source/nodejs/adaptivecards-site/themes/adaptivecards/scripts/filter-setupconfig.js
@@ -4,7 +4,8 @@
 hexo.extend.filter.register('after_init', function () {
 	// Allow the site to deployed to a sub directory
 	// E.g., set the env variable to "PR123" on the build server 
-	if (process.env.SITE_ROOT) {
-		hexo.config.root = `/${process.env.SITE_ROOT}/`;
+	if (process.env.SITE_PATH) {
+		console.log(`INFO: Setting hexo config root to ${process.env.SITE_PATH}`);
+		hexo.config.root = `/${process.env.SITE_PATH}/`;
 	}
 });
