a9188c725da74094b32a863b076eb1e3558c0e7b
nesalang <Joseph.Woo@microsoft.com>
2019-07-31 10:25:57 -0700

    fixes merge issue

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRColumnView.mm
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRColumnView.mm
@@ -20,11 +20,13 @@
 {
     // if auto, maintain content size whenever possible
     if([self.columnWidth isEqualToString:@"auto"]){        
+
         NSInteger priority = ACRColumnWidthPriorityAuto;
         if (self.isLastColumn) {
             priority = ACRColumnWidthPriorityStretchAuto;
         }
-        [view setContentHuggingPriority:ACRColumnWidthPriorityAuto forAxis:UILayoutConstraintAxisHorizontal];
+
+        [view setContentHuggingPriority:priority forAxis:UILayoutConstraintAxisHorizontal];
         [view setContentCompressionResistancePriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisHorizontal];
       // if columnWidth is set to stretch or number, allow column to fill the available space
     } else if([self.columnWidth isEqualToString:@"stretch"]){ 
