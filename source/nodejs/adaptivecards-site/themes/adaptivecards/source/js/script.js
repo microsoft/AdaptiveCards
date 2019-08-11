8470e8779d13bf2d78bde4b0ccf039e06f13241a
Matt Hidinger <matt.hidinger@gmail.com>
2019-08-10 20:34:11 -0700

    added site footer and polished blog styling

--- source/nodejs/adaptivecards-site/themes/adaptivecards/source/js/script.js
+++ source/nodejs/adaptivecards-site/themes/adaptivecards/source/js/script.js
@@ -392,7 +392,7 @@ $(function () {
 
 	$("#feedback-button").click(function(e) {
 		e.preventDefault();
-		window.open("https://github.com/MicrosoftDocs/AdaptiveCards/issues/new?title=" 
+		window.open("https://github.com/Microsoft/AdaptiveCards/issues/new?title=" 
 			+ encodeURIComponent("[Website] [Your feedback title here]") 
 			+ "&body=" + encodeURIComponent("\r\n\r\n[Your detailed feedback here]\r\n\r\n---\r\n* URL: " 
 			+ window.location.href));
