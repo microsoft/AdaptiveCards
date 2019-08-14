ad24111745d5cda513ed742de03979d0fc163a6b
Utsav Lathia <t-utlath@microsoft.com>
2019-08-13 18:34:36 -0700

    removed mandatory test from 1DS as it is optional now

--- source/nodejs/adaptivecards/src/__tests__/logging/Microsoft1DS.spec.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-import { Microsoft1DSLogger } from "../../logging/Microsoft1DSLogger/Microsoft1DSLogger";
-
-test('Microsoft1DSLogger should be instantiated', () => {
-    const logger = new Microsoft1DSLogger();
-    expect(logger).toEqual(expect.anything());
-})
