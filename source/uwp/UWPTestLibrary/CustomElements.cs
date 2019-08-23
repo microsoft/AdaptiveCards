6f31e7c7c15d05eea4c020a7ea71b4472dda6e28
Rebecca Muraira <rebecch@microsoft.com>
2019-08-23 14:41:32 -0700

    stuff

--- source/uwp/UWPTestLibrary/CustomElements.cs
+++ source/uwp/UWPTestLibrary/CustomElements.cs
@@ -40,7 +40,17 @@ namespace TestLibrary
             {
                 throw new Exception("Animal Type is required and must be a string!");
             }
+
             animalGrid.AnimalType = inputJson["animalType"].GetString();
+
+            if (animalGrid.AnimalType != "Mammals" &&
+                animalGrid.AnimalType != "Birds" &&
+                animalGrid.AnimalType != "Fish" &&
+                animalGrid.AnimalType != "Reptiles" &&
+                animalGrid.AnimalType != "Insects")
+            {
+                throw new Exception("Unknown animal type (known types are Mammals, Birds, Fish, Reptiles, and Insects");
+            }
 
             // Set the animal count
             if (inputJson.ContainsKey("animalCount"))
@@ -53,12 +63,12 @@ namespace TestLibrary
 
                 if (animalCountJson.GetNumber() > 5)
                 {
-                    var warning = new AdaptiveWarning
-                    {
-                        StatusCode = WarningStatusCode.CustomWarning,
-                        Message = "We only have 5 animals, some will be repeats"
-                    };
+                    //var warning = new AdaptiveWarning();
+                    //warning.StatusCode = WarningStatusCode.CustomWarning;
+                    //warning.Message = "We only have 5 animals, some will be repeats";
 
+
+                    var warning = new AdaptiveWarning(WarningStatusCode.CustomWarning, "We only have 5 animals, some will be repeats");
                     warnings.Add(warning);
                 }
 
@@ -73,22 +83,90 @@ namespace TestLibrary
     {
         public UIElement Render(IAdaptiveCardElement element, AdaptiveRenderContext context, AdaptiveRenderArgs renderArgs)
         {
-            Uri[] mammelUris =
-            {
-                new Uri("https://picsum.photos/300/200?image=1003"),
-                new Uri("https://en.wikipedia.org/wiki/File:Kittyply_edit1.jpg")
-            };
-
             AnimalGrid animalGrid = element as AnimalGrid;
-            for (uint i = 0; i < animalGrid.AnimalCount; i++)
-            {
+            string[] animalUris = new string[5];
 
+            bool knownAnimal = false;
+            if (animalGrid.AnimalType == "Mammals")
+            {
+                knownAnimal = true;
+
+                animalUris[0] = "https://images.pexels.com/photos/145939/pexels-photo-145939.jpeg";
+                animalUris[1] = "https://images.pexels.com/photos/2071873/pexels-photo-2071873.jpeg";
+                animalUris[2] = "https://images.pexels.com/photos/1319515/pexels-photo-1319515.jpeg";
+                animalUris[3] = "https://images.pexels.com/photos/2295744/pexels-photo-2295744.jpeg";
+                animalUris[4] = "https://images.pexels.com/photos/145947/pexels-photo-145947.jpeg";
+            }
+            else if (animalGrid.AnimalType == "Birds")
+            {
+                knownAnimal = true;
+
+                animalUris[0] = "https://images.pexels.com/photos/2629372/pexels-photo-2629372.jpeg";
+                animalUris[1] = "https://images.pexels.com/photos/2226006/pexels-photo-2226006.jpeg";
+                animalUris[2] = "https://images.pexels.com/photos/151511/pexels-photo-151511.jpeg";
+                animalUris[3] = "https://images.pexels.com/photos/2152399/pexels-photo-2152399.jpeg";
+                animalUris[4] = "https://images.pexels.com/photos/2214495/pexels-photo-2214495.jpeg";
+            }
+            else if (animalGrid.AnimalType == "Fish")
+            {
+                knownAnimal = true;
+
+                animalUris[0] = "https://images.pexels.com/photos/128756/pexels-photo-128756.jpeg";
+                animalUris[1] = "https://images.pexels.com/photos/325045/pexels-photo-325045.jpeg";
+                animalUris[2] = "https://images.pexels.com/photos/60090/pexels-photo-60090.jpeg";
+                animalUris[3] = "https://images.pexels.com/photos/1112007/pexels-photo-1112007.jpeg";
+                animalUris[4] = "https://images.pexels.com/photos/1365264/pexels-photo-1365264.jpeg";
+            }
+            else if (animalGrid.AnimalType == "Reptiles")
+            {
+                knownAnimal = true;
+
+                animalUris[0] = "https://images.pexels.com/photos/47310/king-snake-snake-banded-red-47310.jpeg";
+                animalUris[1] = "https://images.pexels.com/photos/62289/yemen-chameleon-chamaeleo-calyptratus-chameleon-reptile-62289.jpeg";
+                animalUris[2] = "https://images.pexels.com/photos/735174/pexels-photo-735174.jpeg";
+                animalUris[3] = "https://images.pexels.com/photos/2758845/pexels-photo-2758845.jpeg";
+                animalUris[4] = "https://images.pexels.com/photos/792358/pexels-photo-792358.jpeg";
+            }
+            else if (animalGrid.AnimalType == "Insects")
+            {
+                knownAnimal = true;
+
+                animalUris[0] = "https://images.pexels.com/photos/1085542/pexels-photo-1085542.jpeg";
+                animalUris[1] = "https://images.pexels.com/photos/37830/locust-jumping-grasshopper-animal-37830.jpeg";
+                animalUris[2] = "https://images.pexels.com/photos/53957/striped-core-butterflies-butterfly-brown-53957.jpeg";
+                animalUris[3] = "https://images.pexels.com/photos/1104974/pexels-photo-1104974.jpeg";
+                animalUris[4] = "https://images.pexels.com/photos/395241/pexels-photo-395241.jpeg";
             }
 
-            var textBlock = new TextBlock();
-            textBlock.Text = "This is an animal grid with " + animalGrid.AnimalCount + " " + animalGrid.AnimalType + "!";
+            if (knownAnimal)
+            {
+                AdaptiveImageSet animalImageSet = new AdaptiveImageSet
+                {
+                    ImageSize = ImageSize.Large
+                };
 
-            return textBlock;
+                for (uint i = 0; i < animalGrid.AnimalCount; i++)
+                {
+                    AdaptiveImage image = new AdaptiveImage
+                    {
+                        Url = animalUris[i % 5]
+                    };
+
+                    animalImageSet.Images.Add(image);
+                }
+
+                AdaptiveImageSetRenderer imageSetRenderer = new AdaptiveImageSetRenderer();
+                return imageSetRenderer.Render(animalImageSet, context, renderArgs);
+            }
+            else
+            {
+
+                var textBlock = new TextBlock
+                {
+                    Text = "This is an animal grid with " + animalGrid.AnimalCount + " " + animalGrid.AnimalType + "!"
+                };
+                return textBlock;
+            }
         }
     }
 }
