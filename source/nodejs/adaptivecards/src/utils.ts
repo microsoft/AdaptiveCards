4c9fad71c98c1e25e4bd703b7ef8704012020375
David Claux <dclaux@users.noreply.github.com>
2019-09-11 14:19:35 -0700

    Fix string/boolean parsing regression with null

--- source/nodejs/adaptivecards/src/utils.ts
+++ source/nodejs/adaptivecards/src/utils.ts
@@ -19,11 +19,11 @@ export function appendChild(node: Node, child: Node) {
 }
 
 export function getStringValue(obj: any, defaultValue: string = undefined): string {
-    return (typeof obj !== 'undefined') ? obj.toString() : defaultValue;
+    return typeof obj === "string" ? obj.toString() : defaultValue;
 }
 
 export function getNumberValue(obj: any, defaultValue: number = undefined): number {
-    return obj && typeof obj === "number" ? obj : defaultValue;
+    return typeof obj === "number" ? obj : defaultValue;
 }
 
 export function getBoolValue(value: any, defaultValue: boolean): boolean {
