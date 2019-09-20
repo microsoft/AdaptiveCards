8a9147763c0228c0eca134d6dd09c5ee28a5c48b
shalinijoshi19 <shalinij@microsoft.com>
2019-09-20 15:21:52 -0700

    Pulling in changes to adaptivecards-site folder from master

--- source/nodejs/adaptivecards-site/themes/adaptivecards/scripts/trysamplelink.js
+++ source/nodejs/adaptivecards-site/themes/adaptivecards/scripts/trysamplelink.js
@@ -5,5 +5,5 @@
 var path = require("path");
 
 hexo.extend.helper.register('trysamplelink', function (samplePath) {
-  return '<a class="w3-btn w3-blue" target="_blank" href="/designer/index.html?card=/payloads/' + path.basename(samplePath) + '">Try it Yourself Â»</a>';
+  return '<a class="w3-btn w3-blue" target="_blank" href="/designer/index.html?card=/payloads/' + path.basename(samplePath) + '">Try it Yourself »</a>';
 });
