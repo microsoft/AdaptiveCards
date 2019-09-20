8bc56b7c6e0952970e9ac118840691f22e37ab57
nesalang <Joseph.Woo@microsoft.com>
2019-09-20 09:54:18 -0700

    picked up sample app update that added file browser

--- source/ios/AdaptiveCards/ADCIOSVisualizer/ADCIOSVisualizer/ACVTableViewController.m
+++ source/ios/AdaptiveCards/ADCIOSVisualizer/ADCIOSVisualizer/ACVTableViewController.m
@@ -17,26 +17,10 @@
     [super viewDidLoad];
 
     NSBundle *main = [NSBundle mainBundle];
-    pathsToFiles = [main pathsForResourcesOfType:@"json" inDirectory:nil];
-    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"NOT (SELF contains[c] 'sample.json')"];
-    pathsToFiles = [pathsToFiles filteredArrayUsingPredicate:predicate];
-    pathsToFiles = [pathsToFiles sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
-        NSString *path1 = obj1, *path2 = obj2;
-        NSComparisonResult result = [[[NSFileManager defaultManager] displayNameAtPath:path1] compare:[[NSFileManager defaultManager] displayNameAtPath:path2]];
-        return result;
-    }];
-    NSInteger cnt = [pathsToFiles count];
-    enum DesiredIdx { eDefaultViewIdx = 2 };
-    if(cnt >= eDefaultViewIdx)
-    {
-        [_delegate source:self userconfig:[NSString stringWithContentsOfFile:[main pathForResource:@"sample" ofType:@"json"]
-                                                                    encoding:NSUTF8StringEncoding
-                                                                       error:nil]];
-        self.userSelectedJSon =
-        [NSString stringWithContentsOfFile:pathsToFiles[[pathsToFiles count] - eDefaultViewIdx]
-                                  encoding:NSUTF8StringEncoding
-                                     error:nil];
-    }
+  
+    [_delegate source:self userconfig:[NSString stringWithContentsOfFile:[main pathForResource:@"sample" ofType:@"json"]
+                                                                encoding:NSUTF8StringEncoding
+                                                                   error:nil]];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -65,6 +49,41 @@
                               encoding:NSUTF8StringEncoding
                                  error:nil];
     [_delegate fromACVTable:self userSelectedJson:self.userSelectedJSon];
+    if (!self.IsCollapsed) {
+        UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+        CGFloat height = cell.frame.size.height;
+        self.tableHeight.active = NO;
+        self.tableHeight = [tableView.heightAnchor constraintEqualToConstant:height];
+        [UIView animateWithDuration:0.6 animations:^{ self.tableHeight.active = YES; } completion:^(BOOL finished){[tableView scrollToRowAtIndexPath:indexPath atScrollPosition:UITableViewScrollPositionTop animated:YES];}];
+        self.IsCollapsed = YES;
+        
+    } else {
+        [UIView animateWithDuration:0.6 animations:^{
+            self.tableHeight.active = NO;
+            self.tableHeight = [self.tableView.heightAnchor constraintEqualToConstant:200];
+            self.tableHeight.active = YES;
+        }];
+        self.IsCollapsed = NO;
+    }
+}
+
+- (void)updateTable:(NSArray<NSString *> *)data {
+    pathsToFiles = data;
+    if (self.tableView.hidden) {
+        if (data) {
+            [UIView animateWithDuration:0.6 animations:^{
+                self.tableView.hidden = NO;
+                self.tableHeight.active = NO;
+                self.tableHeight = [self.tableView.heightAnchor constraintEqualToConstant:200];
+                self.tableHeight.active = YES;
+            }];
+            self.IsCollapsed = NO;
+
+        }
+    } else if (!data) {
+        [UIView animateWithDuration:0.6 animations:^{self.tableView.hidden = YES;}];
+    }
+    [self.tableView reloadData];
 }
 
 @end
