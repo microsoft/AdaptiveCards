1b4a93f1624efa659ac419fae150d26626439317
Rebecca Muraira <rebecch@microsoft.com>
2019-10-04 12:27:28 -0700

    cleanup

--- source/uwp/Renderer/lib/AdaptiveActionSet.h
+++ source/uwp/Renderer/lib/AdaptiveActionSet.h
@@ -105,7 +105,6 @@ namespace AdaptiveNamespace
             return AdaptiveCardElementBase::get_Requirements(requirements);
         }
 
-
         IFACEMETHODIMP ToJson(_Out_ ABI::Windows::Data::Json::IJsonObject** result)
         {
             return AdaptiveCardElementBase::ToJson(result);
