894c494f14826e6ba77a01310fef40ef149e5b84
Alberto Medina Gutierrez <almedina@microsoft.com>
2019-10-11 17:06:42 -0700

    Add simple test app

---
+++ source/dotnet/Library/AdaptiveCards.Sample.WPFTemplating/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+using AdaptiveCards.Rendering.Wpf;
+using AdaptiveCards.Templating;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdaptiveCards.Sample.WPFTemplating
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Renderer = new AdaptiveCardRenderer()
+            {
+                Resources = Resources
+            };
+
+            cardTemplate.Text = File.ReadAllText("Samples\\CardTemplate.json");
+            templateData.Text = File.ReadAllText("Samples\\DataTemplate.json");
+        }
+
+        public AdaptiveCardRenderer Renderer { get; set; }
+
+        private void renderCard_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            AdaptiveCardParseResult parseResult = AdaptiveCard.FromJson(cardTemplate.Text);
+            AdaptiveCard card = parseResult.Card;
+            RenderedAdaptiveCard renderedCard = Renderer.RenderCard(card);
+            */
+            
+            var transformer = new AdaptiveTransformer();
+            var cardJson = transformer.Transform(cardTemplate.Text, templateData.Text);
+            AdaptiveCard card2 = AdaptiveCard.FromJson(cardJson).Card;
+            RenderedAdaptiveCard renderedCard = Renderer.RenderCard(card2);
+
+            cardGrid.Opacity = 1;
+            cardGrid.Children.Clear();
+            cardGrid.Children.Add(renderedCard.FrameworkElement);
+        }
+    }
+}
