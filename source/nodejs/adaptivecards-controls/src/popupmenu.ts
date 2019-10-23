ed208c13020d8c3aab4cbbb0ff60db6188ef7df1
David Claux <dclaux@users.noreply.github.com>
2019-10-23 16:36:04 -0700

    Small popup control refactor

--- source/nodejs/adaptivecards-controls/src/popupmenu.ts
+++ source/nodejs/adaptivecards-controls/src/popupmenu.ts
@@ -1,15 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as Utils from "./utils";
 import * as Constants from "./constants";
 import { Collection } from "./collection";
 import { PopupControl } from "./inputwithpopup";
 import { DropDownItem } from "./dropdown";
 
 export class PopupMenu extends PopupControl {
-    private _overlayElement: HTMLElement;
-    private _popupControlElement: HTMLElement;
-    private _isOpen: boolean;
     private _items: Collection<DropDownItem> = new Collection<DropDownItem>();
     private _renderedItems: Array<HTMLElement> = [];
     private _selectedIndex: number = -1;
@@ -93,115 +89,6 @@ export class PopupMenu extends PopupControl {
 
                 break;
         }
-    }
-
-    popup(rootElement: HTMLElement) {
-        if (!this._isOpen) {
-            this._overlayElement = document.createElement("div");
-            this._overlayElement.className = "ms-ctrl-overlay";
-            this._overlayElement.tabIndex = 0;
-            this._overlayElement.style.width = document.documentElement.scrollWidth + "px";
-            this._overlayElement.style.height = document.documentElement.scrollHeight + "px";
-            this._overlayElement.onfocus = (e) => { this.closePopup(); };
-
-            document.body.appendChild(this._overlayElement);
-
-            var rootElementBounds = rootElement.getBoundingClientRect();
-
-            this._popupControlElement = this.render(rootElementBounds);
-            this._popupControlElement.classList.remove(
-                "ms-ctrl-slide",
-                "ms-ctrl-slideLeftToRight",
-                "ms-ctrl-slideRightToLeft",
-                "ms-ctrl-slideTopToBottom",
-                "ms-ctrl-slideRightToLeft");
-
-            this._overlayElement.appendChild(this._popupControlElement);
-
-            var popupElementBounds = this._popupControlElement.getBoundingClientRect();
-
-            var availableSpaceBelow = window.innerHeight - rootElementBounds.bottom;
-            var availableSpaceAbove = rootElementBounds.top;
-
-            var left = rootElementBounds.left + Utils.getScrollX();
-            var top;
-
-            if (availableSpaceAbove < popupElementBounds.height && availableSpaceBelow < popupElementBounds.height) {
-                // Not enough space above or below root element
-                var maxPopupHeight = window.innerHeight;
-
-                this._popupControlElement.style.maxHeight = maxPopupHeight + "px";
-
-                if (maxPopupHeight < popupElementBounds.height) {
-                    top = Utils.getScrollY();
-                }
-                else {
-                    top = Utils.getScrollY() + rootElementBounds.top + (rootElementBounds.height - maxPopupHeight) /2;
-                }
-
-                var availableSpaceRight = window.innerWidth - rootElementBounds.right;
-                var availableSpaceLeft = rootElementBounds.left;
-
-                if (availableSpaceLeft < popupElementBounds.width && availableSpaceRight < popupElementBounds.width) {
-                    // Not enough space left or right of root element
-                    var maxPopupWidth = window.innerWidth;
-
-                    this._popupControlElement.style.maxWidth = maxPopupWidth + "px";
-
-                    if (maxPopupWidth < popupElementBounds.width) {
-                        left = Utils.getScrollX();
-                    }
-                    else {
-                        left = Utils.getScrollX() + rootElementBounds.left + (rootElementBounds.width - maxPopupWidth) /2;
-                    }
-                }
-                else {
-                    // Enough space on the left or right of the root element
-                    if (availableSpaceRight >= popupElementBounds.width) {
-                        left = Utils.getScrollX() + rootElementBounds.right;
-
-                        this._popupControlElement.classList.add("ms-ctrl-slide", "ms-ctrl-slideLeftToRight");
-                    }
-                    else {
-                        left = Utils.getScrollX() + rootElementBounds.left - popupElementBounds.width;
-
-                        this._popupControlElement.classList.add("ms-ctrl-slide", "ms-ctrl-slideRightToLeft");
-                    }
-                }
-            }
-            else {
-                // Enough space above or below root element
-                if (availableSpaceBelow >= popupElementBounds.height) {
-                    top = Utils.getScrollY() + rootElementBounds.bottom;
-
-                    this._popupControlElement.classList.add("ms-ctrl-slide", "ms-ctrl-slideTopToBottom");
-                }
-                else {
-                    top = Utils.getScrollY() + rootElementBounds.top - popupElementBounds.height
-
-                    this._popupControlElement.classList.add("ms-ctrl-slide", "ms-ctrl-slideBottomToTop");
-                }
-            }
-
-            this._popupControlElement.style.left = left + "px";
-            this._popupControlElement.style.top = top + "px";
-
-            this._popupControlElement.focus();
-
-            this._isOpen = true;
-        }
-    }
-
-    closePopup() {
-        if (this._isOpen) {
-            document.body.removeChild(this._overlayElement);
-
-            this._isOpen = false;
-        }
-    }
-
-    get isOpen(): boolean {
-        return this._isOpen;
     }
 
     get items(): Collection<DropDownItem> {
