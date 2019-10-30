2ca4a5b40bd0e950d2f5db646b4233c6fc7ce32b
Joseph Woo <Joseph.Woo@microsoft.com>
2019-10-30 13:45:34 -0700

    Parse Context Update (#3548)
    * made parse context not static; need to fix the internal id being null when showcard is parsed
    
    * adding missing file
    
    * add parse context for property parsing
    
    * work in progress
    
    * fixes concurrency issue during parsing
    
    * minor fix
    
    * updated PR per CR comments

--- source/dotnet/Library/AdaptiveCards/AdaptiveInlinesConverter.cs
+++ source/dotnet/Library/AdaptiveCards/AdaptiveInlinesConverter.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 
 namespace AdaptiveCards
 {
-    class AdaptiveInlinesConverter : JsonConverter
+    class AdaptiveInlinesConverter : AdaptiveTypedBaseElementConverter 
     {
         public override bool CanRead => true;
 
@@ -39,7 +39,17 @@ namespace AdaptiveCards
                     {
                         throw new AdaptiveSerializationException($"Property 'type' must be '{AdaptiveTextRun.TypeName}'");
                     }
-                    arrayList.Add((AdaptiveInline)jobj.ToObject(typeof(AdaptiveTextRun)));
+
+                    if(ParseContext == null) {
+                        ParseContext = new ParseContext();
+                    }
+
+                    var adaptiveInline = JsonConvert.DeserializeObject<AdaptiveTextRun>(jobj.ToString(), new JsonSerializerSettings
+                    {
+                        ContractResolver = new WarningLoggingContractResolver(new AdaptiveCardParseResult(), ParseContext),
+                        Converters = { new StrictIntConverter() }
+                    });
+                    arrayList.Add(adaptiveInline);
                 }
             }
             return arrayList;
--- source/dotnet/Library/AdaptiveCards/ParseContext.cs
+++ source/dotnet/Library/AdaptiveCards/ParseContext.cs
@@ -5,24 +5,18 @@ using Newtonsoft.Json;
 
 namespace AdaptiveCards
 {
-    static class ParseContext
+    public class ParseContext
     {
-        public static void Initialize()
-        {
-            elementIds = new Dictionary<string, List<AdaptiveInternalID>>();
-            idStack = new Stack<Tuple<string, AdaptiveInternalID, bool>>();
-        }
-
         public enum ContextType { Element, Action };
 
-        public static ContextType Type { get; set; }
+        public ContextType Type { get; set; }
 
-        private static IDictionary<string, List<AdaptiveInternalID>> elementIds = new Dictionary<string, List<AdaptiveInternalID>>();
+        private IDictionary<string, List<AdaptiveInternalID>> elementIds = new Dictionary<string, List<AdaptiveInternalID>>();
 
-        private static Stack<Tuple<string, AdaptiveInternalID, bool>> idStack = new Stack<Tuple<string, AdaptiveInternalID, bool>>();
+        private Stack<Tuple<string, AdaptiveInternalID, bool>> idStack = new Stack<Tuple<string, AdaptiveInternalID, bool>>();
 
         // Push the provided state on to our ID stack
-        public static void PushElement(string idJsonProperty, AdaptiveInternalID internalId)
+        public void PushElement(string idJsonProperty, AdaptiveInternalID internalId)
         {
             if (internalId.Equals(AdaptiveInternalID.Invalid))
             {
@@ -31,7 +25,7 @@ namespace AdaptiveCards
             idStack.Push(new Tuple<string, AdaptiveInternalID, bool>(idJsonProperty, internalId, AdaptiveFallbackConverter.IsInFallback));
         }
 
-        public static AdaptiveInternalID PeekElement()
+        public AdaptiveInternalID PeekElement()
         {
             if (idStack.Count == 0)
             {
@@ -42,7 +36,7 @@ namespace AdaptiveCards
         }
 
         // Pop the last id off our stack and perform validation 
-        public static void PopElement()
+        public void PopElement()
         {
             // about to pop an element off the stack. perform collision list maintenance and detection.
             var idsToPop = idStack.Peek();
@@ -121,7 +115,7 @@ namespace AdaptiveCards
 
         // Walk stack looking for first element to be marked fallback (which isn't the ID we're supposed to skip), then
         // return its internal ID. If none, return an invalid ID. (see comment above)
-        public static AdaptiveInternalID GetNearestFallbackID(AdaptiveInternalID skipID)
+        public AdaptiveInternalID GetNearestFallbackID(AdaptiveInternalID skipID)
         {
             foreach (var curElement in idStack)
             {
