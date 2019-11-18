a117e8bbacef4883b7e728e8db68f644123ada2c
David Claux <dclaux@users.noreply.github.com>
2019-11-18 10:20:56 -0800

    [TS] Fix collapsed ShowCard losing input values

--- source/nodejs/adaptivecards/src/card-elements.ts
+++ source/nodejs/adaptivecards/src/card-elements.ts
@@ -4595,7 +4595,7 @@ class ActionCollection {
 
         (<InlineAdaptiveCard>action.card).suppressStyle = suppressStyle;
 
-        var renderedCard = action.card.render();
+        var renderedCard = action.card.renderedElement ? action.card.renderedElement : action.card.render();
 
         this._actionCard = renderedCard;
         this._expandedAction = action;
