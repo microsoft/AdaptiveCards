4ea3f3795e63e8a38e4ad5e916e947ba833e17c8
Joseph Woo <Joseph.Woo@microsoft.com>
2019-11-20 17:51:59 -0800

    Jwoo/release/1.2 (#3629)
    * Fixes vertical alignment issue & horizontal stretch
    
    * updated changes for release/1.2
    
    * updated

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRColumnSetRenderer.mm
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRColumnSetRenderer.mm
@@ -164,11 +164,7 @@
                                                    constant:0]];
                     prevRelColumnWidth = relativeColumnWidth;
                 }
-
-                if (curView.hasStretchableView || (columnSetElem->GetHeight() == HeightType::Stretch)) {
-                    [columnSetView setAlignmentForColumnStretch];
-                }
-
+                
                 prevView = curView;
                 prevRelColumnWidth = relativeColumnWidth;
             }
@@ -178,13 +174,19 @@
                 NSLog(@"unexpected column width property is given");
             }
         }
+
+        // to enable filler space to fill, its superview must strech it
+        if (curView.hasStretchableView || (columnSetElem->GetHeight() == HeightType::Stretch)) {
+            [columnSetView setAlignmentForColumnStretch];
+        }
+
         prevColumn = column;
     }
 
     castedRenderer.fillAlignment = NO;
 
     if ([constraints count]) {
-        [columnSetView addConstraints:constraints];
+        [NSLayoutConstraint activateConstraints:constraints];
     }
 
     std::shared_ptr<BaseActionElement> selectAction = columnSetElem->GetSelectAction();
