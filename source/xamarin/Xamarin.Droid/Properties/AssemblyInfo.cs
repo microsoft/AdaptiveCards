fd08be4b00cbf61ee9be024d4865fd74f5d36aae
Alberto Medina Gutierrez <almedina@microsoft.com>
2019-12-04 17:46:45 -0800

    Change key location

--- source/xamarin/Xamarin.Droid/Properties/AssemblyInfo.cs
+++ source/xamarin/Xamarin.Droid/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.CompilerServices;
 using Android.App;
 
 #if DELAY_SIGN
-[assembly: AssemblyKeyFileAttribute(@"\\cp1l4omt\StrongNamePublicKeys\35MSSharedLib1024.snk")]
+[assembly: AssemblyKeyFileAttribute(@"\source\xamarin\Build\35MSSharedLib1024.snk")]
 [assembly: AssemblyDelaySignAttribute(true)]
 #endif
