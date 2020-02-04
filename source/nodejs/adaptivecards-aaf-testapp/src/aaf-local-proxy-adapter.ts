f64a821d4f32c633586233dbbc3f01cc18f99373
David Claux <dclaux@users.noreply.github.com>
2020-02-04 13:52:40 -0800

    Work in progress

--- source/nodejs/adaptivecards-aaf-testapp/src/aaf-local-proxy-adapter.ts
+++ source/nodejs/adaptivecards-aaf-testapp/src/aaf-local-proxy-adapter.ts
@@ -3,7 +3,7 @@ import * as Adaptive from "adaptivecards";
 import * as Shared from "./shared";
 
 export class LocalProxyAdapter extends AAF.ProxyAdapter {
-    executeAction(action: Adaptive.Action): string {
+    executeAction(action: Adaptive.SubmitAction): string {
         return JSON.stringify(Shared.sampleRefreshCard);
     }
 }
