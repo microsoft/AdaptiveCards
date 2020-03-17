2ee0e4d02a41bd8a9a37417149f48b20d4b018d0
Alberto Medina Gutierrez <almedina@microsoft.com>
2020-03-17 16:17:05 -0700

    Solution 1 that I dont like

---
+++ source/uwp/Visualizer/ErrorTabView.xaml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace AdaptiveCardVisualizer
+{
+    public sealed partial class ErrorTabView : UserControl
+    {
+        public ErrorTabView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
