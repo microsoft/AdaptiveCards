f9e6ca9d77def2d736c9739a17f74f8ad2431c6b
David Claux <dclaux@users.noreply.github.com>
2020-03-24 11:05:09 -0700

    Code simplification

--- source/nodejs/adaptivecards-templating/src/template-engine.ts
+++ source/nodejs/adaptivecards-templating/src/template-engine.ts
@@ -1,161 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import * as Shared from "./shared";
 import * as CEL from "adaptive-expressions";
-
-export class TemplateBinding {
-    static parse(expressionString: string): TemplateBinding {
-        let expression = CEL.Expression.parse(expressionString);
-
-        // NOTE: Planned syntax ?# isn't supported yet
-        return new TemplateBinding(expressionString, expression, true /* allowNull */);
-    }
-
-    private constructor(readonly expressionString: string, private readonly expression: CEL.Expression, readonly allowNull: boolean = true) {}
-
-    evaluate(context: EvaluationContext): any {
-        let evaluationResult = this.expression.tryEvaluate(context);
-
-        if (!evaluationResult.error) {
-            return evaluationResult.value;
-        }
-
-        throw new Error(evaluationResult.error);
-    }
-}
-
-class TemplatizedString {
-    private _parts: Array<string | TemplateBinding> = [];
-
-    static parse(s: string): string | TemplatizedString {
-        let result = new TemplatizedString();
-        let i = 0;
-
-        do {
-            let expressionFound = false;
-            let start = i;
-            let loop;
-            
-            do {
-                loop = false;
-
-                start = s.indexOf(Shared.GlobalSettings.expressionStartMarker, start);
-
-                if (start >= 0) {
-                    // This handles the {{...}} syntax used for DATE and TIME functions in the AC renderer
-                    // Should probably be removed if we can find a way to migrate DATE and TIME to the
-                    // expression language.
-                    if (start + Shared.GlobalSettings.expressionStartMarker.length < s.length && s[start + Shared.GlobalSettings.expressionStartMarker.length] === "{") {
-                        start += 2;
-
-                        loop = true;
-                    }
-                }
-            } while (loop);
-
-            if (start >= 0) {
-                let end = s.indexOf(Shared.GlobalSettings.expressionEndMarker, start);
-
-                if (end >= 0) {
-                    expressionFound = true;
-
-                    if (start > i) {
-                        result._parts.push(s.substring(i, start));
-                    }
-
-                    let bindngExpression = s.substring(start + Shared.GlobalSettings.expressionStartMarker.length, end);
-                    let part: string | TemplateBinding;
-
-                    try {
-                        part = TemplateBinding.parse(bindngExpression);
-                    }
-                    catch (e) {
-                        part = bindngExpression;
-                    }
-
-                    result._parts.push(part);
-
-                    i = end + 1;
-                }
-            }
-
-            if (!expressionFound) {
-                result._parts.push(s.substr(i));
-
-                break;
-            }
-        } while (i < s.length);
-
-        if (result._parts.length === 1 && typeof result._parts[0] === "string") {
-            return <string>result._parts[0];
-        }
-        else {
-            return result;
-        }
-    }
-
-    private _shouldDropOwner: boolean = false;
-
-    private evalExpression(binding: TemplateBinding, context: EvaluationContext): any {
-        let result = binding.evaluate(context);
-
-        if (result === undefined) {
-            this._shouldDropOwner = this._shouldDropOwner || !binding.allowNull;
-        }
-
-        return result;
-    }
-
-    private internalEvaluate(context: EvaluationContext): any {
-        if (this._parts.length === 0) {
-            return undefined;
-        }
-        else if (this._parts.length === 1) {
-            // If the templatized string only has 1 part, we want it to evaluate
-            // to same the type as produced by the expression
-            if (typeof this._parts[0] === "string") {
-                return this._parts[0];
-            }
-            else {
-                return this.evalExpression(<TemplateBinding>this._parts[0], context);
-            }
-        }
-        else {
-            // If the templatized string has multiple parts, we want it to evaluate
-            // to a string. In that context, each part that evaluates to undefined
-            // gets replaced by the original expression by default or by a resource
-            // string provided by the application
-            let s = "";
-
-            for (let part of this._parts) {
-                if (typeof part === "string") {
-                    s += part;
-                }
-                else {
-                    let evaluatedPart = this.evalExpression(<TemplateBinding>part, context);
-
-                    if (evaluatedPart === undefined) {
-                        evaluatedPart = Shared.GlobalSettings.undefinedExpressionValueSubstitutionString ? Shared.GlobalSettings.undefinedExpressionValueSubstitutionString : (<TemplateBinding>part).expressionString;
-                    }
-
-                    s += evaluatedPart;
-                }
-            }
-
-            return s;
-        }
-    }
-
-    evaluate(context: EvaluationContext): any {
-        this._shouldDropOwner = false;
-
-        return this.internalEvaluate(context);
-    }
-
-    get shouldDropOwner(): boolean {
-        return this._shouldDropOwner;
-    }
-}
 
 interface EvaluationContextState {
     $data: any;
@@ -204,7 +49,29 @@ export class EvaluationContext {
 export class Template {
     private static prepare(node: any): any {
         if (typeof node === "string") {
-            return TemplatizedString.parse(node);
+            let regExp = /\${([^}]*)}/g;
+            let matches: RegExpExecArray = null;
+            let lastMatch: RegExpExecArray;
+            let matchCount = 0;
+
+            // Determine if the string contains any ${<expression>}
+            while (matchCount <= 1 && (matches = regExp.exec(node)) != null) {
+                lastMatch = matches;
+
+                matchCount++;
+            };
+
+            // If there are none, it's just a string
+            if (matchCount === 0) {
+                return node;
+            }
+            // If the entire string is enclosed in a single ${}, extract the enclosed expression
+            else if (matchCount === 1 && lastMatch[0].length === node.length) {
+                return CEL.Expression.parse(lastMatch[1]);
+            }
+
+            // Otherwise, it's an interpolated string with multiple embedded expressions
+            return CEL.Expression.parse("`" + node + "`");
         }
         else if (typeof node === "object" && node !== null) {
             if (Array.isArray(node)) {
@@ -274,33 +141,28 @@ export class Template {
 
             result = itemArray;
         }
-        else if (node instanceof TemplatizedString) {
-            result = node.evaluate(this._context);
+        else if (node instanceof CEL.Expression) {
+            let evaluationResult = node.tryEvaluate(this._context);
 
-            if (node.shouldDropOwner) {
-                result = null;
+            if (!evaluationResult.error) {
+                result = evaluationResult.value;
+            }
+            else {
+                throw new Error(evaluationResult.error);
             }
         }
         else if (typeof node === "object" && node !== null) {
             let dropObject = false;
             let when = node["$when"];
 
-            if (when instanceof TemplatizedString) {
-                let whenValue: any;
+            if (when instanceof CEL.Expression) {
+                let evaluationResult = when.tryEvaluate(this._context);
+                let whenValue: boolean = false;
                 
-                try {
-                    whenValue = when.evaluate(this._context);
-
-                    // If $when doesn't evaluate to a boolean, consider it is false
-                    if (typeof whenValue !== "boolean") {
-                        whenValue = false;
-                    }
+                // If $when fails to evaluate or evaluates to anything but a boolean, consider it is false
+                if (!evaluationResult.error) {
+                    whenValue = typeof evaluationResult.value === "boolean" && evaluationResult.value;
                 }
-                catch {
-                    // If we hit an exception, consider $when to be false
-                    whenValue = false;
-                }
-
                 dropObject = !whenValue;
             }
 
@@ -308,8 +170,15 @@ export class Template {
                 let dataContext = node["$data"];
 
                 if (dataContext !== undefined) {
-                    if (dataContext instanceof TemplatizedString) {
-                        dataContext = dataContext.evaluate(this._context);
+                    if (dataContext instanceof CEL.Expression) {
+                        let evaluationResult = dataContext.tryEvaluate(this._context);
+
+                        if (!evaluationResult.error) {
+                            dataContext = evaluationResult.value;
+                        }
+                        else {
+                            throw new Error(evaluationResult.error);
+                        }
                     }
 
                     if (Array.isArray(dataContext)) {
