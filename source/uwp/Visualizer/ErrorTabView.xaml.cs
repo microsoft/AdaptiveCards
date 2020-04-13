7d5fc8cba44440e5be6193fdff25305720f3e3bd
Alberto Medina Gutierrez <almedina@microsoft.com>
2020-04-13 11:25:39 -0700

    Add copyright banner and remove extra usings

--- source/uwp/Visualizer/ErrorTabView.xaml.cs
+++ source/uwp/Visualizer/ErrorTabView.xaml.cs
@@ -1,18 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
