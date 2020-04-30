a618dbfa819df0a546acd062ef8f5c87bc8c2772
David Claux <dclaux@users.noreply.github.com>
2020-04-30 15:17:07 -0700

    [Templating] Fix broken {{Date}} and {{TIME}}

--- source/nodejs/adaptivecards-templating/src/template-engine.ts
+++ source/nodejs/adaptivecards-templating/src/template-engine.ts
@@ -210,7 +210,7 @@ export class Template {
      * @returns An Expression object if the provided interpolated string contained at least one expression (e.g. "${expression}"); the original string otherwise.
      */
     public static parseInterpolatedString(interpolatedString: string): AEL.Expression | string {
-        let regExp = /\${([^}]*)}/g;
+        let regExp = /\${([^{}}]+)}/g;
         let matches: RegExpExecArray = null;
         let lastMatch: RegExpExecArray;
         let matchCount = 0;
