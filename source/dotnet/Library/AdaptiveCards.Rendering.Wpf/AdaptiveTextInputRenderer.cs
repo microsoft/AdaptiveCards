7242e4dd0be2a4850804f9636311d9b8b346e184
Alberto Medina Gutierrez <almedina@microsoft.com>
2020-07-02 16:20:32 -0700

    Fix crash when clicking inline actin

--- source/dotnet/Library/AdaptiveCards.Rendering.Wpf/AdaptiveTextInputRenderer.cs
+++ source/dotnet/Library/AdaptiveCards.Rendering.Wpf/AdaptiveTextInputRenderer.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+using System.Dynamic;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -111,6 +112,11 @@ namespace AdaptiveCards.Rendering.Wpf
                 uiTitle.FontSize = context.Config.GetFontSize(AdaptiveFontType.Default, AdaptiveTextSize.Default);
                 uiTitle.Style = context.GetStyle($"Adaptive.Input.Text.InlineAction.Title");
                 uiButton.Content = uiTitle;
+            }
+
+            if (input.InlineAction is AdaptiveSubmitAction)
+            {
+                context.SubmitActionCardId[input.InlineAction as AdaptiveSubmitAction] = context.RenderArgs.ContainerCardId;
             }
 
             uiButton.Click += (sender, e) =>
