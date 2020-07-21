f29dbb5757ddd1c86c4d6da09d011b49fffb50a6
David Claux <dclaux@users.noreply.github.com>
2020-07-21 13:09:53 -0700

    Add extensibility samples (#4131)
    * Add extensibility samples
    
    * Move sample files to other folder
    
    * Fix string
    
    Co-authored-by: shalinijoshi19 <shalinij@microsoft.com>

--- source/nodejs/adaptivecards-designer-app/src/app.ts
+++ source/nodejs/adaptivecards-designer-app/src/app.ts
@@ -4,7 +4,6 @@ import * as monaco from "monaco-editor";
 import * as markdownit from "markdown-it";
 import * as ACDesigner from "adaptivecards-designer";
 import * as ACTemplating from "adaptivecards-templating";
-import * as Adaptive from "adaptivecards";
 import "adaptivecards-designer/dist/adaptivecards-designer.css";
 import "./app.css";
 
