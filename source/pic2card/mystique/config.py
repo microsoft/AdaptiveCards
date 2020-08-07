d3dedbec5a940007b0dedbe3e0ffda1f583e2834
Haridas N <haridas.n@imaginea.com>
2020-08-07 20:48:25 +0530

    [pic2card] Detr cpp binding for model inference (#4550)
    * Detr cpp binding (#17)
    
    * creating python binding using pybind11
    
    * Numpy to cv::Mat conversion via pybind11
    
    * opencv cv::Mat to numpy ndarray type conversion.
    
    * Testcase to validate the detr library
    
    * cpp detr binding tested.
    
    It doesn't require python torch library, instead it will be dynamically
    linked to the libtorch library.
    
    * CMakeLists updates, required for standard binary
    
    * Integration of c++ inference binding.
    
    * Readme update.
    
    * Fixing the hard coded paths in test.
    
    * Python Objec Detection class for detr cpp binding. (#18)

--- source/pic2card/mystique/config.py
+++ source/pic2card/mystique/config.py
@@ -59,7 +59,8 @@ MODEL_REGISTRY = {
     "tf_faster_rcnn": "mystique.detect_objects.ObjectDetection",
     "tfs_faster_rcnn": "mystique.detect_objects.TfsObjectDetection",
     "pth_faster_rcnn": "mystique.obj_detect.PtObjectDetection",
-    "pth_detr": "mystique.obj_detect.DetrOD"
+    "pth_detr": "mystique.obj_detect.DetrOD",
+    "pth_detr_cpp": "mystique.obj_detect.DetrCppOD"
 }
 
 ACTIVE_MODEL_NAME = os.environ.get("ACTIVE_MODEL_NAME", "tf_faster_rcnn")
