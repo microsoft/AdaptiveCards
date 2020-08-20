9e7bbda6b3bc40f1852ac6dc830b15a1412e7f62
Haridas N <haridas.n@imaginea.com>
2020-08-20 16:21:50 +0530

    Addressed dashboard rendering and added detr_cpp image transformation (#4629)
    * Enhance the image view in card dashboard.
    
    * Image transformation aded to detr_cpp pipeline
    
    * Fixed the debug image rendering bugs.
    
    * removed pil image usage from debug api.
    
    * Cleanups in the cpp apis and test codes.
    
    CPP class will load model inside constructor itself.
    
    * Removed model load function.
    
    * Fixed thread safe issue with the dev server.

--- source/pic2card/mystique/utils.py
+++ source/pic2card/mystique/utils.py
@@ -87,8 +87,8 @@ def plot_results(pil_img: Image,
     `Image.open` to render the image.
     """
     label_map = label_map or config.ID_TO_LABEL
-    plt.figure(figsize=(16, 10))
     plt.imshow(pil_img)
+    plt.margins(0, 0)
     plt.axis('off')
     ax = plt.gca()
 
@@ -103,14 +103,15 @@ def plot_results(pil_img: Image,
                                                          COLORS * 100):
 
         ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
-                                   fill=False, color=c, linewidth=3))
+                                   fill=False, color=c, linewidth=1))
         text = f'{label_map[cl_id]}: {score:0.2f}'
-        ax.text(xmin, ymin, text, fontsize=15,
+        ax.text(xmin, ymin, text, fontsize=8,
                 bbox=dict(facecolor='yellow', alpha=0.5))
 
     img_buf = io.BytesIO()
-    plt.savefig(img_buf, format="png")
+    plt.savefig(img_buf, format="png", bbox_inches='tight', pad_inches=0)
     img_buf.seek(0)
+    plt.close()
     return img_buf
 
 
