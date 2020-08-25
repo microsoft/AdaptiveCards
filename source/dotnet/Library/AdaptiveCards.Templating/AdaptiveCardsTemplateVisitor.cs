e352d8462a2626875848e3aa62b796640f9b1413
Alberto Medina Gutierrez <almedina@microsoft.com>
2020-08-25 11:59:11 -0700

    Fix the fix

--- source/dotnet/Library/AdaptiveCards.Templating/AdaptiveCardsTemplateVisitor.cs
+++ source/dotnet/Library/AdaptiveCards.Templating/AdaptiveCardsTemplateVisitor.cs
@@ -167,7 +167,7 @@ namespace AdaptiveCards.Templating
                 throw new ArgumentNullException("Parent data context or selection path is null");
             }
 
-            var (value, error) = new ValueExpression(jpath).TryGetValue(parentDataContext.AELMemory);
+            var (value, error) = new ValueExpression("=" + jpath).TryGetValue(parentDataContext.AELMemory);
             if (error == null)
             {
                 var serializedValue = JsonConvert.SerializeObject(value);
