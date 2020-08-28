9ca5c9004718771b73e34a622ec1791089cfdbd0
David Thompson <davyt23@hotmail.co.uk>
2020-08-29 00:42:39 +0100

    [Xamarin] Implement Toggle Inputs as checkboxes

--- source/dotnet/Library/AdaptiveCards.Xaml.Shared/Rendering/AdaptiveToggleInputRenderer.cs
+++ source/dotnet/Library/AdaptiveCards.Xaml.Shared/Rendering/AdaptiveToggleInputRenderer.cs
@@ -20,15 +20,61 @@ namespace AdaptiveCards.Rendering.Wpf
             uiToggle.Foreground =
                 context.GetColorBrush(context.Config.ContainerStyles.Default.ForegroundColors.Default.Default);
 #elif XAMARIN
-            //AdaptiveChoiceSetRenderer.SetContent(uiToggle, input.Title, input.Wrap);
+
+            var grid = new Grid();
+
+            grid.ColumnSpacing = 0;
+            grid.RowSpacing = 0;
+            grid.Padding = 0;
+            grid.Margin = 0;
+
+            grid.ColumnDefinitions = new ColumnDefinitionCollection
+                    {
+                        new ColumnDefinition() { Width = GridLength.Auto },
+                        new ColumnDefinition() { Width = GridLength.Auto }
+                    };
+
+            uiToggle.Margin = 0;
+
+            uiToggle.IsChecked = input.Value == (input.ValueOn ?? "true");
+
+            uiToggle.Style = context.GetStyle($"Adaptive.Input.Toggle");
+
+            uiToggle.SetContext(input);
+
+            Grid.SetColumn(uiToggle, 0);
+
+            grid.Children.Add(uiToggle);
+
+            TextBlock choiceTextBlock = new TextBlock { Text = input.Title, LineBreakMode = LineBreakMode.NoWrap, TextColor = uiToggle.Color };
+
+            choiceTextBlock.Padding = 0;
+
+            choiceTextBlock.Margin = 0;
+
+            choiceTextBlock.VerticalOptions = LayoutOptions.CenterAndExpand;
+
             uiToggle.Color =
                 context.GetColor(context.Config.ContainerStyles.Default.ForegroundColors.Default.Default);
+
+            Grid.SetColumn(choiceTextBlock, 1);
+
+            grid.Children.Add(choiceTextBlock);
+
+            context.InputBindings.Add(input.Id, () => uiToggle.GetState() == true ? input.ValueOn ?? "true" : input.ValueOff ?? "false");
+
+            return grid;
+            //AdaptiveChoiceSetRenderer.SetContent(uiToggle, input.Title, input.Wrap);
+
 #endif
+
+#if WPF
             uiToggle.SetState(input.Value == (input.ValueOn ?? "true"));
             uiToggle.Style = context.GetStyle($"Adaptive.Input.Toggle");
             uiToggle.SetContext(input);
             context.InputBindings.Add(input.Id, () => uiToggle.GetState() == true ? input.ValueOn ?? "true" : input.ValueOff ?? "false");
             return uiToggle;
+#endif
         }
     }
 
