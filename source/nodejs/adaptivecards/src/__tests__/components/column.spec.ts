7e11aeceaf036c9443400a0158b00948013692cc
Scott Lovegrove <scott@doist.com>
2020-09-15 18:06:23 +0100

    Adds test for background image setter

--- source/nodejs/adaptivecards/src/__tests__/components/column.spec.ts
+++ source/nodejs/adaptivecards/src/__tests__/components/column.spec.ts
@@ -1,8 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { Column } from "../../card-elements";
+import { BackgroundImage, Column } from "../../card-elements";
 
 test('Column should be instantiated', () => {
     const column = new Column();
     expect(column).toEqual(expect.anything());
+})
+
+test('Column can have a backgroundImage set', () =>{
+    const column = new Column();
+    const background = new BackgroundImage();
+    background.url = 'https://adaptivecards.io/content/outlook-logo.png'
+    column.backgroundImage = background;
+
+    expect(column.backgroundImage).not.toBeNull()
 })
