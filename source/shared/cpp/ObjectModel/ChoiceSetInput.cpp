799e0f1e30eddb5dca9e43654f14870a057efa64
nesalang <nesalang@gmail.com>
2020-09-29 10:55:29 -0700

    Fixed bad merge

--- source/shared/cpp/ObjectModel/ChoiceSetInput.cpp
+++ source/shared/cpp/ObjectModel/ChoiceSetInput.cpp
@@ -95,15 +95,6 @@ void ChoiceSetInput::SetWrap(bool value)
 {
     m_wrap = value;
 }
-std::string ChoiceSetInput::GetPlaceholder() const
-{
-    return m_placeholder;
-}
-
-void ChoiceSetInput::SetPlaceholder(const std::string& value)
-{
-    m_placeholder = value;
-}
 
 std::string ChoiceSetInput::GetPlaceholder() const
 {
