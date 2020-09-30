3a33ed8611834160a26f5da01859956acd8e778e
nesalang <Joseph.Woo@microsoft.com>
2020-09-29 17:37:05 -0700

    removed erroneous check-in

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRInputLabelView.mm
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRInputLabelView.mm
@@ -126,12 +126,10 @@
         NSError *error = nil;
         if (NO == [inputHandler validate:&error]) {
             if (self.hasErrorMessage) {
-                inputHandler.hasVisibilityChanged = self.errorMessage.hidden != NO;
                 self.errorMessage.hidden = NO;
             }
         } else {
             if (self.hasErrorMessage) {
-                inputHandler.hasVisibilityChanged = self.errorMessage.hidden != YES;
                 self.errorMessage.hidden = YES;
             }
             self.stack.arrangedSubviews[1].layer.borderWidth = self.validationSuccessBorderWidth;
@@ -212,7 +210,5 @@
 @synthesize hasValidationProperties;
 
 @synthesize id;
-
-@synthesize hasVisibilityChanged;
 
 @end
