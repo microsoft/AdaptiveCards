72e4377796aedf122eee51b07faa383b0e295d53
Paul Campbell (DEP) <paulcam@microsoft.com>
2020-10-01 12:35:59 -0700

    [JS][Designer] Generate stats for webpack-generated projects

--- source/nodejs/adaptivecards-designer-app/webpack.config.js
+++ source/nodejs/adaptivecards-designer-app/webpack.config.js
@@ -4,65 +4,71 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = (env, argv) => {
-	const mode = argv.mode || 'development';
-	const devMode = mode === "development";
+    const mode = argv.mode || 'development';
+    const devMode = mode === "development";
 
-	console.info('running webpack with mode:', mode);
+    console.info('running webpack with mode:', mode);
 
-	return {
-		mode: mode,
-		entry: {
-			"adaptivecards-designer-app": "./src/app.ts",
-		},
-		output: {
-			path: path.resolve(__dirname, "dist"),
-			filename: devMode ? "[name].js" : "[name].min.js",
-		},
-		resolve: {
-			extensions: [".ts", ".tsx", ".js"]
-		},
-		module: {
-			rules: [
-				{
-					test: /\.ts$/,
-					loader: "ts-loader",
-					exclude: /(node_modules|__tests__)/
-				},
-				{
-					test: /\.ttf$/,
-					loader: "file-loader"
-				},
-				{
-					test: /\.css$/,
-					use: [
-						'style-loader',
-						//MiniCssExtractPlugin.loader,
-						'css-loader'
-					]
-				}
-			]
-		},
-		plugins: [
-			//new CleanWebpackPlugin(['dist']),
-			new CopyWebpackPlugin({
-				patterns: [{
-					from: 'node_modules/adaptivecards-designer/dist/containers/*',
-					to: 'containers/',
-					flatten: true
-				}]
-			}),
-			new HtmlWebpackPlugin({
-				title: "Adaptive Cards Designer",
-				template: "./index.html"
-			}),
-			new MiniCssExtractPlugin({
-				filename: '[name].css'
-			}),
-			new MonacoWebpackPlugin({
-				languages: ['json']
-			})
-		]
-	}
+    return {
+        mode: mode,
+        entry: {
+            "adaptivecards-designer-app": "./src/app.ts",
+        },
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: devMode ? "[name].js" : "[name].min.js",
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"]
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    loader: "ts-loader",
+                    exclude: /(node_modules|__tests__)/
+                },
+                {
+                    test: /\.ttf$/,
+                    loader: "file-loader"
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        //MiniCssExtractPlugin.loader,
+                        'css-loader'
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            //new CleanWebpackPlugin(['dist']),
+            new CopyWebpackPlugin({
+                patterns: [{
+                    from: 'node_modules/adaptivecards-designer/dist/containers/*',
+                    to: 'containers/',
+                    flatten: true
+                }]
+            }),
+            new HtmlWebpackPlugin({
+                title: "Adaptive Cards Designer",
+                template: "./index.html"
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].css'
+            }),
+            new MonacoWebpackPlugin({
+                languages: ['json']
+            }),
+            new BundleAnalyzerPlugin({
+                analyzerMode: "disabled",
+                generateStatsFile: true,
+                openAnalyzer: false
+            })
+        ]
+    }
 }
--- source/nodejs/adaptivecards-templating/webpack.config.js
+++ source/nodejs/adaptivecards-templating/webpack.config.js
@@ -1,37 +1,45 @@
 const path = require("path");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = (env, argv) => {
-	const mode = argv.mode || 'development';
-	const devMode = mode === "development";
+    const mode = argv.mode || 'development';
+    const devMode = mode === "development";
 
-	console.info('running webpack with mode:', mode);
+    console.info('running webpack with mode:', mode);
 
-	return {
-		mode: mode,
-		entry: {
-			"adaptivecards-templating": "./src/adaptivecards-templating.ts"
-		},
-		output: {
-			path: path.resolve(__dirname, "./dist"),
-			filename: devMode ? "[name].js" : "[name].min.js",
-			libraryTarget: "umd",
-			library: "ACData",
-			globalObject: "this"
-		},
-		devtool: devMode ? "inline-source-map" : "source-map",
-		devServer: {
-			contentBase: './dist'
-		},
-		resolve: {
-			extensions: [".ts", ".tsx", ".js"]
-		},
-		module: {
-			rules: [{
-					test: /\.ts$/,
-					loader: "ts-loader",
-					exclude: /(node_modules|__tests__)/
-				}
-			]
-		}
-	};
+    return {
+        mode: mode,
+        entry: {
+            "adaptivecards-templating": "./src/adaptivecards-templating.ts"
+        },
+        output: {
+            path: path.resolve(__dirname, "./dist"),
+            filename: devMode ? "[name].js" : "[name].min.js",
+            libraryTarget: "umd",
+            library: "ACData",
+            globalObject: "this"
+        },
+        devtool: devMode ? "inline-source-map" : "source-map",
+        devServer: {
+            contentBase: './dist'
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"]
+        },
+        module: {
+            rules: [{
+                    test: /\.ts$/,
+                    loader: "ts-loader",
+                    exclude: /(node_modules|__tests__)/
+                }
+            ]
+        },
+        plugins: [
+            new BundleAnalyzerPlugin({
+                analyzerMode: "disabled",
+                generateStatsFile: true,
+                openAnalyzer: false
+            })
+        ]
+    };
 }
