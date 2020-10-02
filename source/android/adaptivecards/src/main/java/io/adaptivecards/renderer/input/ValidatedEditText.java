0c0cefb1e90b3da40fbc7f16b1fed01fe98c0d89
Alberto Medina Gutierrez <almedina@microsoft.com>
2020-10-01 17:53:48 -0700

    Change input hint to rectangle around input

--- source/android/adaptivecards/src/main/java/io/adaptivecards/renderer/input/ValidatedEditText.java
+++ source/android/adaptivecards/src/main/java/io/adaptivecards/renderer/input/ValidatedEditText.java
@@ -2,7 +2,9 @@ package io.adaptivecards.renderer.input;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -15,7 +17,7 @@ public class ValidatedEditText extends EditText
     public ValidatedEditText(Context context) {
         super(context);
         setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        getBackground().setColorFilter(null);
+        m_paint.setColor(Color.TRANSPARENT);
     }
 
     public ValidatedEditText(Context context, int errorColor) {
@@ -23,19 +25,41 @@ public class ValidatedEditText extends EditText
         m_errorColor = errorColor;
     }
 
+    @SuppressLint("Range")
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+
+        // Left vertical line
+        canvas.drawRect(0, 0, borderWidth, getHeight(), m_paint);
+
+        // Top horizontal line
+        canvas.drawRect(0, 0, getWidth(), borderWidth, m_paint);
+
+        // Bottom horizontal line
+        canvas.drawRect(0, getHeight() - borderWidth, getWidth(), getHeight(), m_paint);
+
+        // Right vertical line
+        canvas.drawRect(getWidth() - borderWidth, 0, getWidth(), getHeight(), m_paint);
+    }
+
     public void setValidationResult(boolean isValid)
     {
         if (isValid)
         {
-            // Change border color to original color
-            getBackground().setColorFilter(null);
+            // Change border color to transparent color
+            m_paint.setColor(Color.TRANSPARENT);
         }
         else
         {
             // Change border to red
-            getBackground().setColorFilter(m_errorColor, PorterDuff.Mode.SRC_ATOP);
+            m_paint.setColor(m_errorColor);
         }
+        invalidate();
     }
 
-    private int m_errorColor;
+    private int m_errorColor = Color.TRANSPARENT;
+    private final Paint m_paint = new Paint();
+    private final float borderWidth = 5.0f;
 }
