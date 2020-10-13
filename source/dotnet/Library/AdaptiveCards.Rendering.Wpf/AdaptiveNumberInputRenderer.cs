b2ce8f716fdd6173d1a2b8f26f0b320b87ecaa12
RebeccaAnne <rebecch@microsoft.com>
2020-10-13 15:28:20 -0700

    [.NET] Don't set number input text box value to NaN. (#4911)

--- source/dotnet/Library/AdaptiveCards.Rendering.Wpf/AdaptiveNumberInputRenderer.cs
+++ source/dotnet/Library/AdaptiveCards.Rendering.Wpf/AdaptiveNumberInputRenderer.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+using System;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -10,7 +11,12 @@ namespace AdaptiveCards.Rendering.Wpf
     {
         public static FrameworkElement Render(AdaptiveNumberInput input, AdaptiveRenderContext context)
         {
-            var textBox = new TextBox() { Text = input.Value.ToString() };
+            var textBox = new TextBox();
+
+            if (!Double.IsNaN(input.Value))
+            {
+                textBox.Text = input.Value.ToString();
+            }
             textBox.SetPlaceholder(input.Placeholder);
             textBox.Style = context.GetStyle($"Adaptive.Input.Text.Number");
             textBox.SetContext(input);
