ff4b7684730cd3f03e995bcee26bb2578fbdd9a8
Risheek Rajolu <risheekrr@gmail.com>
2020-10-22 16:59:18 -0400

    Append error to label

--- source/android/adaptivecards/src/main/java/io/adaptivecards/renderer/layout/StretchableInputLayout.java
+++ source/android/adaptivecards/src/main/java/io/adaptivecards/renderer/layout/StretchableInputLayout.java
@@ -1,16 +1,32 @@
 package io.adaptivecards.renderer.layout;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import io.adaptivecards.renderer.BaseCardElementRenderer;
+import io.adaptivecards.renderer.input.ValidatedEditText;
 
 /**
  * This class is only supposed to be used for inputs that have: label, validation, isRequired or stretch height
  */
 public class StretchableInputLayout extends StretchableElementLayout
 {
+    private TextView m_label = null;
+    private View m_inputView = null;
+    private TextView m_errorMessage = null;
+//    private TextView m_errorLabel;
+    private boolean isValid = true;
+
     public StretchableInputLayout(Context context, boolean mustStretch)
     {
         super(context, mustStretch);
+
+        // Using setContentDescription on EditText is discouraged, so using hidden TextView instead
+//        m_errorLabel = new TextView(context);
     }
 
     public View getLabel()
@@ -18,7 +34,7 @@ public class StretchableInputLayout extends StretchableElementLayout
         return m_label;
     }
 
-    public void setLabel(View label)
+    public void setLabel(TextView label)
     {
         addView(label);
         m_label = label;
@@ -33,6 +49,10 @@ public class StretchableInputLayout extends StretchableElementLayout
     {
         addView(input);
         m_inputView = input;
+//        if(m_inputView.getId() == View.NO_ID) {
+//            m_inputView.setId(View.generateViewId());
+//        }
+        m_label.setLabelFor(m_inputView.getId());
     }
 
     public View getErrorMessage()
@@ -40,13 +60,36 @@ public class StretchableInputLayout extends StretchableElementLayout
         return m_errorMessage;
     }
 
-    public void setErrorMessage(View errorMessage)
+    public void setErrorMessage(TextView errorMessage)
     {
         addView(errorMessage);
         m_errorMessage = errorMessage;
+        m_errorMessage.setImportantForAccessibility(IMPORTANT_FOR_ACCESSIBILITY_NO);
     }
 
-    private View m_label = null;
-    private View m_inputView = null;
-    private View m_errorMessage = null;
+    public void setValidationResult(boolean isValid)
+    {
+        BaseCardElementRenderer.setVisibility(!isValid, m_errorMessage);
+        if(m_inputView instanceof ValidatedEditText)
+        {
+            ((ValidatedEditText) m_inputView).setValidationResult(isValid);
+        }
+        if(isValid)
+        {
+            m_label.setContentDescription(null);
+        }
+        else
+        {
+            m_label.setContentDescription(m_label.getText() + " " + m_errorMessage.getText());
+        }
+    }
+
+//    private void updateAccessbleLabel() {
+//        m_accessibleLabel.removeAllViews();
+//        m_accessibleLabel.addView(m_label);
+//        m_accessibleLabel.addView(m_errorMessage);
+//    }
+
+
+
 }
