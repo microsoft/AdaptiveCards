f14c5e753255d0c61beb80f4db0a53a31d55a50f
Ibrahim Sulaiman <ibrahim.sulaiman@imaginea.com>
2020-12-08 17:18:21 +0530

    Handled the custom action for the selctAction type

--- source/community/reactnative/src/components/actions/select-Action.js
+++ source/community/reactnative/src/components/actions/select-Action.js
@@ -12,31 +12,25 @@ import {
 } from '../../utils/context';
 import * as Constants from '../../utils/constants';
 import * as Utils from '../../utils/util';
+import { HostConfigManager } from '../../utils/host-config';
 
 export class SelectAction extends React.Component {
 
 	static contextType = InputContext;
-	onExecuteAction = undefined;
-	toggleVisibilityForElementWithID = undefined;
-	render() {
-		const ButtonComponent = TouchableOpacity;
-		return (<InputContextConsumer>
-			{({ onExecuteAction, toggleVisibilityForElementWithID }) => {
-				this.onExecuteAction = onExecuteAction;
-				this.toggleVisibilityForElementWithID = toggleVisibilityForElementWithID;
 
-				return <ButtonComponent onPress={() => { this.onClickHandle() }} style={this.props.style}>
-					<React.Fragment>{this.props.children}</React.Fragment>
-				</ButtonComponent>
-			}}
-		</InputContextConsumer>);
+	constructor(props) {
+		super(props);
+
+		this.payload = this.props.selectActionData;
+		this.onExecuteAction = undefined;
+		this.toggleVisibilityForElementWithID = undefined;
 	}
 
 	/**
 	 * @description Invoked on tapping the button component
 	 */
 	onClickHandle() {
-		switch (this.props.selectActionData.type) {
+		switch (this.payload.type) {
 			case Constants.ActionSubmit:
 				let actionObject = { "type": Constants.ActionSubmit, "data": this.props.selectActionData.data };
 				this.onExecuteAction(actionObject);
@@ -51,7 +45,39 @@ export class SelectAction extends React.Component {
 				this.toggleVisibilityForElementWithID(this.props.selectActionData.targetElements);
 				break;
 			default:
+				this.onCustomActionCalled();
 				break;
 		}
+	}
+
+	/**
+	 * @description Invoked for the custom action type
+	 */
+	onCustomActionCalled() {
+		let actionObject = {};
+		for (let key in this.payload) {
+			if (this.payload.hasOwnProperty(key)) {
+				actionObject[key] = this.payload[key];
+			}
+		}
+		this.onExecuteAction(actionObject);
+	}
+
+	render() {
+		if (HostConfigManager.getHostConfig().supportsInteractivity === false) {
+			return null;
+		}
+
+		const ButtonComponent = TouchableOpacity;
+		return (<InputContextConsumer>
+			{({ onExecuteAction, toggleVisibilityForElementWithID }) => {
+				this.onExecuteAction = onExecuteAction;
+				this.toggleVisibilityForElementWithID = toggleVisibilityForElementWithID;
+
+				return <ButtonComponent onPress={() => { this.onClickHandle() }} style={this.props.style}>
+					<React.Fragment>{this.props.children}</React.Fragment>
+				</ButtonComponent>
+			}}
+		</InputContextConsumer>);
 	}
 }
