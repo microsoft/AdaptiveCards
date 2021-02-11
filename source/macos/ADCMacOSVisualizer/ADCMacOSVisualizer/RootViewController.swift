c42a2fa2f6dfa66db77daa204a712515da30c48c
blue-stone99 <75681162+blue-stone99@users.noreply.github.com>
2021-02-11 13:08:23 +0530

    wired samples to render (#4)

--- source/macos/ADCMacOSVisualizer/ADCMacOSVisualizer/RootViewController.swift
+++ source/macos/ADCMacOSVisualizer/ADCMacOSVisualizer/RootViewController.swift
@@ -1,17 +1,17 @@
+import AdaptiveCards
 import Cocoa
 
 class RootViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
     @IBOutlet var tableView: NSTableView!
-        
+    @IBOutlet var stackView: NSStackView!
+    
     var items: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let main = Bundle.main
-        guard let sample = main.resourcePath?.appending("/samples") else {
-            return
-        }
         let filesManager = FileManager.default
+        guard let sample = main.resourcePath?.appending("/samples") else { return }
         do {
             items = try filesManager.contentsOfDirectory(atPath: sample)
         } catch {
@@ -19,7 +19,35 @@ class RootViewController: NSViewController, NSTableViewDelegate, NSTableViewData
         }
         tableView.rowHeight = 32
     }
+    
+    // MARK: Private Methods
+    private func renderCard(with jsonString: String) {
+        let result = AdaptiveCard.from(jsonString: jsonString)
+        guard let card = result.getAdaptiveCard() else {
+            print("RENDER FAILED")
+            return
+        }
+        print("ELEMENTS:")
+        card.getBody().forEach { print($0.getTypeString() ?? "nil") }
+        card.getBody().forEach { print(type(of: $0)) }
+        
+        switch AdaptiveCard.parseHostConfig(from: hostConfig) {
+        case .success(let config):
+            print("PARSED HOSTCONFIG")
+            print("Font family - \(config.getFontFamily() ?? "nil")")
+            
+            let cardView = AdaptiveCard.render(card: card, with: config, width: 335)
+            if let renderedView = stackView.arrangedSubviews.first {
+                renderedView.removeFromSuperview()
+            }
+            stackView.addArrangedSubview(cardView)
+            
+        case .failure(let error):
+            print("Failure: \(error.localizedDescription)")
+        }
+    }
 
+    // MARK: TableView Datasource
     func numberOfRows(in tableView: NSTableView) -> Int {
         return items.count
     }
@@ -32,15 +60,15 @@ class RootViewController: NSViewController, NSTableViewDelegate, NSTableViewData
         let selectedRowNum = tableView.selectedRow
         let path = "/samples/\(items[selectedRowNum])"
         let parts = path.components(separatedBy: ".")
-        if let filepath = Bundle.main.path(forResource: parts[0], ofType: "json") {
-            do {
-                let contents = try String(contentsOfFile: filepath)
-                print(contents)
-            } catch {
-                print(error)
-            }
-        } else {
-            print("Not found")
+        guard let filepath = Bundle.main.path(forResource: parts[0], ofType: "json") else {
+            print("File Not found: \(items[selectedRowNum])")
+            return
+        }
+        do {
+            let contents = try String(contentsOfFile: filepath)
+            renderCard(with: contents)
+        } catch {
+            print(error)
         }
     }
 }
