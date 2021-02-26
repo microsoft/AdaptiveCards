6d456f278c6372bcd9720fa25c7ff4fad32cdff7
Robin Liao <yilia@microsoft.com>
2021-02-25 21:50:38 -0800

    fix PR build

--- source/nodejs/adaptivecards/src/__tests__/components/overflow_action.spec.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-import { OverflowAction, Action } from "../../card-elements";
-
-test('OverflowAction should be instantiated', () => {
-    const actions: Action[] = [];
-    const actionSet = new OverflowAction(actions);
-    expect(actionSet).toEqual(expect.anything());
-})
