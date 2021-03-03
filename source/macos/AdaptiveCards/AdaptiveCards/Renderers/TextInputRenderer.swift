5aa822e2619113b77425e1a97872385cfdf817fa
Rohit Sharma <78854679+rohshar6@users.noreply.github.com>
2021-03-03 17:36:30 +0530

    stackview added and updates made (#40)
    Signed-off-by: rohshar6 <rohshar6@cisco.com>

--- source/macos/AdaptiveCards/AdaptiveCards/Renderers/TextInputRenderer.swift
+++ source/macos/AdaptiveCards/AdaptiveCards/Renderers/TextInputRenderer.swift
@@ -9,7 +9,12 @@ class TextInputRenderer: NSObject, BaseCardElementRendererProtocol {
             logError("Element is not of type ACSTextInput")
             return NSView()
         }
-        
+        let stackview: NSStackView = {
+            let view = NSStackView()
+            view.orientation = .horizontal
+            view.translatesAutoresizingMaskIntoConstraints = false
+            return view
+        }()
         let textView = ACRTextInputView()
         var attributedInitialValue: NSMutableAttributedString
         
@@ -19,6 +24,18 @@ class TextInputRenderer: NSObject, BaseCardElementRendererProtocol {
 
         if let maxLen = inputBlock.getMaxLength(), Int(truncating: maxLen) > 0 {
             textView.maxLen = Int(truncating: maxLen)
+        }
+        let action = inputBlock.getInlineAction()
+        var renderButton: Bool = false
+        switch action {
+        case is ACSOpenUrlAction:
+            renderButton = true
+        case is ACSToggleVisibilityAction:
+            renderButton = true
+        case is ACSSubmitAction:
+            renderButton = true
+        default:
+            renderButton = false
         }
         
         if inputBlock.getIsMultiline() {
@@ -30,6 +47,11 @@ class TextInputRenderer: NSObject, BaseCardElementRendererProtocol {
                 multilineView.setValue(value: valueString, maximumLen: inputBlock.getMaxLength())
             }
             multilineView.maxLen = inputBlock.getMaxLength() as? Int ?? 0
+            if renderButton {
+                stackview.addArrangedSubview(multilineView)
+                addInlineButton(parentview: stackview, view: multilineView, element: inputBlock, style: style, with: hostConfig)
+                return stackview
+            }
             return multilineView
         } else {
             // Makes text remain in 1 line
@@ -52,7 +74,39 @@ class TextInputRenderer: NSObject, BaseCardElementRendererProtocol {
             }
             textView.attributedStringValue = attributedInitialValue
         }
+        if renderButton {
+            stackview.addArrangedSubview(textView)
+            addInlineButton(parentview: stackview, view: textView, element: inputBlock, style: style, with: hostConfig)
+            return stackview
+        }
         return textView
+    }
+    private func addInlineButton(parentview: NSStackView, view: NSView, element: ACSTextInput, style: ACSContainerStyle, with hostConfig: ACSHostConfig) {
+        let action = element.getInlineAction()
+        let button = NSButton(title: action?.getTitle() ?? "", target: self, action: Selector(("test")))
+        let attributedString: NSMutableAttributedString
+        attributedString = NSMutableAttributedString(string: button.title)
+        if let colorHex = hostConfig.getForegroundColor(style, color: .default, isSubtle: true), let textColor = ColorUtils.color(from: colorHex) {
+            attributedString.addAttributes([.foregroundColor: textColor], range: NSRange(location: 0, length: attributedString.length))
+        }
+        parentview.addArrangedSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isBordered = false
+        button.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        button.widthAnchor.constraint(lessThanOrEqualTo: view.widthAnchor, multiplier: 0.5).isActive = true
+        button.attributedTitle = attributedString
+        // image icon
+        if let imageIcon = action?.getIconUrl(), !imageIcon.isEmpty {
+            guard let url = URL(string: imageIcon) else { return }
+            DispatchQueue.global().async {
+                guard let data = try? Data(contentsOf: url) else { return }
+                DispatchQueue.main.async {
+                    let image = NSImage(data: data)
+                    image?.size = .init(width: button.bounds.width, height: button.bounds.height)
+                    button.image = image
+                }
+            }
+        }
     }
 }
 class ACRTextInputView: NSTextField, NSTextFieldDelegate {
