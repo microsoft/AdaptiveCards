e2b3c99be754d589c1f5dd51331b8d509d17ae8d
Paul Campbell <paulcam@microsoft.com>
2021-03-24 14:48:38 -0700

    per-language explorer generation

--- source/nodejs/adaptivecards-site/themes/adaptivecards/scripts/generator-explorer.js
+++ source/nodejs/adaptivecards-site/themes/adaptivecards/scripts/generator-explorer.js
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 "use strict";
 var typedschema = require("ac-typed-schema");
+var glob = require("glob");
 var path = require("path");
 
 hexo.extend.generator.register("generator-explorer", function (locals) {
@@ -9,50 +10,54 @@ hexo.extend.generator.register("generator-explorer", function (locals) {
     return new Promise(function (resolve, reject) {
 
 		try {
-			var schemaModel = typedschema.markdown.buildModel({
-				schema: "../../../schemas/src",
-				toc: "./schema-explorer-toc.yml",
-				rootDefinition: "AdaptiveCard",
-				examplesPath: "../../../samples"
-			});
-			
-			var pages = [];
+            var languages = glob.sync("./languages/*");
+            languages.forEach(function(languageDir) {
+                var language = languageDir.substring(languageDir.lastIndexOf("/")+1);
+			    var schemaModel = typedschema.markdown.buildModel({
+				    schema: "../../../schemas/src",
+				    toc: path.join(languageDir, "schema-explorer-toc.yml"),
+				    rootDefinition: "AdaptiveCard",
+				    examplesPath: "../../../samples"
+			    });
 
-			schemaModel.forEach(function (root) {
-				root.children.forEach(function (child) {
-					child.htmlPath = "explorer/" + child.name + ".html";
-					var page = {
-						path: child.htmlPath,
-						layout: "explorer",
-						data: {
-							title: "Schema Explorer",
-							schema: schemaModel,
-							element: child,
-							childPath: child.htmlPath,
-							propertiesSummary: typedschema.markdown.createPropertiesSummary(child.type, null, true, true, child.version)
-						}
-					}
+			    var pages = [];
 
-					pages.push(page);
+			    schemaModel.forEach(function (root) {
+				    root.children.forEach(function (child) {
+					    child.htmlPath = language + "/explorer/" + child.name + ".html";
+					    var page = {
+						    path: child.htmlPath,
+						    layout: "explorer",
+						    data: {
+							    title: "Schema Explorer",
+							    schema: schemaModel,
+							    element: child,
+							    childPath: child.htmlPath,
+							    propertiesSummary: typedschema.markdown.createPropertiesSummary(child.type, null, true, true, child.version)
+						    }
+					    }
 
-					// Set the default root as the index
-					if (child.name === "AdaptiveCard") {
-						pages.push({
-							path: "explorer/index.html",
-							layout: "explorer",
-							data: {
-								title: "Schema Explorer",
-								schema: schemaModel,
-								element: child,
-								childPath: child.htmlPath,
-								propertiesSummary: typedschema.markdown.createPropertiesSummary(child.type, null, false, true)
-							}
-						});
-					}
-				});
-			});
+					    pages.push(page);
 
-			resolve(pages);
+					    // Set the default root as the index
+					    if (child.name === "AdaptiveCard") {
+						    pages.push({
+							    path: language + "/explorer/" + "index.html",
+							    layout: "explorer",
+							    data: {
+								    title: "Schema Explorer",
+								    schema: schemaModel,
+								    element: child,
+								    childPath: child.htmlPath,
+								    propertiesSummary: typedschema.markdown.createPropertiesSummary(child.type, null, false, true)
+							    }
+						    });
+					    }
+				    });
+			    });
+                resolve(pages);
+            });
+
 		} catch (err) {
 			reject(err);
 		}
