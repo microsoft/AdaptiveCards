dfeb34a92d97de83bbffecc5b4934ea372ca743a
Paul Campbell (DEP) <paulcam@microsoft.com>
2021-04-27 16:02:35 -0700

    [Public Sample] NodeJS Sample Commit #1

---
+++ sampleApps/nodejs/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+	const mode = argv.mode || 'development';
+	const devMode = mode === "development";
+
+	console.info('running webpack with mode:', mode);
+
+	return {
+		mode: mode,
+		entry: {
+			"ac-sample": "./src/ac-sample.ts"
+		},
+		output: {
+			path: path.resolve(__dirname, "./dist"),
+			filename: devMode ? "[name].js" : "[name].min.js",
+			library: "ac-sample",
+			libraryTarget: "umd",
+			globalObject: "this"
+		},
+		devtool: devMode ? "inline-source-map" : "source-map",
+		devServer: {
+			contentBase: './dist'
+		},
+		resolve: {
+			extensions: [".ts", ".tsx", ".js"]
+		},
+		module: {
+			rules: [{
+				test: /\.ts$/,
+				loader: "ts-loader",
+				exclude: /(node_modules|__tests__)/
+			}]
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				title: "AC Sample Site",
+				template: "./index.html",
+				filename: "index.html",
+				chunks: ["ac-sample"]
+			})
+		]
+    }
+}
