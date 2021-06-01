c469f484930fa75b8be9310d7b7fba20a0983824
Alberto Medina Gutierrez <almedina@microsoft.com>
2021-06-01 16:53:13 -0700

    Add skeleton of test app

---
+++ source/uwp/UWPUITests/UnitTest.cs
@@ -0,0 +1,18 @@
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UWPUITests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
