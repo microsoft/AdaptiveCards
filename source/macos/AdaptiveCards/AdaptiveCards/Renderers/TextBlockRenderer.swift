c7366ebeddcba8558454561750a3160c4d736153
mukuagar <78855675+mukuagar@users.noreply.github.com>
2021-06-10 17:52:57 +0530

    Fixed multiple selection issue (#190)
    * Fixed multiple selection issue
    
    * Minor change

--- source/macos/AdaptiveCards/AdaptiveCards/Renderers/TextBlockRenderer.swift
+++ source/macos/AdaptiveCards/AdaptiveCards/Renderers/TextBlockRenderer.swift
@@ -90,6 +90,7 @@ class ACRTextView: NSTextView, SelectActionHandlingProtocol {
     
     override func resignFirstResponder() -> Bool {
         self.needsDisplay = true
+        clearSelectedRange()
         return super.resignFirstResponder()
     }
     
@@ -116,6 +117,13 @@ class ACRTextView: NSTextView, SelectActionHandlingProtocol {
             }
         }
         return super.hitTest(point)
+    }
+    
+    func clearSelectedRange() {
+        let selectedString = (string as NSString).substring(with: selectedRange())
+        if !selectedString.isEmpty {
+            setSelectedRange(NSRange(location: 0, length: 0))
+        }
     }
 }
 
