54bc9b87eec0e83b620f004783e311536d3c5c41
Alberto Medina Gutierrez <almedina@microsoft.com>
2021-06-24 15:16:12 -0700

    Fix ccopyright banner

--- source/android/uitestapp/src/androidTest/java/io.adaptivecards.uitestapp.testing/UiTests.java
+++ source/android/uitestapp/src/androidTest/java/io.adaptivecards.uitestapp.testing/UiTests.java
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package io.adaptivecards.uitestapp.testing;
 
 import io.adaptivecards.uitestapp.MainActivityUITestApp;
