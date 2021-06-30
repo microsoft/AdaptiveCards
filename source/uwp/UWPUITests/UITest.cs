d758a08de64eb4171985f7656bcb851fbaa90dd7
Alberto Medina Gutierrez <almedina@microsoft.com>
2021-06-30 15:37:16 -0700

    Add comments to test

--- source/uwp/UWPUITests/UITest.cs
+++ source/uwp/UWPUITests/UITest.cs
@@ -24,23 +24,33 @@ namespace UWPUITestLibraryMITA
         {
             TestHelpers.GoToTestCase("ActivityUpdate");
 
+            // Retrieve the "Set due date" Button and click it
             var showCardButton = TestHelpers.FindElementByName("Set due date");
             Assert.IsNotNull(showCardButton, "Could not find 'Set due date' button");
             showCardButton.Click();
 
+            // Retrieve the date input and click on it
+            // TODO: Fix UWP implementation to assign name/id to the CalendarDatePicker control
             var dateInput = TestHelpers.FindElementByClassName("CalendarDatePicker");
             dateInput.Click();
 
+            // Retrieve the popup which contains the calendar view
             var calendarView = TestHelpers.FindFlyoutByAutomationId("CalendarView");
+            // Click on the "Next" button inside the popup
             TestHelpers.FindElementByAutomationId("NextButton", calendarView).Click();
+            // Wait for the scrolling animation to end
             Thread.Sleep(1000);
+            // Click on the day "16" button which in turn closes the popup
             TestHelpers.FindElementByName("16", calendarView).Click();
 
+            // Retrieve the "Add a comment" Input.Text and fill it with information
             var commentTextBox = TestHelpers.CastTo<Edit>(TestHelpers.FindElementByName("Add a comment"));
             commentTextBox.SendKeys("A comment");
 
+            // Submit data
             TestHelpers.FindElementByName("OK").Click();
 
+            // Verify submitted data
             Assert.AreEqual("A comment", TestHelpers.GetInputValue("comment"), "Values for input comment differ");
             Assert.AreEqual("2021-07-16", TestHelpers.GetInputValue("dueDate"), "Values for input dueDate differ");
         }
@@ -50,14 +60,18 @@ namespace UWPUITestLibraryMITA
         {
             TestHelpers.GoToTestCase("Input.Text.ErrorMessage");
 
+            // Click on 'Submit' button
             var showCardButton = TestHelpers.FindElementByName("Submit");
             Assert.IsNotNull(showCardButton, "Could not find 'Submit' button");
             showCardButton.Click();
 
+            // Find the 'Required Input.Text' Input.Text,
+            // as both the Label and the TextBox share the same name we have to discern between them
             var requiredInputTextBox = TestHelpers.FindByMultiple(
                 "Name", "Required Input.Text *\r\n",
                 "ClassName", "TextBox");
 
+            // Verify the retrieved Input.Text has the keyboard focus
             Assert.IsTrue(requiredInputTextBox.HasKeyboardFocus, "The first textblock did not get focus");
         }
 
