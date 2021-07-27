60d35f3c1166497694a4631a1b359c94386aabdf
Mara Kim <marakim@microsoft.com>
2021-07-27 15:46:25 -0700

    Skeleton

---
+++ source/nodejs/react-adaptivecards/README.md
@@ -0,0 +1,17 @@
+# React.js Adaptive Cards component
+
+## Install and import the module
+
+### Node
+
+```console
+npm install react adaptivecards react-adaptivecards --save
+```
+
+```js
+// Import the module:
+import * as AdaptiveCard from "react-adaptivecards";
+
+// OR require it:
+var AdaptiveCard = require("react-adaptivecards");
+```
---
+++ source/nodejs/react-adaptivecards/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+
+module.exports = (env, argv) => {
+	const mode = argv.mode || 'development';
+	const devMode = mode === "development";
+
+	console.info('running webpack with mode:', mode);
+
+	return {
+		mode: mode,
+		entry: {
+			"react-adaptivecards": "./src/react-adaptivecards.ts"
+		},
+		output: {
+			path: path.resolve(__dirname, "./dist"),
+			filename: devMode ? "[name].js" : "[name].min.js",
+			libraryTarget: "umd",
+			library: "AdaptiveCards",
+			globalObject: "this"
+		},
+		devtool: devMode ? "inline-source-map" : "source-map",
+		devServer: {
+			contentBase: './dist'
+		},
+		externals: {
+			"react": {
+				commonjs2: "react",
+				commonjs: "react",
+				root: "react"
+			}
+		},
+		resolve: {
+			extensions: [".ts", ".tsx", ".js"]
+		},
+		module: {
+			rules: [{
+					test: /\.ts$/,
+					loader: "ts-loader",
+					exclude: /(node_modules|__tests__)/
+				}
+			]
+		}
+	};
+}
