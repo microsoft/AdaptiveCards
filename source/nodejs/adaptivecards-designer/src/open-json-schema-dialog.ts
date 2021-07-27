6457fd7d9211afe43ba0f2202528308206312f3d
Mara Kim <marakim@microsoft.com>
2021-07-26 17:17:27 -0700

    Create dialog

---
+++ source/nodejs/adaptivecards-designer/src/open-json-schema-dialog.ts
@@ -0,0 +1,122 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import * as Adaptive from "adaptivecards";
+import * as monaco from "monaco-editor";
+import { Dialog, DialogButton } from "./dialog";
+import { CardData } from "./open-card-dialog";
+import { JSONSchemaCard } from "adaptivecards-templating";
+
+export class OpenJsonSchemaDialog extends Dialog {
+    private _output: CardData;
+    private _previewContainer: HTMLElement;
+    private _previewElement: HTMLElement;
+    private _editor: monaco.editor.IStandaloneCodeEditor;
+
+    readonly submitButton: DialogButton;
+
+    constructor() {
+        super();
+
+        this.submitButton = new DialogButton("Create");
+        this.submitButton.onClick = (sender) => {
+            this.close();
+        }
+        this.buttons = [this.submitButton];
+    }
+
+    open() {
+        super.open();
+        this._editor.layout();
+    }
+
+    private renderPreview(): HTMLElement {
+        const renderedElement = document.createElement("div");
+        renderedElement.className = "acd-open-code-dialog-preview";
+
+        if (this.output?.cardPayload) {
+            try {
+                const cardData = JSON.parse(this.output.cardPayload);
+                const card = new Adaptive.AdaptiveCard();
+                card.parse(cardData);
+                card.render();
+                card.renderedElement.style.width = "100%";
+
+                renderedElement.appendChild(card.renderedElement);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        return renderedElement
+    }
+
+    private refreshPreview() {
+        this._previewContainer.removeChild(this._previewElement);
+        this._previewElement = this.renderPreview();
+        this._previewContainer.appendChild(this._previewElement);
+    }
+
+    private renderEditor(): HTMLElement {
+        const renderedElement = document.createElement("div");
+        renderedElement.className = "acd-open-code-dialog-editor";
+
+        const monaco = window["monaco"];
+        this._editor = monaco.editor.create(
+            renderedElement,
+            {
+                folding: true,
+                fontSize: 13.5,
+                language: 'json',
+            }
+        );
+        this._editor.layout();
+
+        this._editor.onDidChangeModelContent(() => {
+            try {
+                const model = this._editor.getValue();
+                const json = JSON.parse(model);
+
+                const card = JSONSchemaCard(json);
+                this._output = {
+                    cardPayload: JSON.stringify(card),
+                };
+
+                this.refreshPreview();
+            } catch (e) {
+                console.error(e)
+            }
+        });
+
+        return renderedElement
+    }
+
+    protected renderContent(): HTMLElement {
+        const renderedElement = document.createElement("div");
+        renderedElement.className = "acd-open-code-dialog-container";
+
+        this._previewContainer = document.createElement("div");
+        this._previewContainer.className = "acd-open-code-dialog-preview-container";
+        renderedElement.appendChild(this._previewContainer);
+
+        this._previewElement = this.renderPreview()
+        this._previewContainer.appendChild(this._previewElement)
+
+        const editorContainer = document.createElement("div");
+        editorContainer.className = "acd-open-code-dialog-editor-container";
+        renderedElement.appendChild(editorContainer);
+
+        const editorLabel = document.createElement("h3");
+        editorLabel.innerText = "JSON Schema:"
+        editorContainer.appendChild(editorLabel);
+
+        const editorElement = this.renderEditor();
+        editorContainer.appendChild(editorElement);
+
+        return renderedElement;
+    }
+
+    get output(): CardData {
+        return this._output;
+    }
+}
