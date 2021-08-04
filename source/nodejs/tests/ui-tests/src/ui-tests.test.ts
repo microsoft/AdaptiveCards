f986762f99e15a7971205e994cc0e3caaff37e7f
Alberto Medina Gutierrez <almedina@microsoft.com>
2021-08-04 14:48:29 -0700

    Fix mock tests

--- source/nodejs/tests/ui-tests/src/ui-tests.test.ts
+++ source/nodejs/tests/ui-tests/src/ui-tests.test.ts
@@ -2,27 +2,23 @@
 // Licensed under the MIT License.
 import * as AdaptiveCards from "adaptivecards";
 import * as Assert from "assert";
-// import * as Test from "selenium-webdriver/testing";
 import * as Webdriver from "selenium-webdriver";
 
-
 describe("Mock function", function() {
-    it("Test case should work", function() {
+    let driver : Webdriver.WebDriver;
+
+    beforeAll(async function() {
+        driver = new Webdriver.Builder().withCapabilities(Webdriver.Capabilities.edge()).build();
+        await driver.get("http://127.0.0.1:8080/");
+    });
+
+    afterAll(async function() {
+        if (driver) {
+            // driver.quit();
+        }
+    });
+
+    test("Test case should work", async function() {
         Assert.equal(true, true);
     });
 });
-
-/*
-describe("Verify setup with Google Search", function() {
-    it("test case should work", function() {
-        const driver = new Webdriver.Builder().withCapabilities(Webdriver.Capabilities.edge()).build();
-        driver.get("http://www.google.com");
-        const searchBox = driver.findElement(Webdriver.By.name("q"));
-        searchBox.sendKeys("selenium node automation");
-        searchBox.getAttribute("value").then(function(value) {
-            Assert.equal(value, "selenium node automation");
-        });
-        // driver.quit();
-    });
-});
-*/
