03dfc8152c56e6528a167fba8a90410e7ca82608
Jon Wiswall <jonwis@microsoft.com>
2021-08-19 00:29:16 -0700

    Oopsie

--- source/winappsdk/AdaptiveCardsObjectModel/lib/AdaptiveWarning.cpp
+++ source/winappsdk/AdaptiveCardsObjectModel/lib/AdaptiveWarning.cpp
@@ -6,6 +6,5 @@ namespace winrt::AdaptiveCards::ObjectModel::WinUI3::implementation
     AdaptiveWarning::AdaptiveWarning(winrt::AdaptiveCards::ObjectModel::WinUI3::WarningStatusCode const& statusCode, hstring const& message) :
         StatusCode{statusCode}, Message{message}
     {
-        throw hresult_not_implemented();
     }
 }
