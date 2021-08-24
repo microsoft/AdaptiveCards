fea7b00b566bb1e6cb69f8a59fa8c42d050ea1a3
Jon Wiswall <jonwis@microsoft.com>
2021-08-24 16:13:07 -0700

    Event args

--- source/winappsdk/Renderer/lib/AdaptiveMediaEventArgs.cpp
+++ source/winappsdk/Renderer/lib/AdaptiveMediaEventArgs.cpp
@@ -3,20 +3,6 @@
 #include "pch.h"
 #include "AdaptiveMediaEventArgs.h"
 
-using namespace Microsoft::WRL;
-using namespace ABI::AdaptiveCards::Rendering::WinUI3;
-using namespace ABI::AdaptiveCards::ObjectModel::WinUI3;
-using namespace ABI::Windows::Data::Json;
-
-namespace AdaptiveCards::Rendering::WinUI3
+namespace winrt::AdaptiveCards::Rendering::WinUI3::implementation
 {
-    HRESULT AdaptiveMediaEventArgs::RuntimeClassInitialize() { return S_OK; }
-
-    HRESULT AdaptiveMediaEventArgs::RuntimeClassInitialize(_In_ IAdaptiveMedia* media)
-    {
-        m_media = media;
-        return S_OK;
-    }
-
-    HRESULT AdaptiveMediaEventArgs::get_Media(_COM_Outptr_ IAdaptiveMedia** media) { return m_media.CopyTo(media); }
 }
--- source/winappsdk/Renderer/lib/AdaptiveMediaEventArgs.h
+++ source/winappsdk/Renderer/lib/AdaptiveMediaEventArgs.h
@@ -1,24 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 #pragma once
-#include "AdaptiveCards.Rendering.WinUI3.h"
+#include "AdaptiveMediaEventArgs.g.h"
 
-namespace AdaptiveCards::Rendering::WinUI3
+namespace winrt::AdaptiveCards::Rendering::WinUI3::implementation
 {
-    class AdaptiveMediaEventArgs
-        : public Microsoft::WRL::RuntimeClass<Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::RuntimeClassType::WinRt>,
-                                              ABI::AdaptiveCards::Rendering::WinUI3::IAdaptiveMediaEventArgs>
+    struct AdaptiveMediaEventArgs : AdaptiveMediaEventArgsT < AdaptiveMediaEventArgs>
     {
-        AdaptiveRuntime(AdaptiveMediaEventArgs);
-
-    public:
-        HRESULT RuntimeClassInitialize();
-        HRESULT RuntimeClassInitialize(_In_ ABI::AdaptiveCards::ObjectModel::WinUI3::IAdaptiveMedia* media);
-
-        // IAdaptiveMediaEventArgs
-        IFACEMETHODIMP get_Media(_COM_Outptr_ ABI::AdaptiveCards::ObjectModel::WinUI3::IAdaptiveMedia** media);
-
-    private:
-        Microsoft::WRL::ComPtr<ABI::AdaptiveCards::ObjectModel::WinUI3::IAdaptiveMedia> m_media;
+        AdaptiveMediaEventArgs(ObjectModel::WinUI3::AdaptiveMedia const& media = nullptr) : Media{media} {}
+        property<ObjectModel::WinUI3::AdaptiveMedia> Media;
     };
 }
