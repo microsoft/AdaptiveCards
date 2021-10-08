7ea77e7ade5169e8114eac322182205f71061828
almedina-ms <35784165+almedina-ms@users.noreply.github.com>
2021-10-08 03:43:26 -0700

    [Android] Fix behaviour for Filtered ChoiceSet  (#6525)
    * Fix all scenarios but empty string
    
    * Fix break due to long processing time and add tests
    
    * Add comment to explain the synchronized block
    
    * Fix comments from PR
    
    * Add new test case and cleanup test code
    
    * Add wait for popup dismiss
    
    * Add wait times for failing tests
    
    * Another try to fix the test
    
    * Add more wait time
    
    * Add more time to all elements test
    
    * Ignore test until a fix can be found
    
    Co-authored-by: RebeccaAnne <rebecch@microsoft.com>

--- source/android/uitestapp/src/androidTest/java/io.adaptivecards.uitestapp/TestHelpers.kt
+++ source/android/uitestapp/src/androidTest/java/io.adaptivecards.uitestapp/TestHelpers.kt
@@ -3,8 +3,14 @@
 package io.adaptivecards.uitestapp
 
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.RootMatchers
 import androidx.test.espresso.matcher.ViewMatchers
+import io.adaptivecards.renderer.TagContent
+import io.adaptivecards.uitestapp.ui.inputs.RetrievedInput
+import org.hamcrest.Matchers
 
 object TestHelpers {
     internal fun goToTestCasesScreen() {
@@ -17,5 +23,29 @@ object TestHelpers {
 
     internal fun goToInputsScreen() {
         Espresso.onView(ViewMatchers.withId(R.id.navigation_inputs)).perform(ViewActions.click())
+    }
+
+    internal fun findInputInValidatedContainer(validatedContainerTagId : String) : ViewInteraction {
+        return Espresso.onView(ViewMatchers.withParent(ViewMatchers.withTagValue(Matchers.`is`(TagContent(validatedContainerTagId)))));
+    }
+
+    internal fun clearTextInInput(input : ViewInteraction) {
+        input.perform(ViewActions.scrollTo(), ViewActions.click(), ViewActions.clearText());
+    }
+
+    internal fun setTextInInput(input : ViewInteraction, text : String) {
+        input.perform(ViewActions.scrollTo(), ViewActions.click(), ViewActions.clearText(), ViewActions.typeText(text));
+    }
+
+    internal fun selectPopupOption(optionText : String) {
+        Espresso.onData(Matchers.`is`(optionText)).inRoot(RootMatchers.isPlatformPopup()).perform(ViewActions.scrollTo(), ViewActions.click());
+    }
+
+    internal fun clickOnElementWithText(text : String) {
+        Espresso.onView(ViewMatchers.withText(text)).perform(ViewActions.click())
+    }
+
+    internal fun assertInputValuePairExists(inputId : String, value : String) {
+        Espresso.onData(Matchers.`is`(RetrievedInput(inputId, value))).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }
