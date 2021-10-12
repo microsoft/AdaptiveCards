444ac8f6b9dd8127b05dee8701ee1acf0d3bc062
Alberto Medina Gutierrez <almedina@microsoft.com>
2021-10-12 10:41:39 -0700

    Test path

--- source/dotnet/Library/AdaptiveCards.Templating/Properties/AssemblyInfo.cs
+++ source/dotnet/Library/AdaptiveCards.Templating/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System;
 using System.Reflection;
 
 #if DELAY_SIGN
-[assembly: AssemblyKeyFileAttribute(@"../../../../25StrongName.snk")]
+[assembly: AssemblyKeyFileAttribute(@"../../../../36MSApp1024.snk")]
 [assembly: AssemblyDelaySignAttribute(true)]
 #endif
