03e9e696df3303976f0e303c0ee49f7f203bdb8f
Canhua Li <licanhua@live.com>
2021-10-18 14:39:37 -0700

    Add adaptivecards-react-testapp

---
+++ source/nodejs/adaptivecards-react-testapp/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { AC } from './AC';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <AC />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
