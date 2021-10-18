9109b68ca1daf6ca6a1eee2216b6005e5fd6a563
Siddharth Gulati <62313299+SidGulatiMsft@users.noreply.github.com>
2021-10-18 14:14:40 -0700

    Update adaptivecards-designer.ts

--- source/nodejs/adaptivecards-designer/src/adaptivecards-designer.ts
+++ source/nodejs/adaptivecards-designer/src/adaptivecards-designer.ts
@@ -10,7 +10,7 @@ export const defaultMicrosoftHosts: containers.HostContainer[] = [
 	new containers.OutlookContainer("Outlook Actionable Messages", "containers/outlook-container.css"),
 	new containers.DarkTeamsContainer("Microsoft Teams - Dark", "containers/teams-container-dark.css"),
 	new containers.LightTeamsContainer("Microsoft Teams - Light", "containers/teams-container-light.css"),
-    new containers.VivaConnectionsContainer("Viva Connections", "containers/viva-connections-container.css"),
+	new containers.VivaConnectionsContainer("Viva Connections", "containers/viva-connections-container.css"),
 	new containers.DarkCortanaContainer("Cortana Skills - Dark", "containers/cortana-container-dark.css"),
 	new containers.LightCortanaContainer("Cortana Skills - Light", "containers/cortana-container-light.css"),
 	new containers.BotFrameworkContainer("Bot Framework Other Channels (Image render)", "containers/bf-image-container.css"),
