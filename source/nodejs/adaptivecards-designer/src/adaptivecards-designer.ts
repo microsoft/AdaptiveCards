91a55d39b16dca0660b748236a7b2916c7c7fa82
Canhua Li <licanhua@live.com>
2021-10-19 12:23:17 -0700

    Add large medium small conf

--- source/nodejs/adaptivecards-designer/src/adaptivecards-designer.ts
+++ source/nodejs/adaptivecards-designer/src/adaptivecards-designer.ts
@@ -14,6 +14,11 @@ export const defaultMicrosoftHosts: containers.HostContainer[] = [
 	new containers.LightCortanaContainer("Cortana Skills - Light", "containers/cortana-container-light.css"),
 	new containers.BotFrameworkContainer("Bot Framework Other Channels (Image render)", "containers/bf-image-container.css"),
 	new containers.CortanaClassicContainer("Cortana Skills (Classic)", "containers/cortana-classic-container.css"),
-	new containers.LightBerlinContainer("Berlin - Light (Test)", "containers/berlin-container-light.css"),
-	new containers.DarkBerlinContainer("Berlin - Dark (Test)", "containers/berlin-container-dark.css")
+	new containers.LightBerlinLargeContainer("Berlin - Light - Large (Test)", "containers/berlin-container-light.css"),
+	new containers.DarkBerlinLargeContainer("Berlin - Dark - Large (Test)", "containers/berlin-container-dark.css"),
+	new containers.LightBerlinSmallContainer("Berlin - Light - Small (Test)", "containers/berlin-container-light.css"),
+	new containers.DarkBerlinSmallContainer("Berlin - Dark - Small (Test)", "containers/berlin-container-dark.css"),
+	new containers.LightBerlinMediumContainer("Berlin - Light - Medium (Test)", "containers/berlin-container-light.css"),
+	new containers.DarkBerlinMediumContainer("Berlin - Dark - Medium (Test)", "containers/berlin-container-dark.css"),
+
 ]
--- source/nodejs/adaptivecards-designer/src/containers/berlin/berlin-container.ts
+++ source/nodejs/adaptivecards-designer/src/containers/berlin/berlin-container.ts
@@ -5,30 +5,89 @@ import { HostContainer } from "../host-container";
 import * as hostConfigLight from "../../hostConfigs/berlin-light.json";
 import * as hostConfigDark from "../../hostConfigs/berlin-dark.json";
 
-export class BaseBerlinContainer extends HostContainer {
+abstract class BaseBerlinContainer extends HostContainer {
     public renderTo(hostElement: HTMLElement) {
-        this.cardHost.classList.add("berlinOuterContainer");
+        this.cardHost.classList.add("berlin-card");
+        let outerFrame = document.createElement("div");
+        outerFrame.classList.add("berlin-outer-container");
+        outerFrame.classList.add(this.getAdditionalClassName());
+
+        let header = document.createElement("div");
+        header.className = "berlin-header";
+        outerFrame.appendChild(header);
 
         let frame = document.createElement("div");
-        frame.className = "berlinInnerContainer";
+        frame.className = "berlin-inner-container";
         frame.appendChild(this.cardHost);
 
-        hostElement.appendChild(frame);
+        outerFrame.appendChild(frame);
+        hostElement.appendChild(outerFrame);
     }
     get targetVersion(): Adaptive.Version {
-        return Adaptive.Versions.v1_4;
+        return Adaptive.Versions.v1_5;
     }
+
+    abstract getAdditionalClassName(): string;
 }
 
-export class LightBerlinContainer extends BaseBerlinContainer {
+
+abstract class LightBerlinContainer extends BaseBerlinContainer {
     public getHostConfig(): Adaptive.HostConfig {
         return new Adaptive.HostConfig(hostConfigLight);
     }
 }
 
-export class DarkBerlinContainer extends BaseBerlinContainer {
+export class LightBerlinLargeContainer extends LightBerlinContainer {
+    getAdditionalClassName(): string {
+        return "berlin-large-container";
+    }
+}
+
+export class LightBerlinSmallContainer extends LightBerlinContainer {
+    getAdditionalClassName(): string {
+        return "berlin-small-container";
+    }
+}
+
+export class LightBerlinMediumContainer extends LightBerlinContainer {
+    getAdditionalClassName(): string {
+        return "berlin-medium-container";
+    }
+}
+
+abstract class DarkBerlinContainer extends BaseBerlinContainer {
 	public getBackgroundColor(): string {
         return "#201E1F";
+    }
+
+	public getHostConfig(): Adaptive.HostConfig {
+        return new Adaptive.HostConfig(hostConfigDark);
+    }
+}
+
+export class DarkBerlinLargeContainer extends DarkBerlinContainer {
+    getAdditionalClassName(): string {
+        return "berlin-large-container";
+    }
+
+	public getHostConfig(): Adaptive.HostConfig {
+        return new Adaptive.HostConfig(hostConfigDark);
+    }
+}
+
+export class DarkBerlinSmallContainer extends DarkBerlinContainer {
+    getAdditionalClassName(): string {
+        return "berlin-small-container";
+    }
+
+	public getHostConfig(): Adaptive.HostConfig {
+        return new Adaptive.HostConfig(hostConfigDark);
+    }
+}
+
+export class DarkBerlinMediumContainer extends DarkBerlinContainer {
+    getAdditionalClassName(): string {
+        return "berlin-medium-container";
     }
 
 	public getHostConfig(): Adaptive.HostConfig {
