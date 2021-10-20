9f8a094b04a3e5e43a6d8dea77ff24958f4d1a08
nesalang <Joseph.Woo@microsoft.com>
2021-10-20 11:19:21 -0700

    package changes

--- source/nodejs/adaptivecards/src/card-elements.ts
+++ source/nodejs/adaptivecards/src/card-elements.ts
@@ -7168,7 +7168,7 @@ export class AdaptiveCard extends ContainerWithActions {
         swiper.innerHTML = "<div class=\"swiper-wrapper\"><div class=\"swiper-slide\">Slide 1</div><div class=\"swiper-slide\">Slide 2</div><div class=\"swiper-slide\">Slide 3</div></div><div class=\"swiper-pagination\"></div><div class=\"swiper-button-prev\"></div><div class=\"swiper-button-next\"></div><div class=\"swiper-scrollbar\"></div>";
 
         swiper.addEventListener('onload', this.initializeSwiper);
-        swiper.addEventListener('click', this.initializeSwiper);
+        //swiper.addEventListener('click', this.initializeSwiper);
 
         return swiper;
 
