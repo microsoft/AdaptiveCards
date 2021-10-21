ca94372c9defb36a74f4a0168642b43941632441
Canhua Li <licanhua@live.com>
2021-10-21 15:27:11 -0700

    format tsx via .editconfig

--- source/nodejs/adaptivecards-react/src/host-config-context.tsx
+++ source/nodejs/adaptivecards-react/src/host-config-context.tsx
@@ -5,7 +5,7 @@ import React, { createContext, FC, useContext } from 'react';
 import { Props, AdaptiveCard } from './adaptive-card'
 
 interface HostConfig {
-  hostConfig: object;
+    hostConfig: object;
 }
 
 export const HostConfigContext = createContext<HostConfig>({ hostConfig: null });
@@ -13,10 +13,10 @@ export const HostConfigContext = createContext<HostConfig>({ hostConfig: null })
 export type PropsWithoutHostConfig = Omit<Props, 'hostConfig'>;
 
 export const AdaptiveCardUsingHostConfigContext = (props: PropsWithoutHostConfig) => {
-  const context = useContext(HostConfigContext);
-  return <AdaptiveCard {...props} hostConfig={context.hostConfig} />
+    const context = useContext(HostConfigContext);
+    return <AdaptiveCard {...props} hostConfig={context.hostConfig} />
 }
 
 export const ProvidesHostConfigContext: FC<{ hostConfig: object }> = ({ hostConfig, children }) => {
-  return <HostConfigContext.Provider value={{ hostConfig }} >{children}</HostConfigContext.Provider>
+    return <HostConfigContext.Provider value={{ hostConfig }} >{children}</HostConfigContext.Provider>
 };
