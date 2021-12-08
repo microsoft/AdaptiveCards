4278ee4720f8ac1f749dd2026cd002b0f392daf5
Paul Campbell <paulcam@microsoft.com>
2021-12-08 15:51:31 -0800

    tsx/jsx support and remove from .editorconfig

--- source/nodejs/.lintstagedrc.mjs
+++ source/nodejs/.lintstagedrc.mjs
@@ -1,6 +1,7 @@
 import { ESLint } from "eslint";
 import micromatch from "micromatch";
 
+// As suggested here: https://github.com/okonet/lint-staged#eslint--7-1
 const removeIgnoredFiles = async (files) => {
   const eslint = new ESLint();
   const isIgnored = await Promise.all(
@@ -14,7 +15,7 @@ const removeIgnoredFiles = async (files) => {
 
 export default {
   "**/!(package-lock)*": async (files) => {
-    const filesToLint = await removeIgnoredFiles(micromatch(files, ["**/!(__)*.{ts,js}"]));
+    const filesToLint = await removeIgnoredFiles(micromatch(files, ["**/!(__)*.{ts,tsx,js,jsx}"]));
     return [
       `eslint --fix --max-warnings=0 ${filesToLint}`,
       `prettier --write --ignore-unknown ${files.join(" ")}`
