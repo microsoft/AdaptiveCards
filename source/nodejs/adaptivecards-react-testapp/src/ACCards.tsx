aa3199ba5e7724faa7bcb3a412ee50e57feb37a6
Canhua Li <licanhua@live.com>
2022-01-18 14:25:57 -0800

    investigate color problem

--- source/nodejs/adaptivecards-react-testapp/src/ACCards.tsx
+++ source/nodejs/adaptivecards-react-testapp/src/ACCards.tsx
@@ -1,6 +1,5 @@
-import { AdaptiveCard, AdaptiveCardUsingHostConfigContext } from 'adaptivecards-react'
-import HostConfigDark from './host-conf-dark.json'
-
+import { AdaptiveCard } from 'adaptivecards-react'
+import HostConfigDark from './host-conf-light.json'
 
 const card = {
     "type": "AdaptiveCard",
@@ -9,7 +8,7 @@ const card = {
             "type": "TextBlock",
             "size": "Medium",
             "weight": "Bolder",
-            "text": "${title}"
+            "text": "${title}",
         },
         {
             "type": "ColumnSet",
@@ -18,83 +17,16 @@ const card = {
                     "type": "Column",
                     "items": [
                         {
-                            "type": "Image",
-                            "style": "Person",
-                            "url": "${creator.profileImage}",
-                            "size": "Small"
-                        }
-                    ],
-                    "width": "auto"
-                },
-                {
-                    "type": "Column",
-                    "items": [
-                        {
                             "type": "TextBlock",
                             "weight": "Bolder",
                             "text": "${creator.name}",
-                            "wrap": true
+                            "wrap": true,
                         },
-                        {
-                            "type": "TextBlock",
-                            "spacing": "None",
-                            "text": "Created {{DATE(${createdUtc},SHORT)}}",
-                            "isSubtle": true,
-                            "wrap": true
-                        }
                     ],
                     "width": "stretch"
                 }
             ]
         },
-        {
-            "type": "TextBlock",
-            "text": "${description}",
-            "wrap": true
-        },
-        {
-            "type": "FactSet",
-            "facts": [
-                {
-                    "$data": "${properties}",
-                    "title": "${key}:",
-                    "value": "${value}"
-                }
-            ]
-        }
-    ],
-    "actions": [
-        {
-            "type": "Action.ShowCard",
-            "title": "Set due date",
-            "card": {
-                "type": "AdaptiveCard",
-                "body": [
-                    {
-                        "type": "Input.Date",
-                        "id": "dueDate"
-                    },
-                    {
-                        "type": "Input.Text",
-                        "id": "comment",
-                        "placeholder": "Add a comment",
-                        "isMultiline": true
-                    }
-                ],
-                "actions": [
-                    {
-                        "type": "Action.Submit",
-                        "title": "OK"
-                    }
-                ],
-                "$schema": "http://adaptivecards.io/schemas/adaptive-card.json"
-            }
-        },
-        {
-            "type": "Action.OpenUrl",
-            "title": "View",
-            "url": "${viewUrl}"
-        }
     ],
     "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
     "version": "1.3"
@@ -102,11 +34,8 @@ const card = {
 
 const CardWithHostConfigFromProperty = () => (<AdaptiveCard payload={card} hostConfig={HostConfigDark} />);
 
-const CardWithHostConfigFromContext = () => (<AdaptiveCardUsingHostConfigContext payload={card} />);
-
 export const ACCards = () => {
     return (<div>
         <CardWithHostConfigFromProperty />
-        <CardWithHostConfigFromContext />
     </div>);
 };
