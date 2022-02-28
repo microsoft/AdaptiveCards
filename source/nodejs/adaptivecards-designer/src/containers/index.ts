b88edeaea4093e79c809eeeb6c40833cb3c1ed97
naramaka <65971640+naramaka@users.noreply.github.com>
2022-03-01 01:11:13 +0530

    cherry picked federated search

--- source/nodejs/adaptivecards-designer/src/containers/index.ts
+++ source/nodejs/adaptivecards-designer/src/containers/index.ts
@@ -12,3 +12,4 @@ export * from "./teams/teams-container";
 export * from "./timeline/timeline-container";
 export * from "./webchat/webchat-container";
 export * from "./viva-connections/viva-connections-container";
+export * from "./federated-search/federated-search-container";
