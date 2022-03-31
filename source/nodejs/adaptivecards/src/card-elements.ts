f38759c20b23306cb1194481d0fbe0b65df0026e
Eunia Lee <eunialee@microsoft.com>
2022-03-31 11:02:26 -0700

    Spacing fix

--- source/nodejs/adaptivecards/src/card-elements.ts
+++ source/nodejs/adaptivecards/src/card-elements.ts
@@ -5176,7 +5176,6 @@ export class ToggleVisibilityAction extends Action {
 
     execute() {
 		super.execute();
-		
         if (this.parent) {
             for (const elementId of Object.keys(this.targetElements)) {
                 const targetElement = this.parent.getRootElement().getElementById(elementId);
