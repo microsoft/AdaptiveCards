25f00f2d74a1ca78b3f21aaf5050e4ede0e3a4f3
Pankaj Bhojwani <pankaj.d.bhoj@gmail.com>
2022-04-01 09:22:54 -0700

    update this

--- source/uwp/Renderer/lib/MediaHelpers.cpp
+++ source/uwp/Renderer/lib/MediaHelpers.cpp
@@ -194,18 +194,16 @@ namespace AdaptiveCards::Rendering::Uwp::MediaHelpers
                 auto mediaSrc = winrt::MediaSource::CreateFromStream(randomAccessStream, mimeType);
                 if (adaptiveMedia.CaptionSources().Size() > 0)
                 {
-                    for (uint32_t i = 0; i < adaptiveMedia.CaptionSources().Size(); ++i)
+                    for (const auto captionSource : adaptiveMedia.CaptionSources())
                     {
-                        const auto timedTextURL =
-                            GetUrlFromString(renderContext.HostConfig(), adaptiveMedia.CaptionSources().GetAt(i).Url());
+                        const auto timedTextURL = GetUrlFromString(renderContext.HostConfig(), captionSource.Url());
                         const auto timedTextSrc = winrt::TimedTextSource::CreateFromUri(timedTextURL);
                         timedTextSrc.Resolved(
-                            [i](winrt::TimedTextSource sender,
-                                winrt::TimedTextSourceResolveResultEventArgs args)
+                            [captionSource](winrt::TimedTextSource sender, winrt::TimedTextSourceResolveResultEventArgs args)
                             {
                                 if (!args.Error())
                                 {
-                                    args.Tracks().GetAt(0).Label(L"Caption track " + std::to_wstring(i + 1));
+                                    args.Tracks().GetAt(0).Label(captionSource.MimeType());
                                 }
                             });
                         mediaSrc.ExternalTimedTextSources().Append(timedTextSrc);
