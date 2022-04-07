696943023112772139011ba7978b0fd0b8824d9b
Anna Dingler <annadingler@microsoft.com>
2022-04-07 13:21:12 -0700

    Formatting

--- source/nodejs/tests/unit-tests/src/unit-tests.test.ts
+++ source/nodejs/tests/unit-tests/src/unit-tests.test.ts
@@ -19,97 +19,97 @@ describe("test Text property on TextBlock", () => {
 });
 
 describe("Test Templating Library", () => {
-	it("BasicTemplate", () => {
-		const templatePayload = {
-			"type": "AdaptiveCard",
-			"version": "1.0",
-			"body": [
-				{
-					"type": "TextBlock",
-					"text": "Hello ${name}!"
-				}
-			]
-		};
-				
-		const root = {
-			"name": "Adaptive Cards"
-		};		
+    it("BasicTemplate", () => {
+        const templatePayload = {
+            "type": "AdaptiveCard",
+            "version": "1.0",
+            "body": [
+                {
+                    "type": "TextBlock",
+                    "text": "Hello ${name}!"
+                }
+            ]
+        };
+                
+        const root = {
+            "name": "Adaptive Cards"
+        };		
 
-		const expectedOutput = {
-			"type": "AdaptiveCard",
-			"version": "1.0",
-			"body": [ 
-				{ 
-					"type": "TextBlock", 
-					"text": "Hello Adaptive Cards!" 
-				} 
-			]
-		};
+        const expectedOutput = {
+            "type": "AdaptiveCard",
+            "version": "1.0",
+            "body": [ 
+                { 
+                    "type": "TextBlock", 
+                    "text": "Hello Adaptive Cards!" 
+                } 
+            ]
+        };
 
-		runTest(templatePayload, root, expectedOutput);
-	})
+        runTest(templatePayload, root, expectedOutput);
+    })
 
-	it("BasicTemplateWithHost", () => {
-		const templatePayload = {
-			"type": "AdaptiveCard",
-			"version": "1.0",
-			"body": [
-				{
-					"type": "TextBlock",
-					"text": "Hello ${name}! You're using the ${$host.WindowsTheme} theme!"
-				}
-			]
-		};
-				
-		const root = {
-			"name": "Adaptive Cards"
-		};
-		const host = {
-			"WindowsTheme": "Light"
-		}
-		
-		const expectedOutput = {
-			"type": "AdaptiveCard",
-			"version": "1.0",
-			"body": [ 
-				{ 
-					"type": "TextBlock", 
-					"text": "Hello Adaptive Cards! You're using the Light theme!" 
-				} 
-			]
-		};
+    it("BasicTemplateWithHost", () => {
+        const templatePayload = {
+            "type": "AdaptiveCard",
+            "version": "1.0",
+            "body": [
+                {
+                    "type": "TextBlock",
+                    "text": "Hello ${name}! You're using the ${$host.WindowsTheme} theme!"
+                }
+            ]
+        };
+                
+        const root = {
+            "name": "Adaptive Cards"
+        };
+        const host = {
+            "WindowsTheme": "Light"
+        }
+        
+        const expectedOutput = {
+            "type": "AdaptiveCard",
+            "version": "1.0",
+            "body": [ 
+                { 
+                    "type": "TextBlock", 
+                    "text": "Hello Adaptive Cards! You're using the Light theme!" 
+                } 
+            ]
+        };
 
-		runTest(templatePayload, root, expectedOutput, host);
-	});
+        runTest(templatePayload, root, expectedOutput, host);
+    });
 
-	it("ComplexTemplate", () => {
-		runTest(loadFile("template-test-resources/complex-template.json"),
-			loadFile("template-test-resources/complex-template.data.json"),
-			loadFile("template-test-resources/complex-template.output.json"));
-	});
+    it("ComplexTemplate", () => {
+        runTest(loadFile("template-test-resources/complex-template.json"),
+            loadFile("template-test-resources/complex-template.data.json"),
+            loadFile("template-test-resources/complex-template.output.json"));
+    });
 
-	it("ComplexTemplateWithHost", () => {
-		runTest(loadFile("template-test-resources/complex-template-host.json"),
-			loadFile("template-test-resources/complex-template-host.data.json"),
-			loadFile("template-test-resources/complex-template-host.output.json"),
-			loadFile("template-test-resources/complex-template-host.host.json"));
-	});
+    it("ComplexTemplateWithHost", () => {
+        runTest(loadFile("template-test-resources/complex-template-host.json"),
+            loadFile("template-test-resources/complex-template-host.data.json"),
+            loadFile("template-test-resources/complex-template-host.output.json"),
+            loadFile("template-test-resources/complex-template-host.host.json"));
+    });
 });
 
 function runTest(templatePayload: any, data: any, expectedOutput: any, host?: any) {
-	let template = new ACData.Template(templatePayload);
+    let template = new ACData.Template(templatePayload);
 
-	let context = {
-		$root: data,
-		$host: host
-	};
+    let context = {
+        $root: data,
+        $host: host
+    };
 
-	let card = template.expand(context);
+    let card = template.expand(context);
 
-	expect(card).toStrictEqual(expectedOutput);
+    expect(card).toStrictEqual(expectedOutput);
 }
 
 function loadFile(filePath: string) {
-	const dataObject = fs.readFileSync(filePath, "utf8");
-	return JSON.parse(dataObject);
+    const dataObject = fs.readFileSync(filePath, "utf8");
+    return JSON.parse(dataObject);
 }
