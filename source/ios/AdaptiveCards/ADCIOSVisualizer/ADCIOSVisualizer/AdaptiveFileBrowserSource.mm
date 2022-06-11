9689d4b563c34164e2a9d7ba6f7bd28dcc472f6f
Joseph Woo <Joseph.Woo@microsoft.com>
2022-06-10 17:19:14 -0700

    [iOS] Updated Sample App (#7503)
    * [iOS] Updated Sample App
    
    * updated clang format
    
    Co-authored-by: Canhua Li <licanhua@live.com>

--- source/ios/AdaptiveCards/ADCIOSVisualizer/ADCIOSVisualizer/AdaptiveFileBrowserSource.mm
+++ source/ios/AdaptiveCards/ADCIOSVisualizer/ADCIOSVisualizer/AdaptiveFileBrowserSource.mm
@@ -28,6 +28,7 @@ bool compare(shared_ptr<BaseActionElement> const &a, shared_ptr<BaseActionElemen
     __weak UIView *_adaptiveView;
     __weak id<ACVTableViewControllerFetchDataDelegate> _tableFetchDataDelegate;
     ACOHostConfig *_hostConfig;
+    NSSet *_restrictedPaths;
 }
 
 - (instancetype)initWithFrame:(CGRect)frame
@@ -45,7 +46,13 @@ bool compare(shared_ptr<BaseActionElement> const &a, shared_ptr<BaseActionElemen
         if (hostconfigParseResult.isValid) {
             _hostConfig = hostconfigParseResult.config;
         }
-
+#if RELEASE
+        // the most of cards below v1.2 don't have updated accssibility features, but these cards do serve well as visualization test during bug bash
+        // so instead of removing them, excluded these cards when built for release.
+        _restrictedPaths = [NSSet setWithObjects:@"v1.0", @"v1.1", @"HostConfig", @"Templates", @"Elements", @"Tests", nil];
+#else
+        _restrictedPaths = [NSSet setWithObjects:@"HostConfig", @"Templates", nil];
+#endif
         [self updateAdaptiveViewWithNewPath:_rootPath];
     }
     return self;
@@ -95,7 +102,7 @@ bool compare(shared_ptr<BaseActionElement> const &a, shared_ptr<BaseActionElemen
         BOOL isDirectory = NO;
         NSString *resourcePath = [parentDir stringByAppendingPathComponent:path];
         if ([_fileManager fileExistsAtPath:resourcePath isDirectory:&isDirectory]) {
-            if (isDirectory && ![[_fileManager displayNameAtPath:path] isEqualToString:@"HostConfig"]) {
+            if (isDirectory && ![_restrictedPaths containsObject:[_fileManager displayNameAtPath:path]]) {
                 actions.push_back(buildAction(resourcePath.UTF8String, [_fileManager displayNameAtPath:path].UTF8String));
             } else if ([[resourcePath pathExtension] isEqualToString:@"json"]) {
                 [filesList addObject:resourcePath];
