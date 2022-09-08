6915e3ed960373aa968a56abd38e90f6c266e7f7
Joseph Woo <Joseph.Woo@microsoft.com>
2022-09-07 18:47:34 -0700

    Fixed the link parser

--- source/shared/cpp/ObjectModel/MarkDownBlockParser.cpp
+++ source/shared/cpp/ObjectModel/MarkDownBlockParser.cpp
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+#include "MarkDownBlockParser.h"
 #include "pch.h"
 #include <iostream>
-#include "MarkDownBlockParser.h"
 
 using namespace AdaptiveCards;
 
