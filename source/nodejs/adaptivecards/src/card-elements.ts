05ae46c38043007bb8c36b084cfa09e4966fd7cc
Canhua Li <licanhua@live.com>
2022-09-14 10:58:49 -0700

    prefix adaptivecards#  for ttpolicy

--- source/nodejs/adaptivecards/src/card-elements.ts
+++ source/nodejs/adaptivecards/src/card-elements.ts
@@ -1109,7 +1109,7 @@ export class TextBlock extends BaseTextBlock {
     // Markdown processing is handled outside of Adaptive Cards. It's up to the host to ensure that markdown is safely
     // processed.
     private static readonly _ttMarkdownPolicy = window.trustedTypes?.createPolicy(
-        "markdownPassthroughPolicy",
+        "adaptivecards#markdownPassthroughPolicy",
         { createHTML: (value) => value }
     );
 
@@ -1118,7 +1118,7 @@ export class TextBlock extends BaseTextBlock {
     // _originalInnerHtml so that we can restore/recalculate truncation later if space availability has changed (see
     // TextBlock.restoreOriginalContent())
     private static readonly _ttRoundtripPolicy = window.trustedTypes?.createPolicy(
-        "restoreContentsPolicy",
+        "adaptivecards#restoreContentsPolicy",
         { createHTML: (value) => value }
     );
 
