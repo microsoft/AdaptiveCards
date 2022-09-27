8d84a0f6f8d35b55425252f1cd1e2abd548dcc65
Anna Dingler <annadingler@microsoft.com>
2022-09-27 13:44:30 -0700

    Use ariaLabel

--- source/nodejs/adaptivecards-designer/src/tool-box.ts
+++ source/nodejs/adaptivecards-designer/src/tool-box.ts
@@ -130,7 +130,7 @@ export class Toolbox {
         this._expandCollapseButtonElement.tabIndex = 0;
         this._expandCollapseButtonElement.setAttribute("role", "button");
         this._expandCollapseButtonElement.setAttribute("aria-expanded", "true");
-        this._expandCollapseButtonElement.setAttribute("aria-label", this.title);
+        this._expandCollapseButtonElement.ariaLabel = this.title;
 
         this._headerIconElement = document.createElement("span")
         this._headerIconElement.classList.add("acd-icon", "acd-icon-header-expanded");
