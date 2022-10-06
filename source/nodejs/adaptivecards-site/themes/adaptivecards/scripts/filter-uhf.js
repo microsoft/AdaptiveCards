3cf1ef41f88aa953466accf807d9a9ff86f99d67
Anna Dingler <annadingler@microsoft.com>
2022-10-06 15:26:10 -0700

    WIP: Format header, update urls, and use preview mode

--- source/nodejs/adaptivecards-site/themes/adaptivecards/scripts/filter-uhf.js
+++ source/nodejs/adaptivecards-site/themes/adaptivecards/scripts/filter-uhf.js
@@ -5,6 +5,8 @@ const fetch = require("node-fetch");
 /* from UHF MSDocsHeader-AdaptiveCards */
 
 hexo.extend.filter.register("before_generate", async function (data) {
+	const url_for = hexo.extend.helper.get('url_for').bind(hexo);
+
     const serviceEndpoint = "https://uhf.microsoft.com";
     const partnerId = "MSDocsAdaptivecards";
     const headerId = "MSDocsAdaptivecardsHeader";
@@ -19,15 +21,21 @@ hexo.extend.filter.register("before_generate", async function (data) {
         "?headerId=" +
         headerId +
         "&footerId=" +
-        footerId;
+        footerId + "&Preview=1";
 
     const response = await fetch(serviceUrl);
-    const body = await response.text();
-
-    const splitBody = body.split("<![CDATA[");
+	const body = await response.text();
+	
+	const splitBody = body.split("<![CDATA[");
+	
+	let header = splitBody[3].split("]]>")[0];
+	header = header.replace("urlForExplorer", url_for('explorer'));
+	header = header.replace("urlForSamples", url_for('samples'));
+	header = header.replace("urlForBlog", url_for('blog'));
+	header = header.replace("urlForDesigner", url_for('designer'));
 
     hexo.config.uhf_css = splitBody[1].split("]]>")[0];
     hexo.config.uhf_javascript = splitBody[2].split("]]>")[0];
-    hexo.config.uhf_header = splitBody[3].split("]]>")[0];
+    hexo.config.uhf_header = header;
     hexo.config.uhf_footer = splitBody[4].split("]]>")[0];
 });
