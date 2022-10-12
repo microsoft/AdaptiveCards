6a714b57776f5d403ba194bba88b9a7c81961fbe
Anna Dingler <annadingler@microsoft.com>
2022-10-12 11:19:20 -0700

    Manually update header

--- source/nodejs/adaptivecards-site/themes/adaptivecards/scripts/filter-uhf.js
+++ source/nodejs/adaptivecards-site/themes/adaptivecards/scripts/filter-uhf.js
@@ -5,8 +5,6 @@ const fetch = require("node-fetch");
 /* from UHF MSDocsHeader-AdaptiveCards */
 
 hexo.extend.filter.register("before_generate", async function (data) {
-	const url_for = hexo.extend.helper.get('url_for').bind(hexo);
-
     const serviceEndpoint = "https://uhf.microsoft.com";
     const partnerId = "MSDocsAdaptivecards";
     const headerId = "MSDocsAdaptivecardsHeader";
@@ -27,15 +25,8 @@ hexo.extend.filter.register("before_generate", async function (data) {
 	const body = await response.text();
 	
 	const splitBody = body.split("<![CDATA[");
-	
-	let header = splitBody[3].split("]]>")[0];
-	header = header.replace("urlForExplorer", url_for('explorer'));
-	header = header.replace("urlForSamples", url_for('samples'));
-	header = header.replace("urlForBlog", url_for('blog'));
-	header = header.replace("urlForDesigner", url_for('designer'));
 
     hexo.config.uhf_css = splitBody[1].split("]]>")[0];
     hexo.config.uhf_javascript = splitBody[2].split("]]>")[0];
-    hexo.config.uhf_header = header;
     hexo.config.uhf_footer = splitBody[4].split("]]>")[0];
 });
