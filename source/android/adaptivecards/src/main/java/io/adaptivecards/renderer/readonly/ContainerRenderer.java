e19529aace278a10f66f0276e96f8f017f79b21d
Anna Dingler <annadingler@microsoft.com>
2022-11-22 12:07:15 -0800

    Remove unused import

--- source/android/adaptivecards/src/main/java/io/adaptivecards/renderer/readonly/ContainerRenderer.java
+++ source/android/adaptivecards/src/main/java/io/adaptivecards/renderer/readonly/ContainerRenderer.java
@@ -12,7 +12,6 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.appcompat.widget.TooltipCompat;
 import androidx.fragment.app.FragmentManager;
