799436ec5dac7a0237f4b62d4551bcf4a7e64d67
Kristen Schau <47155823+krschau@users.noreply.github.com>
2023-01-05 07:08:12 -0800

    [UWP Renderer] Clean up imports and usings in Renderer and Visualizer (#8158)

--- source/uwp/Visualizer/Helpers/BindableBase.cs
+++ source/uwp/Visualizer/Helpers/BindableBase.cs
@@ -1,12 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdaptiveCardVisualizer.Helpers
 {
