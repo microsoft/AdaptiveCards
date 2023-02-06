7dc841ac29e816c99296ef9584f449c5edc701bf
Anna Dingler <annadingler@microsoft.com>
2023-02-06 10:07:24 -0800

    Update test utils to use ariaLabel

--- source/nodejs/tests/ui-tests/src/test-utils.ts
+++ source/nodejs/tests/ui-tests/src/test-utils.ts
@@ -117,7 +117,7 @@ export class TestUtils {
     }    
 
     async tryGetContainerWithAction(tooltip: string, container?: ACContainer): Promise<WebElement | undefined> {
-        const containerList: WebElement[] = await this.getRoot(container).findElements(ByExtended.ariaDescription(tooltip));
+        const containerList: WebElement[] = await this.getRoot(container).findElements(ByExtended.ariaLabel(tooltip));
         return (containerList.length > 0) ? containerList[0] : undefined;
     }
 
