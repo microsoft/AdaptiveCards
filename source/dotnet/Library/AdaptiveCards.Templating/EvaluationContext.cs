14c37d9699c6430ee65884bd20a21b5b5a35a637
Paul Campbell <paulcam@microsoft.com>
2023-02-23 16:45:50 -0800

    Add host parameter to AdaptiveCards.Templating

--- source/dotnet/Library/AdaptiveCards.Templating/EvaluationContext.cs
+++ source/dotnet/Library/AdaptiveCards.Templating/EvaluationContext.cs
@@ -17,6 +17,7 @@ namespace AdaptiveCards.Templating
         ///     ""person"": {
         ///         ""firstName"": ""Hello"",
         ///         ""lastName"": ""World""
+        ///     }
         /// }";
         ///
         /// var context = new EvaluationContext()
@@ -30,21 +31,55 @@ namespace AdaptiveCards.Templating
         { get; set; }
 
         /// <summary>
+        /// Provides Host Data Context 
+        /// </summary>
+        /// <remarks>
+        /// Typically this is supplied by the host application providing additional context for template binding. For example, the host might supply language or theming information that the template can use for layout.
+        /// </remarks>
+        /// <example>
+        /// <code>
+        /// 
+        /// string jsonData = @"{
+        ///     ""person"": {
+        ///         ""firstName"": ""Hello"",
+        ///         ""lastName"": ""World""
+        ///     }
+        /// }";
+        ///
+        /// string hostData = @"{
+        ///     ""applicationName"": ""Contoso AdaptiveCards Host",
+        ///     ""platform"": ""mobile""
+        /// }";
+        ///
+        /// var context = new EvaluationContext()
+        /// {
+        ///     Root = jsonData,
+        ///     Host = hostData
+        /// };
+        ///
+        /// </code>
+        /// </example>
+        public object Host
+        { get; set; }
+
+        /// <summary>
         /// default consturctor
         /// </summary>
         public EvaluationContext()
         {
             Root = null;
+            Host = null;
         }
 
         /// <summary>
-        /// constructor for <c>EvaluationContext</c> that takes one argument that will be used for root data context
+        /// constructor for <c>EvaluationContext</c> that takes one required argument used for root data context and one optional argument supplying host data
         /// </summary>
-        /// <param name="rootData"></param>
-        public EvaluationContext(object rootData)
+        /// <param name="rootData">Data to use while binding</param>
+        /// <param name="hostData">Data supplied by the host for use while binding</param>
+        public EvaluationContext(object rootData, object hostData = null)
         {
             Root = rootData;
+            Host = hostData;
         }
-
     }
 }
