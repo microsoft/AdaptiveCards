3b59c5188d480319189f2f37f2c49b80f4271b6c
Paul Campbell <paulcam@microsoft.com>
2024-01-25 17:40:48 -0800

    mostly working?

--- source/dotnet/Library/AdaptiveCards.Templating.CSharp.WinRT/AdaptiveCardsTemplateWinRT.cs
+++ source/dotnet/Library/AdaptiveCards.Templating.CSharp.WinRT/AdaptiveCardsTemplateWinRT.cs
@@ -5,13 +5,13 @@ namespace AdaptiveCardsTemplatingCSharpWinRT
     {
         public AdaptiveCardTemplate(String jsonTemplate)
         {
-            //adaptiveCardTemplate = new AdaptiveCards.Templating.AdaptiveCardTemplate(null);
+            adaptiveCardTemplate = new AdaptiveCards.Templating.AdaptiveCardTemplate(null);
         }
 
         public string Expand(String rootData)
         {
-            //return adaptiveCardTemplate.Expand(rootData);
-            return "Expanded has not been implemetned";
+            return adaptiveCardTemplate.Expand(rootData);
+            //return "Expanded has not been implemetned";
         }
 
         private AdaptiveCards.Templating.AdaptiveCardTemplate adaptiveCardTemplate;
