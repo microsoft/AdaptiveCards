4a58c66c69fb4e00ab942e9d5a9c778b5ebae1a3
Joseph Woo <Joseph.Woo@microsoft.com>
2024-01-29 15:02:32 -0800

    updated

--- source/dotnet/Library/AdaptiveCards.Templating.CSharp.WinRT/AdaptiveCardsTemplate.cs
+++ source/dotnet/Library/AdaptiveCards.Templating.CSharp.WinRT/AdaptiveCardsTemplate.cs
@@ -4,13 +4,25 @@
 // AdaptiveCards template windows runtime component library
 namespace AdaptiveCards.Template
 {
+    /// <summary>
+    /// AdaptiveCards template windows runtime component library
+    /// </summary>
     public sealed class AdaptiveCardTemplate
     {
+        /// <summary>
+        /// Given a JSON template, create a AdaptiveCardTemplate object that can be used to expand the template
+        /// </summary>
+        /// <param name="jsonTemplate"></param>
         public AdaptiveCardTemplate(String jsonTemplate)
         {
             adaptiveCardTemplate = new AdaptiveCards.Templating.AdaptiveCardTemplate(jsonTemplate);
         }
 
+        /// <summary>
+        /// Expands the template with the given data
+        /// </summary>
+        /// <param name="rootData"></param>
+        /// <returns></returns>
         public string Expand(String rootData)
         {
             return adaptiveCardTemplate.Expand(rootData);
