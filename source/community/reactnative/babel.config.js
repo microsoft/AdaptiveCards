97c1d579fbe92f809d9a52b2a7c684f70cf92e5a
Paul Campbell <paulcam@microsoft.com>
2024-06-28 15:09:24 -0700

    Remove old ReactNative implementation (#8946)
    * Remove old ReactNative implementation
    
    This stuff has been spun out to https://github.com/Bigthinkcode/AdaptiveCards/
    
    * Delete github workflow as well...

--- source/community/reactnative/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = (api) => {
-    api.cache(true)
-  
-    return {
-        presets: ['module:metro-react-native-babel-preset']
-    }
-}
--- source/community/reactnative/src/components/inputs/index.js
+++
@@ -1,7 +0,0 @@
-export * from './text-input';
-export * from './number-input';
-export * from './toggle-input';
-export * from './date-input';
-export * from './time-input';
-export * from './picker-input';
-export * from './choice-set/choice-set-input';
