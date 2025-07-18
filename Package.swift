de39ccc332da2b78319d028fdab5261e782b0cd9
p901uiy <bijin.ext.karim@swedbank.se>
2025-07-18 12:52:56 +0200

    SwiftPM: broaden header search path

--- Package.swift
+++ Package.swift
@@ -18,6 +18,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/SVGKit/SVGKit.git", from: "3.0.0"),
+        .package(url: "https://github.com/microsoft/fluentui-apple.git", from: "0.34.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -31,7 +33,13 @@ let package = Package(
             ),
         .target(
             name: "AdaptiveCards",
-            dependencies: ["ObjectModel"],
+            dependencies: ["ObjectModel",
+                           .product(name: "SVGKit", package: "SVGKit"),
+                           .product(name: "FluentUITooltip", package: "fluentui-apple")
+                          ],
+            cSettings: [
+                .headerSearchPath(".")
+            ]
             path: "source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards",
             exclude: [
                 "CMakeLists.txt",
